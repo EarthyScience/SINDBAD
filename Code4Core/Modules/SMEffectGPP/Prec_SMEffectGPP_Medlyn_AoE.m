c11a91c9c0f5c25ac0fc015820febbaba93ac83d
Martin Jung <mjung@bgc-jena.mpg.de>
2014-06-19 14:30:24 +0200

    added conversion factor from mols to gC/mm

--- Code4Core/Modules/SMEffectGPP/Prec_SMEffectGPP_Medlyn_AoE.m
+++ Code4Core/Modules/SMEffectGPP/Prec_SMEffectGPP_Medlyn_AoE.m
@@ -1,6 +1,7 @@
 function [fe,fx,d,p]=Prec_SMEffectGPP_Medlyn_AoE(f,fe,fx,s,d,p,info);
 
 %precomputed: AoE   	= ca .* PsurfDay  ./ (1.6 .* (VPDDay + g1 .* sqrt(VPDDay)));    
-fe.AoE = f.ca .* f.PsurfDay  ./ (1.6 .* ( f.VPDDay + repmat( p.Transp.g1 ,1,info.Forcing.Size(2)) .* sqrt( f.VPDDay )));  
+%conversion factor:  6.6667e-004
+fe.AoE =  6.6667e-004 .* f.ca .* f.PsurfDay  ./ (1.6 .* ( f.VPDDay + repmat( p.Transp.g1 ,1,info.Forcing.Size(2)) .* sqrt( f.VPDDay )));  
  
 end
