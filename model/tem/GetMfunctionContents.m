3eae151dc2585dce371b98a27db04f152178bc71
Martin Jung <mjung@bgc-jena.mpg.de>
2015-01-09 17:12:40 +0100

    adjusted to also get contents of called functions that are in the same folder

--- model/tem/GetMfunctionContents.m
+++ model/tem/GetMfunctionContents.m
@@ -10,4 +10,35 @@ if strcmp(C(end),'return') || strcmp(C(end),'end')
 else
     C=C(2:end);
 end
+
+[pathstr,name,ext] = fileparts(mpth);
+
+%find mfunctions in directory
+mf=dir([pathstr '*.m']);
+
+%check if you find it in C
+for ii=1:length(mf)
+   [pathstr2,name,ext] = fileparts(mf(ii).name);
+
+    tmp=strfind(C,name);
+    
+    for iii=1:length(tmp)
+        
+        if ~isempty(tmp{iii})
+            %something found
+            P1=C(1:iii-1);
+            
+            [P2]=GetMfunctionContents([pathstr filesep mf(ii).name]);
+            
+            P3=C(iii+1:end);
+            C=vertcat(P1,P2,P3);
+        end
+        
+    end
+    
+    
+    
+    
+end
+
 end
