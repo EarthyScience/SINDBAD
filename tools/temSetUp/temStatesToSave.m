b0c23cedb0118be164f19c149ea694640bdf2ca2
Nuno <ncarval@bgc-jena.mpg.de>
2015-04-30 14:38:57 +0100

    adjustments on the states to save

---
+++ tools/temSetUp/temStatesToSave.m
@@ -0,0 +1,42 @@
+function info = temStatesToSave(info)
+% state variables to save
+% 0 nothing; 
+% 1 simple; 
+% 2 full;
+% 3 cmip5 style;
+
+% save nothing
+info.variables.saveState	= {};
+info.variables.aggStates    = {};
+
+% save simple
+if info.flags.saveStates >= 1
+    info.variables.aggStates	= {'cVeg' 'cLitter' 'cSoil'};
+    cPoolsID    = [];
+    for ij = 1:numel(info.variables.aggStates)
+        cPoolsID	= [cPoolsID info.helper.cPoolsID4.(info.variables.aggStates{ij})];
+    end
+    cPoolsID = unique(cPoolsID);
+    for ij = cPoolsID
+        info.variables.saveState	= [info.variables.saveState ...
+            {['s.cPools(' num2str(ij) ').value']}];
+    end
+%     info.variables.saveState	= [info.variables.saveState ...
+%         {'s.cVeg' 's.cLitter' 's.cSoil'}];
+    info.variables.saveState	= [info.variables.saveState ...
+        {'s.wGW' 's.wSWE' 's.wSM'}];
+end
+
+% save full
+if info.flags.saveStates == 2
+    for ij = 1:14
+        info.variables.saveState	= [info.variables.saveState ...
+            {['s.cPools(' num2str(ij) ').value']}];
+    end
+    for ij = 1:numel(info.params.SOIL.HeightLayer)
+        info.variables.saveState	= [info.variables.saveState ...
+            {['s.smPools(' num2str(ij) ').value']}];
+    end
+end
+
+end % function
