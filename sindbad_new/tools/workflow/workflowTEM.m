4ea636a3e11c7548c54a81828871a40b388a0028
Nuno Carvalhais <ncarval@BGC.MPG.DE>
2018-03-21 12:11:20 +0100

    initialize json and folder structure
    started the workflow/setup of TEM

---
+++ sindbad_new/tools/workflow/workflowTEM.m
@@ -0,0 +1,13 @@
+% workflow of the tem
+
+% the experiment configuratio file
+expConfigFile = '..\..\settings\experiment_standard.json';
+
+% setup the TEM
+info = setupTEM(expConfigFile);
+
+% prepare the TEM runs
+
+% run the model
+
+% post process the TEM outputs
