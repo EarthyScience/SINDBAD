8418bcf475f9b07f69a8154bb5e7661b600774c8
Christoph Niemann <cniemann@bgc-jena.mpg.de>
2018-03-22 10:52:25 +0100

    info update

--- sindbad_new/tools/writeJsonFile.m
+++ sindbad_new/tools/writeJsonFile.m
@@ -3,7 +3,7 @@ function fid = writeJsonFile(pth, filename, data_json)
     json_txt_created = jsonencode(data_json);
     json_txt_created = strrep(json_txt_created, '\/','/');
     
-    fid = fopen(strcat(pth, filename),'w');
+    fid = fopen(strcat(pth, filesep, filename),'w');
     fprintf(fid,json_txt_created);
     fid = fclose(fid);
 
