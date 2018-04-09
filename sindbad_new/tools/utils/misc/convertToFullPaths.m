3068ebeb8aa8fee556831c0b64c429faff8f05db
Nuno Carvalhais <ncarval@BGC.MPG.DE>
2018-04-09 19:01:50 +0200

    multi stuff
    built a converter to full paths based on sindbadroot (new). simplified the readConfigFiles (better error handling). adjusted readJsonFile to deal with \g and other special characters. added computer name to stampExperiment. added a flag to the readConfigFiles so that if some fields are missing yield an error (to be discussed if we just set this rule to false).

---
+++ sindbad_new/tools/utils/misc/convertToFullPaths.m
@@ -0,0 +1,19 @@
+function strPaths = convertToFullPaths(strPaths)
+if isstruct(strPaths)
+    for fn = fieldnames(strPaths)'
+        if ischar(strPaths.(fn{1}))
+            strPaths.(fn{1}) = getFullPath([sindbadroot strPaths.(fn{1})]);
+        end
+    end
+elseif ischar(strPaths)
+    strPaths	= getFullPath([sindbadroot strPaths]);
+elseif iscell(strPaths)
+    for i = 1:numel(strPaths)
+        if ischar(strPaths{i})
+            strPaths{i} = getFullPath([sindbadroot strPaths{i}]);
+        end
+    end
+else
+    error('ERR : strPaths : not a known datatype for strPaths')
+end
+end
