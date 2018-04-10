ec285436c48fbab223de3c45ff72a387768d92b9
Nuno Carvalhais <ncarval@BGC.MPG.DE>
2018-04-10 13:13:33 +0200

    changing filenames

---
+++ sindbad_new/model/modules/gppFwsoil/gppFwsoil_none/dyna_GPPfwSoil_none.m
@@ -0,0 +1,19 @@
+function [fx,s,d] = dyna_GPPfwSoil_none(f,fe,fx,s,d,p,info,tix)
+% #########################################################################
+% PURPOSE	: none
+% 
+% REFERENCES:
+% 
+% CONTACT	: mjung, ncarval
+% 
+% INPUT
+% 
+% OUTPUT
+% 
+% DEPENDENCIES  :
+% 
+% NOTES:
+% 
+% #########################################################################
+
+end
---
+++ sindbad_new/model/modules/gppFwsoil/gppFwsoil_none/prec_GPPfwSoil_none.m
@@ -0,0 +1,22 @@
+function [fe,fx,d,p] = prec_GPPfwSoil_none(f,fe,fx,s,d,p,info,tix)
+% #########################################################################
+% PURPOSE	: none
+% 
+% REFERENCES:
+% 
+% CONTACT	: mjung, ncarval
+% 
+% INPUT
+% 
+% OUTPUT
+% 
+% DEPENDENCIES  :
+% 
+% NOTES:
+% 
+% #########################################################################
+
+% d.GPPfwSoil.SMScGPP	= ones(info.forcing.size); %-> should be
+% initialized in teh preallocation function
+
+end
