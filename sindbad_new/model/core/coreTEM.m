261cd19e70dfed28850885567fc0d21ff1318083
Nuno <ncarval@bgc-jena.mpg.de>
2018-04-14 00:59:53 +0200

    remove old cAlloc. Adjust .fun->.funHandle in the code.

--- sindbad_new/model/core/coreTEM.m
+++ sindbad_new/model/core/coreTEM.m
@@ -59,7 +59,7 @@ function [f,fe,fx,s,d,p] = coreTEM(f,fe,fx,s,d,p,info)
 % -------------------------------------------------------------------------
 for prc = 1:numel(info.code.preComp)
     if info.code.preComp(prc).doAlways == 1
-        [f,fe,fx,s,d,p]	= info.code.preComp(prc).fun(f,fe,fx,s,d,p,info);
+        [f,fe,fx,s,d,p]	= info.code.preComp(prc).funHandle(f,fe,fx,s,d,p,info);
     end
 end
 
@@ -72,7 +72,7 @@ ms	= info.tem.model.code.ms;
 % LOOP : loop through the whole length of of the forcing dataset
 for tix = 1:info.forcing.size(2)
     % get states from previous time step
-    [f,fe,fx,s,d,p]	= ms.getStates.fun(f,fe,fx,s,d,p,info,tix);
+    [f,fe,fx,s,d,p]	= ms.getStates.funHandle(f,fe,fx,s,d,p,info,tix);
               
         
     % ---------------------------------------------------------------------
@@ -80,82 +80,83 @@ for tix = 1:info.forcing.size(2)
     % 0 - SOIL - to get the soil related params ...
     % 0 - VEG - put here any LC changes / phenology / disturbances / ...
     % ---------------------------------------------------------------------
-    [f,fe,fx,s,d,p]	= ms.ptopo.fun(f,fe,fx,s,d,p,info,tix);
-    [f,fe,fx,s,d,p]	= ms.psoil.fun(f,fe,fx,s,d,p,info,tix);
-    [f,fe,fx,s,d,p]	= ms.pveg.fun(f,fe,fx,s,d,p,info,tix);
+    [f,fe,fx,s,d,p]	= ms.ptopo.funHandle(f,fe,fx,s,d,p,info,tix);
+    [f,fe,fx,s,d,p]	= ms.psoil.funHandle(f,fe,fx,s,d,p,info,tix);
+    [f,fe,fx,s,d,p]	= ms.pveg.funHandle(f,fe,fx,s,d,p,info,tix);
 
     % ---------------------------------------------------------------------
     % 1 - Snow
     % ---------------------------------------------------------------------
-    [f,fe,fx,s,d,p]	= ms.wSnwFr.fun(f,fe,fx,s,d,p,info,tix);    % add snow fall and calculate SnowCoverFraction
-    [f,fe,fx,s,d,p]    = ms.EvapSub.fun(f,fe,fx,s,d,p,info,tix);  % calculate sublimation and update swe
-    [f,fe,fx,s,d,p]    = ms.Qsnw.fun(f,fe,fx,s,d,p,info,tix);     % calculate snowmelt and update SWE
+    [f,fe,fx,s,d,p]	= ms.wSnwFr.funHandle(f,fe,fx,s,d,p,info,tix);    % add snow fall and calculate SnowCoverFraction
+    [f,fe,fx,s,d,p]    = ms.EvapSub.funHandle(f,fe,fx,s,d,p,info,tix);  % calculate sublimation and update swe
+    [f,fe,fx,s,d,p]    = ms.Qsnw.funHandle(f,fe,fx,s,d,p,info,tix);     % calculate snowmelt and update SWE
     
     % ---------------------------------------------------------------------
     % 2 - Water 
     % ---------------------------------------------------------------------
-    [f,fe,fx,s,d,p]    = ms.EvapInt.fun(f,fe,fx,s,d,p,info,tix);         % interception evaporation
-    [f,fe,fx,s,d,p]    = ms.QinfExc.fun(f,fe,fx,s,d,p,info,tix);           % infiltration excess runoff
-    [f,fe,fx,s,d,p]    = ms.wSoilSatFr.fun(f,fe,fx,s,d,p,info,tix);    % saturation runoff
-    [f,fe,fx,s,d,p]    = ms.Qsat.fun(f,fe,fx,s,d,p,info,tix);            % saturation runoff
-    [f,fe,fx,s,d,p]    = ms.QwSoilRchg.fun(f,fe,fx,s,d,p,info,tix);         % recharge the soil
-    [f,fe,fx,s,d,p]    = ms.Qint.fun(f,fe,fx,s,d,p,info,tix);            % interflow
+    [f,fe,fx,s,d,p]    = ms.EvapInt.funHandle(f,fe,fx,s,d,p,info,tix);         % interception evaporation
+    [f,fe,fx,s,d,p]    = ms.QinfExc.funHandle(f,fe,fx,s,d,p,info,tix);           % infiltration excess runoff
+    [f,fe,fx,s,d,p]    = ms.wSoilSatFr.funHandle(f,fe,fx,s,d,p,info,tix);    % saturation runoff
+    [f,fe,fx,s,d,p]    = ms.Qsat.funHandle(f,fe,fx,s,d,p,info,tix);            % saturation runoff
+    [f,fe,fx,s,d,p]    = ms.QwSoilRchg.funHandle(f,fe,fx,s,d,p,info,tix);         % recharge the soil
+    [f,fe,fx,s,d,p]    = ms.Qint.funHandle(f,fe,fx,s,d,p,info,tix);            % interflow
                                                                         % if e.g. infiltration excess runoff and or saturation runoff are not
                                                                         % explicitly modelled then assign a dummy handle that returnes zeros and
                                                                         % lump the FastRunoff into interflow
-    [f,fe,fx,s,d,p]    = ms.QwGRchg.fun(f,fe,fx,s,d,p,info,tix);           % recharge the groundwater 
-    [f,fe,fx,s,d,p]    = ms.Qbase.fun(f,fe,fx,s,d,p,info,tix);             % baseflow
-    [f,fe,fx,s,d,p]    = ms.wG2wSoil.fun(f,fe,fx,s,d,p,info,tix);       % Groundwater soil moisture interactions (e.g. capilary flux, water
+    [f,fe,fx,s,d,p]    = ms.QwGRchg.funHandle(f,fe,fx,s,d,p,info,tix);           % recharge the groundwater 
+    [f,fe,fx,s,d,p]    = ms.Qbase.funHandle(f,fe,fx,s,d,p,info,tix);             % baseflow
+    [f,fe,fx,s,d,p]    = ms.wG2wSoil.funHandle(f,fe,fx,s,d,p,info,tix);       % Groundwater soil moisture interactions (e.g. capilary flux, water
                                                                         % table in root zone etc)
-    [f,fe,fx,s,d,p]    = ms.EvapSoil.fun(f,fe,fx,s,d,p,info,tix);             % soil evaporation
+    [f,fe,fx,s,d,p]    = ms.EvapSoil.funHandle(f,fe,fx,s,d,p,info,tix);             % soil evaporation
             
     % ---------------------------------------------------------------------
     % 3 - Transpiration and GPP
     % ---------------------------------------------------------------------
-	[f,fe,fx,s,d,p]    = ms.WUE.fun(f,fe,fx,s,d,p,info,tix);              % estimate WUE
-    [f,fe,fx,s,d,p]    = ms.TranfwSoil.fun(f,fe,fx,s,d,p,info,tix);     % supply limited Transpiration
-    [f,fe,fx,s,d,p]    = ms.GPPfRdir.fun(f,fe,fx,s,d,p,info,tix);   % compute 'stress' scalars
-    [f,fe,fx,s,d,p]    = ms.GPPpot.fun(f,fe,fx,s,d,p,info,tix);           % maximum instantaneous radiation use efficiency
-    [f,fe,fx,s,d,p]    = ms.GPPfTair.fun(f,fe,fx,s,d,p,info,tix);    % effect of temperature
-    [f,fe,fx,s,d,p]    = ms.GPPfVPD.fun(f,fe,fx,s,d,p,info,tix);     % VPD effect
-    [f,fe,fx,s,d,p]    = ms.GPPdem.fun(f,fe,fx,s,d,p,info,tix);        % combine effects as multiplicative or minimum
-    [f,fe,fx,s,d,p]    = ms.GPPfwSoil.fun(f,fe,fx,s,d,p,info,tix);      % if 'coupled' requires access to iwue param    
-    [f,fe,fx,s,d,p]    = ms.GPPact.fun(f,fe,fx,s,d,p,info,tix);        % combine effects as multiplicative or minimum    
-    [f,fe,fx,s,d,p]    = ms.TranAct.fun(f,fe,fx,s,d,p,info,tix);           % if coupled computed from GPP
-    [f,fe,fx,s,d,p]    = ms.wRootUptake.fun(f,fe,fx,s,d,p,info,tix);       % root water uptake (extract water from soil)
+	[f,fe,fx,s,d,p]    = ms.WUE.funHandle(f,fe,fx,s,d,p,info,tix);              % estimate WUE
+    [f,fe,fx,s,d,p]    = ms.TranfwSoil.funHandle(f,fe,fx,s,d,p,info,tix);     % supply limited Transpiration
+    [f,fe,fx,s,d,p]    = ms.GPPfRdir.funHandle(f,fe,fx,s,d,p,info,tix);   % compute 'stress' scalars
+    [f,fe,fx,s,d,p]    = ms.GPPpot.funHandle(f,fe,fx,s,d,p,info,tix);           % maximum instantaneous radiation use efficiency
+    [f,fe,fx,s,d,p]    = ms.GPPfTair.funHandle(f,fe,fx,s,d,p,info,tix);    % effect of temperature
+    [f,fe,fx,s,d,p]    = ms.GPPfVPD.funHandle(f,fe,fx,s,d,p,info,tix);     % VPD effect
+    [f,fe,fx,s,d,p]    = ms.GPPdem.funHandle(f,fe,fx,s,d,p,info,tix);        % combine effects as multiplicative or minimum
+    [f,fe,fx,s,d,p]    = ms.GPPfwSoil.funHandle(f,fe,fx,s,d,p,info,tix);      % if 'coupled' requires access to iwue param    
+    [f,fe,fx,s,d,p]    = ms.GPPact.funHandle(f,fe,fx,s,d,p,info,tix);        % combine effects as multiplicative or minimum    
+    [f,fe,fx,s,d,p]    = ms.TranAct.funHandle(f,fe,fx,s,d,p,info,tix);           % if coupled computed from GPP
+    [f,fe,fx,s,d,p]    = ms.wRootUptake.funHandle(f,fe,fx,s,d,p,info,tix);       % root water uptake (extract water from soil)
     
     % ---------------------------------------------------------------------
     % 4 - Climate effects on metabolic processes
     % ---------------------------------------------------------------------
-%% these names need to change... to cTaufwSoil and cTaufTsoil
-    [f,fe,fx,s,d,p]    = ms.cTaufTsoil.fun(f,fe,fx,s,d,p,info,tix);  % effect of soil temperature on decomposition rates
-    [f,fe,fx,s,d,p]    = ms.cTaufwSoil.fun(f,fe,fx,s,d,p,info,tix);  % effect of soil moisture on decomposition rates
+    [f,fe,fx,s,d,p]    = ms.cTaufTsoil.funHandle(f,fe,fx,s,d,p,info,tix);  % effect of soil temperature on decomposition rates
+    [f,fe,fx,s,d,p]    = ms.cTaufwSoil.funHandle(f,fe,fx,s,d,p,info,tix);  % effect of soil moisture on decomposition rates
     
-    [f,fe,fx,s,d,p]    = ms.RAfTair.fun(f,fe,fx,s,d,p,info,tix);   % temperature effect on autotrophic maintenance respiration
+    [f,fe,fx,s,d,p]    = ms.RAfTair.funHandle(f,fe,fx,s,d,p,info,tix);    % temperature effect on autotrophic maintenance respiration
     
 
     % ---------------------------------------------------------------------
     % 5 - Allocation of C within plant organs
     % ---------------------------------------------------------------------
-    [f,fe,fx,s,d,p]	= ms.cAllocfwSoil.fun(f,fe,fx,s,d,p,info,tix);      % effect of soil moisture on carbon allocation 
-    [f,fe,fx,s,d,p]	= ms.cAllocfTsoil.fun(f,fe,fx,s,d,p,info,tix);      % effect of soil temperature on carbon allocation 
-    [f,fe,fx,s,d,p]	= ms.cAllocfNut.fun(f,fe,fx,s,d,p,info,tix);        % (pseudo)effect of nutrients on carbon allocation 
-    [f,fe,fx,s,d,p]	= ms.cAlloc.fun(f,fe,fx,s,d,p,info,tix);        % combine the different effects of carbon allocation 
-    [f,fe,fx,s,d,p]	= ms.cAllocfTreeCover.fun(f,fe,fx,s,d,p,info,tix);  % adjustment of carbon allocation according to tree cover
+    [f,fe,fx,s,d,p]	= ms.cAllocfwSoil.funHandle(f,fe,fx,s,d,p,info,tix);      % effect of soil moisture on carbon allocation 
+    [f,fe,fx,s,d,p]	= ms.cAllocfTsoil.funHandle(f,fe,fx,s,d,p,info,tix);      % effect of soil temperature on carbon allocation 
+    [f,fe,fx,s,d,p]	= ms.cAllocfNut.funHandle(f,fe,fx,s,d,p,info,tix);        % (pseudo)effect of nutrients on carbon allocation 
+    [f,fe,fx,s,d,p]	= ms.cAlloc.funHandle(f,fe,fx,s,d,p,info,tix);        % combine the different effects of carbon allocation 
+    [f,fe,fx,s,d,p]	= ms.cAllocfTreeCover.funHandle(f,fe,fx,s,d,p,info,tix);  % adjustment of carbon allocation according to tree cover
     
     % ---------------------------------------------------------------------
     % 6 - Autotrophic respiration
     % ---------------------------------------------------------------------
-    [f,fe,fx,s,d,p]    = ms.RAact.fun(f,fe,fx,s,d,p,info,tix);             % determine growth and maintenance respiration -> NPP
+    [f,fe,fx,s,d,p]    = ms.RAact.funHandle(f,fe,fx,s,d,p,info,tix);             % determine growth and maintenance respiration -> NPP
     
     % ---------------------------------------------------------------------
     % 7 - Carbon transfers to soil pools
     % ---------------------------------------------------------------------
-    [f,fe,fx,s,d,p]    = ms.cTaufpSoil.fun(f,fe,fx,s,d,p,info,tix);   % effect of soil texture on soil decomposition rates
-    [f,fe,fx,s,d,p]    = ms.cTaufpVeg.fun(f,fe,fx,s,d,p,info,tix);   % effect of vegetation properties on soil decomposition rates
-    [f,fe,fx,s,d,p]    = ms.cFlowfpSoil.fun(f,fe,fx,s,d,p,info,tix); % effect of soil texture on transfer between C pools
-    [f,fe,fx,s,d,p]    = ms.cFlowfpVeg.fun(f,fe,fx,s,d,p,info,tix);  % effect of vegetation properties on transfer between C pools
-    [f,fe,fx,s,d,p]    = ms.cCycle.fun(f,fe,fx,s,d,p,info,tix);               % allocate carbon to vegetation components
+    [f,fe,fx,s,d,p]    = ms.cTauBase.funHandle(f,fe,fx,s,d,p,info,tix);
+    [f,fe,fx,s,d,p]    = ms.cFlowBase.funHandle(f,fe,fx,s,d,p,info,tix);
+    [f,fe,fx,s,d,p]    = ms.cTaufpSoil.funHandle(f,fe,fx,s,d,p,info,tix);   % effect of soil texture on soil decomposition rates
+    [f,fe,fx,s,d,p]    = ms.cTaufpVeg.funHandle(f,fe,fx,s,d,p,info,tix);   % effect of vegetation properties on soil decomposition rates
+    [f,fe,fx,s,d,p]    = ms.cFlowfpSoil.funHandle(f,fe,fx,s,d,p,info,tix); % effect of soil texture on transfer between C pools
+    [f,fe,fx,s,d,p]    = ms.cFlowfpVeg.funHandle(f,fe,fx,s,d,p,info,tix);  % effect of vegetation properties on transfer between C pools
+    [f,fe,fx,s,d,p]    = ms.cCycle.funHandle(f,fe,fx,s,d,p,info,tix);               % allocate carbon to vegetation components
                                                                         % litterfall and litter scalars
                                                                         % calculate carbon cycle/decomposition/respiration in soil
 	
@@ -165,7 +166,7 @@ for tix = 1:info.forcing.size(2)
     % ---------------------------------------------------------------------
     
     % store current states in previous state variables
-    [f,fe,fx,s,d,p]	= ms.storeStates.fun(f,fe,fx,s,d,p,info,tix);
+    [f,fe,fx,s,d,p]	= ms.storeStates.funHandle(f,fe,fx,s,d,p,info,tix);
     
 end % END LOOP
 
