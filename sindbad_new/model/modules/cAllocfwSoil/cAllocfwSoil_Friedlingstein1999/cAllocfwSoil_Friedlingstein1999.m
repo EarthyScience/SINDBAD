ecb041799b935735802acb84b27fb2ad40cd5355
Nuno <ncarval@bgc-jena.mpg.de>
2018-04-13 23:58:08 +0200

    modularizing carbon allocation

---
+++ sindbad_new/model/modules/cAllocfwSoil/cAllocfwSoil_Friedlingstein1999/cAllocfwSoil_Friedlingstein1999.m
@@ -0,0 +1,9 @@
+function [f,fe,fx,s,d,p] = cAllocfwSoil_Friedlingstein1999(f,fe,fx,s,d,p,info,tix)
+
+% computation for the moisture effect on decomposition/mineralization
+NL_fW                                   = d.cTaufwSoil.BGME(:,tix);
+NL_fW(NL_fW >= p.cAllocfwSoil.maxL_fW)  = p.cAllocfwSoil.maxL_fW(NL_fW >= p.cAllocfwSoil.maxL_fW);
+NL_fW(NL_fW <= p.cAllocfwSoil.minL_fW)	= p.cAllocfwSoil.minL_fW(NL_fW <= p.cAllocfwSoil.minL_fW);
+fe.cAllocfwSoil.NL_fW(:,tix)            = NL_fW;
+
+end % function
