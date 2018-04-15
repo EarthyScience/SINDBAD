67fdabef3dbf5e2ab40ff7b4429d3f29175cc270
dr-ko <skoirala@bgc-jena.mpg.de>
2018-04-15 09:25:11 +0200

    editorial temstruct

--- model/tem/initTEMStruct.m
+++ model/tem/initTEMStruct.m
@@ -2,7 +2,7 @@ function [fx,fe,d,s] = initTEMStruct(info,varargin)
 % initialize model states and flux matrices
 % carbon and water pools
 
-% set up all the variables as NaN matrices...    
+% set up all the variables as NaN matrices...
 [fx,fe,d]	= PreAllocate(info);
 
 % initialize carbon and water states
@@ -15,11 +15,11 @@ CheckInitialisedStates(info,s,d)
 if nargin == 3
     sSU	= varargin{1};
     dSU = varargin{2};
-    
+
     % this should also be automatized...
     % inherit the state variables
     % carbon
-    if~isempty(strmatch('cPools',fieldnames(sSU),'exact'))
+    if ~isempty(strmatch('cPools',fieldnames(sSU),'exact'))
         s.cPools = sSU.cPools;
     end
     % water
@@ -28,15 +28,15 @@ if nargin == 3
     d.Temp.pwGW(:,1)      = sSU.wGW(:,end);
     d.Temp.pwGWR(:,1)     = sSU.wGWR(:,end);
 %     d.Temp.pwWTD(:,1)     = sSU.wWTD(:,end);
-    
+
     % inherit the diagnostics
     % carbon
     % this is ungly....
-    if~isempty(strmatch('CAllocationVeg',fieldnames(d),'exact'))
+    if ~isempty(strmatch('CAllocationVeg',fieldnames(d),'exact'))
         d.CAllocationVeg.c2pool     = dSU.CAllocationVeg.c2pool;
     end
     % water
-    
+
     %MJ:
 %    s.wFrSat	= sSU.frSat;
     %d.SaturatedFraction.frSat(:,1)	= dSU.SaturatedFraction.frSat(:,end);
@@ -44,9 +44,9 @@ if nargin == 3
     if~isempty(strmatch('SoilMoistEffectRH',fieldnames(d),'exact'))
         d.SoilMoistEffectRH.pBGME       = dSU.SoilMoistEffectRH.BGME(:,end);
     end
-    
+
     % do we also need to inherit other states or factors!?!?
-    
+
 end
 
 
