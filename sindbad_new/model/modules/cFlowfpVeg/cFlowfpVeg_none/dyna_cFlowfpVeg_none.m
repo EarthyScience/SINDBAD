b5e0117a14383904d7d30a435195a0a3c11db633
Tina Trautmann <ttraut@bgc-jena.mpg.de>
2018-04-19 15:08:15 +0200

    adding catalog folder for cost functions, search algorithm and variable bounds; adjusting dependent fields in info after editing the settings

--- sindbad_new/model/modules/cFlowfpVeg/cFlowfpVeg_none/dyna_cFlowfpVeg_none.m
+++ sindbad_new/model/modules/cFlowfpVeg/cFlowfpVeg_none/dyna_cFlowfpVeg_none.m
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-function [fx,s,d,f] = dyna_cFlowfpVeg_none(f,fe,fx,s,d,p,info,tix)
-=======
 function [f,fe,fx,s,d,p] = dyna_cFlowfpVeg_none(f,fe,fx,s,d,p,info,tix)
->>>>>>> 3cef301d1b5d3fa9e7d8f974e6d6a565f3b9e986
 end %function
