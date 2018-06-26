db37bd32feb084e7bab20658538fef701b36221c
Tina Trautmann <ttraut@bgc-jena.mpg.de>
2018-06-26 11:56:15 +0200

    common output folder and path update
    - basis for all output folders/paths is the outputDirPath from experiment.json
    - outputDirPath can be a relative path within sindbad or an absolute path
    - if the output folder already exists, a new subfolder is created
    - if no outputDirPath is provided in the experiment.json, it is genereated automatically in the sindbad root as ../output/[experimentName_domain_runDate]/
    - the name of outputInfoFile is generated automatically based on the experiment name, the spatial domain and run date 
    - the output folder includes a copy of the settings (all config files), the genereated code, the info.json and potential further output from model run
    
    - the paths are saved in the info using filesep

--- tools/workflow/setExperimentPaths.m
+++ tools/workflow/setExperimentPaths.m
@@ -7,12 +7,15 @@ function [ info ] = setExperimentPaths(info)
 %       ++ after reading the experiment.json file
 %
 % Purposes:
+%   + creates the outputInfoFilename
 %   + creates the paths for model run and generated code within the info
 %   based on the outputDirPath in the experiment.json
 %       ++ if no/only whitespace for outputDirPath is given, a default name
 %       using the experiment name, domain and runDate is defined
 %       ++ if the output directory already exists, a new path using 
 %       the experiment name, domain and runDate is created as subfolder
+%   + checks if paths of forcing and constraints exists and converts them
+%   to absolute paths
 
 % Conventions:
 %   + whitespaces are removed
@@ -30,7 +33,10 @@ function [ info ] = setExperimentPaths(info)
 %% check if the experiment name, domain and runDate exist
 if isfield(info.experiment, 'name')
     info.experiment.name(info.experiment.name == ' ') = [];
-    name = info.experiment.name;    
+    name = info.experiment.name;
+else
+    name = 'unnamedExperiment';
+    info.experiment.name = name;
 end
 
 if isfield(info.experiment, 'domain')
@@ -44,28 +50,47 @@ default_tmp = [name '_' domain '_' info.experiment.runDate];
 %% check if the outputDirPath is given in the info
 if isfield(info.experiment, 'outputDirPath')
     info.experiment.outputDirPath(info.experiment.outputDirPath == ' ') = [];
-    if ~isempty(info.experiment.outputDirPath)       
-        outputDirPath_full = convertToFullPaths(info.experiment.outputDirPath);
+    if ~isempty(info.experiment.outputDirPath)
+         info.experiment.outputDirPath  = replace(info.experiment.outputDirPath,{'/','\'},filesep);
+        % check whether it is an absolute path
+        if strcmp(getFullPath(info.experiment.outputDirPath), info.experiment.outputDirPath)==1
+            outputDirPath_full = info.experiment.outputDirPath;
+        else
+            outputDirPath_full = convertToFullPaths(info.experiment.outputDirPath);
+        end
+    else
+        outputDirPath_def               = ['output' filesep default_tmp filesep];
+        outputDirPath_full              = convertToFullPaths(outputDirPath_def);
+        disp(['MSG : setupTEM : no "outputDirPath" was provided : a default path is created: ' outputDirPath_full  ])     
     end
 else
     % default output directory
-    info.experiment.outputDirPath  = ['output' filesep default_tmp filesep];
-    outputDirPath_full = convertToFullPaths(info.experiment.outputDirPath);
-    disp(['MSG : setupTEM : no "outputDirPath" was provided : a default path is created: ' info.experiment.outputDirPath ])
-end    
-    
+    outputDirPath_def               = ['output' filesep default_tmp filesep];
+    outputDirPath_full              = convertToFullPaths(outputDirPath_def);
+    disp(['MSG : setupTEM : no "outputDirPath" was provided : a default path is created: ' outputDirPath_full  ])
+end
+
+
+
 %% check if the outputDirPath already exists -> if so, rename it
 ii = 0;
-while exist(outputDirPath_full, 'dir')
-    outputDirPath_new   = [info.experiment.outputDirPath default_tmp '_v' num2str(ii) filesep];
-    outputDirPath_full  = convertToFullPaths(outputDirPath_new);
+outputDirPath_new = outputDirPath_full;
+while exist(outputDirPath_new, 'dir')
+    outputDirPath_new  =    [outputDirPath_full default_tmp '_v' num2str(ii) filesep];
     ii = ii+1;
 end
 
 if ii > 0
-    info.experiment.outputDirPath = outputDirPath_new;
+    outputDirPath_full =    outputDirPath_new;
+    default_tmp        =    [default_tmp '_v' num2str(ii)];
     disp(['MSG : setupTEM : the outputDirPath: ' info.experiment.outputDirPath ' already exists' newline  'a default output path is created: ' outputDirPath_new  ]) 
 end
+
+%% put the full outputDirPath in the info
+info.experiment.outputDirPath	=   outputDirPath_full;
+
+%% set the output info.json file -should this be a filename or absolute path?
+info.experiment.outputInfoFile =    [outputDirPath_full 'Info_' default_tmp '.json'];
 
 %% set the runDir path
 info.tem.model.paths.runDir    =    info.experiment.outputDirPath;
@@ -87,28 +112,58 @@ for n1 = {'model','spinup'}
             end
         end
         if feedIt
-            info.tem.(n1{1}).paths.genCode.(n2{1})	= convertToFullPaths([sindbadroot info.tem.model.paths.runDir 'code' filesep 'gen' str2 str1 '.m']);
+            info.tem.(n1{1}).paths.genCode.(n2{1})	= [info.tem.model.paths.runDir 'code' filesep 'gen' str2 str1 '.m'];
         end
     end
 end
 
 
 %% convert paths in info to absolute paths
-info.experiment.outputDirPath	=   outputDirPath_full;
+info.tem.model.paths.coreTEM           =   convertToFullPaths(info.tem.model.paths.coreTEM);
+info.tem.model.paths.modulesDir        =   convertToFullPaths(info.tem.model.paths.modulesDir);
 
-info.tem.model.paths            =   convertToFullPaths(info.tem.model.paths);
-info.tem.spinup.paths           =   convertToFullPaths(info.tem.spinup.paths);
+info.tem.spinup.paths.restartFile      =   convertToFullPaths(info.tem.spinup.paths);
 
-%% set the output info.json file
-if isfield(info.experiment, 'outputInfoFile')
-    info.experiment.outputInfoFile(info.experiment.outputInfoFile == ' ') = [];
-    if isempty(info.experiment.outputInfoFile)
-        info.experiment.outputInfoFile = ['Info_' default_tmp '.json'];
+%paths of forcing
+for ii=1:numel(info.tem.forcing.VariableNames)
+    var_tmp = info.tem.forcing.VariableNames{ii};
+    pth_tmp = info.tem.forcing.(var_tmp).DataPath;
+    if exist(pth_tmp)~= 0
+        if strcmp(strrep(getFullPath(pth_tmp),'\','/'), strrep(pth_tmp,'\','/'))==0
+            info.tem.forcing.(var_tmp).DataPath = convertToFullPaths(pth_tmp);
+        end
+    else
+        warning(['MSG: path for forcing variable ' var_tmp ': ' pth_tmp ' does not exist!']);
     end
-else
-    info.experiment.outputInfoFile	=   [info.experiment.outputDirPath info.experiment.outputInfoFile]; 
 end
 
+%paths of constraints
+if isfield(info,'opti')
+    for ii=1:numel(info.opti.constraints.VariableNames)
+        var_tmp = info.opti.constraints.VariableNames{ii};
+        pth_tmp = info.opti.constraints.(var_tmp).DataPath;
+        if exist(pth_tmp)~= 0
+            if strcmp(strrep(getFullPath(pth_tmp),'\','/'), strrep(pth_tmp,'\','/'))==0
+                info.opti.constraints.(var_tmp).DataPath = convertToFullPaths(pth_tmp);
+            end
+        else
+            warning(['MSG: path for observational variable ' var_tmp ': ' pth_tmp ' does not exist!']);
+        end
+        %for observational uncertainties
+        if ~isempty(info.opti.constraints.(var_tmp).VariableUncertainty.Data.DataPath)
+            pth_tmp = info.opti.constraints.(var_tmp).VariableUncertainty.Data.DataPath;
+            if exist(pth_tmp)~= 0
+                if strcmp(strrep(getFullPath(pth_tmp),'\','/'), strrep(pth_tmp,'\','/'))==0
+                    info.opti.constraints.(var_tmp).VariableUncertainty.Data.DataPath = convertToFullPaths(pth_tmp);
+                end
+            else
+                warning(['MSG: path for uncertainty of observational variable ' var_tmp ': ' pth_tmp ' does not exist!']);
+            end
+        else
+            disp(['MSG: no uncertainty data for observational ' var_tmp ' provided']);
+        end
+    end
+end
 
 end
 
