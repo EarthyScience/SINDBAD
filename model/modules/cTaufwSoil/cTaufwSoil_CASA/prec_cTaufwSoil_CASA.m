f039bd88cd249358963b9518529a4d09e876f30f
Nuno <j274812@nwytg.com>
2018-08-14 09:54:20 +0200

    assorted additions for the carbon cycle

--- model/modules/cTaufwSoil/cTaufwSoil_CASA/prec_cTaufwSoil_CASA.m
+++ model/modules/cTaufwSoil/cTaufwSoil_CASA/prec_cTaufwSoil_CASA.m
@@ -1,5 +1,5 @@
 function [f,fe,fx,s,d,p] = prec_cTaufwSoil_CASA(f,fe,fx,s,d,p,info)
-% d.prev.cTaufwSoil_fwSoil        =   info.tem.helpers.arrays.onespix;
+% d.prev.d_cTaufwSoil_fwSoil        =   info.tem.helpers.arrays.onespix;
 % %sujan
 % d.prev.cTaufwSoil.fwSoil        =   info.tem.helpers.arrays.onespixzix.c.cEco;
 end % function
