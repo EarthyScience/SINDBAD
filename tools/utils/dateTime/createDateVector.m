bda2838bb80f29de256a9ea2948ae5c519cfc197
Tina Trautmann <ttraut@bgc-jena.mpg.de>
2019-10-11 14:46:58 +0900

    updates on tools/utils
    - createDateVector: can handel 'mm-dd-yyyy' and 'yyyy-mm-dd' format
    - readForcing: also get space (lat, lon) information when reading .mat files
    - readSpinupForcing: assumes the file is in the same folder as the forcing data and has the same name with the extension '[..]SpinUp.mat' 
    - just passed my version of the other files (but sometimes changes were made by Sujan)

--- tools/utils/dateTime/createDateVector.m
+++ tools/utils/dateTime/createDateVector.m
@@ -28,8 +28,16 @@ function [xOut, days, months, years, Y, M, D] = createDateVector(startDate, endD
 %
 
 %%
+tmp = strsplit(startDate,'-');
+if str2double(tmp{1}) > str2double(tmp{3})
 startD      = datenum(startDate,'yyyy-mm-dd');
 endD        = datenum(endDate,'yyyy-mm-dd');
+    inForm      = 'yyyy-MM-dd';
+else
+    startD      = datenum(startDate,'mm-dd-yyyy');
+    endD        = datenum(endDate,'mm-dd-yyyy');
+    inForm      = 'MM-dd-yyyy';
+end
 days        = endD-startD+1; % days between startD & endD + 1 to account start day
 daysvec     = datevec(days-1); % duration of days as datevector
 years       = daysvec(1)+1; % years of duration + 1 to account start year
@@ -37,14 +45,14 @@ months      = daysvec(1)*12+daysvec(2);
 
 switch steps
     case 'd'
-        xOut        = [datetime(startDate,'InputFormat','yyyy-MM-dd'),...
-            datetime(startDate, 'InputFormat','yyyy-MM-dd')+caldays(1:days-1)];
+        xOut        = [datetime(startDate,'InputFormat',inForm),...
+            datetime(startDate, 'InputFormat',inForm)+caldays(1:days-1)];
     case 'm'
-        xOut      = [datetime(startDate,'InputFormat','yyyy-MM-dd'),...
-            datetime(startDate, 'InputFormat','yyyy-MM-dd')+calmonths(1:months-1)];
+        xOut      = [datetime(startDate,'InputFormat',inForm),...
+            datetime(startDate, 'InputFormat',inForm)+calmonths(1:months-1)];
     case 'y'
-        xOut      = [datetime(startDate,'InputFormat','yyyy-MM-dd'),...
-            datetime(startDate, 'InputFormat','yyyy-MM-dd')+calmonths(1:years-1)];
+        xOut      = [datetime(startDate,'InputFormat',inForm),...
+            datetime(startDate, 'InputFormat',inForm)+calmonths(1:years-1)];
     otherwise
         disp(['WARN : readInput : not a valid time step : ' steps ]);
 end
