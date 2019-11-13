4fc75c791f099f5d1391967389a1b1805503c8a0
dr-ko <skoirala@bgc-jena.mpg.de>
2019-11-13 03:50:14 +0100

    module cleanup scripts and examples

--- model/modules/cLAI/cLAI_constant/prec_cLAI_constant.m
+++ model/modules/cLAI/cLAI_constant/prec_cLAI_constant.m
@@ -1,4 +1,23 @@
 function [f,fe,fx,s,d,p]=prec_cLAI_constant(f,fe,fx,s,d,p,info)
-d.cLAI.LAI = info.tem.helpers.arrays.onespixtix .* p.cLAI.constantLAI;    
-% d.cLAI.LAI = repmat(1:1:size(info.tem.helpers.arrays.onespixtix,2),size(info.tem.helpers.arrays.onespixtix,1),1) ; %.* p.cLAI.constantLAI;    
+% sets the value of LAI as a constant 
+% Inputs:
+%	- info helper for array 
+%	- p.cLAI.constantLAI
+%
+% Outputs:
+%   - fe.cLAI.LAI: an extra forcing that creates a time series of constant LAI
+%
+% Modifies:
+% 	- None
+% References:
+%	- 
+%
+% Created by:
+%   - Sujan Koirala (skoirala@bgc-jena.mpg.de)
+%
+% Versions:
+%   - 1.0 on 11.11.2019 (skoirala): cleaned up the code
+%
+%% 
+fe.cLAI.LAI = info.tem.helpers.arrays.onespixtix .* p.cLAI.constantLAI;    
 end
