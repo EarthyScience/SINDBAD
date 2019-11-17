f4f24067e19542e7c0dbff05f6625d2aae036ac6
dr-ko <skoirala@bgc-jena.mpg.de>
2019-11-17 21:08:19 +0100

    settings cleanup

--- testBeds/runTests_skoirala.m
+++ testBeds/runTests_skoirala.m
@@ -106,8 +106,8 @@ testCases               =   [1 2 3 4 5];
 % testCases               =   [5];
 % testCases               =   [4];
 % testCases             =   [ 5 ];
-% testCases             =   [ 1 3 4 ];
-testCases             =   [ 3 ];
+% testCases             =   [3];
+% testCases             =   [ 1 ];
 
 %% run the different tests
 for i                   =   testCases
