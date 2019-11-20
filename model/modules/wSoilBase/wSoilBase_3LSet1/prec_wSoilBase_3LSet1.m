4f77930024daa98c9fbb616d7c9cb15e3463aa6f
SujanKoirala <skoirala@bgc-jena.mpg.de>
2019-11-20 18:42:55 +0100

    wSoil pSoil integration: Iter 2

--- model/modules/wSoilBase/wSoilBase_3LSet1/prec_wSoilBase_3LSet1.m
+++ model/modules/wSoilBase/wSoilBase_3LSet1/prec_wSoilBase_3LSet1.m
@@ -1,4 +1,5 @@
-function [f,fe,fx,s,d,p] = prec_wSoilBase_1LSet1(f,fe,fx,s,d,p,info)
+function [f,fe,fx,s,d,p] = prec_wSoilBase_3LSet1(f,fe,fx,s,d,p,info)
+
 nSoilLayers            = info.tem.model.variables.states.w.nZix.wSoil;
 soilDepths             = p.wSoilBase.layerDepths;
 
@@ -9,7 +10,6 @@ fe.wSoilBase.soilDepths          = zeros(nSoilLayers,1);
 fe.wSoilBase.nsoilLayers        = nSoilLayers;
 
 for sl = 1:nSoilLayers
-fe.wSoilBase.soilDepths(sl,1) = soilDepths(sl);
+    fe.wSoilBase.soilDepths(sl,1) = soilDepths(sl);
 end
-
-end %function 
+end %function
