de0bd43e8ea316a41799533527d32acdc7d7cf27
dr-ko <skoirala@bgc-jena.mpg.de>
2019-12-04 09:16:06 +0100

    documentation of pSoil and wSoilBase

--- model/modules/pSoil/pSoil_Saxton1986/extraFunctions/calcSoilParamsSaxton1986.m
+++ model/modules/pSoil/pSoil_Saxton1986/extraFunctions/calcSoilParamsSaxton1986.m
@@ -1,40 +1,37 @@
 function [Alpha,Beta,K,Theta,Psi] = calcSoilParamsSaxton1986(s,p,info,sl,WT)
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-% calculate the soil hydraulic properties based on Saxton, 1986
-% 
+% calculates the soil hydraulic properties based on Saxton 1986
+%
 % Inputs:
-%   - p.pSoilm. :  parameters of the empirical fit
-%   - CLAY : clay array (from soilTexture)
-%   - SAND : sand array  (from soilTexture)
-%   - WT   : Psi : matric potential of soil water (kPa)
-%   -      : wilting point     : 'WP'      : WT = 1500
-%   -      : field capacity    : 'FC'      : WT = 33
-%   -      : saturation        : 'Sat'     : WT = 10
+%	- info  
+%	- s.wd.p_soilTexture_[CLAY/SAND]: is converted from fraction input to percentage 
+%   - p.pSoil.: texture-based parameters
 %
 % Outputs:
-%   - K,Psi,Theta @ FC, Sat, and WP
-%   - moisture retension parameters (Alpha and Beta)
+%   - properties of moisture-retention curves: (Alpha and Beta)
+%   - hydraulic conductivity (k), matric potention (psi) and porosity
+%   (theta) at saturation (Sat), field capacity (FC), and wilting point
+%   (WP)
 %
 % Modifies:
+% 	- None
 %
 % References:
-%   - Saxton, K.E., W.J. Rawls, J.S. Romberger, and R.I. Papendick. 1986. 
-%     Estimating generalized soil-water characteristics from texture. 
-%     Soil Sci. Soc. Am. J. Vol. 50(4):1031-1036.
-%     http://www.bsyse.wsu.edu/saxton/soilwater/Article.htm
-%
+%    - Saxton, K.E., W.J. Rawls, J.S. Romberger, and R.I. Papendick. 1986. 
+%       Estimating generalized soil-water characteristics from texture. 
+%       Soil Sci. Soc. Am. J. Vol. 50(4):1031-1036.
+%       http://www.bsyse.wsu.edu/saxton/soilwater/Article.htm
+% 
 % Created by:
 %   - Sujan Koirala (skoirala)
-%   - Nuno Carvalhais (ncarval)
+%   - Nuno Carvalhais (ncarval):
 %
 % Versions:
-%   - 1.0 on 18.11.2019 (skoirala): 
+%   - 1.0 on 22.11.2019 (skoirala):
 %
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 %%
-%--> get the number of soil layers
-
 %-->  CONVERT SAND AND CLAY TO PERCENTAGES
 CLAY            =   s.wd.p_soilTexture_CLAY(:,sl) .* 100;
 SAND            =   s.wd.p_soilTexture_SAND(:,sl) .* 100;
@@ -43,28 +40,25 @@ SAND            =   s.wd.p_soilTexture_SAND(:,sl) .* 100;
 A               =   exp(p.pSoil.a + p.pSoil.b .* CLAY + p.pSoil.c .* SAND .^ 2 + p.pSoil.d1 .* SAND .^ 2 .* CLAY) * 100;
 B               =   p.pSoil.e + p.pSoil.f1 .* CLAY .^ 2 + p.pSoil.g .* SAND .^ 2 .* CLAY;
 
-%-->  WATER POTENTIAL, Psi, kPa
+%-->  soil matric potential, Psi, kPa
 Psi             =   WT .* info.tem.helpers.arrays.onespix;
 
-%--> WATER CONTENT AT SATURATION (m^3/m^3)
+%--> soil moisture content at saturation (m^3/m^3)
 Theta_s         =   p.pSoil.h + p.pSoil.j .* SAND + p.pSoil.k .* log10(CLAY);
 
-%--> WATER POTENTIAL AT AIR ENTRY (kPa)
+%--> air entry pressure (kPa)
 Psi_e           =   100 .* (p.pSoil.m + p.pSoil.n .* Theta_s);
 
 Theta           =   zeros(size(CLAY));
 ndx     = find(Psi >= 10 & Psi <= 1500);
 if ~isempty(ndx)
-    % ---------------------------------------------------------------------
-    % Psi(ndx) = A(ndx) .* Theta(ndx) .^ B(ndx);
-    % ---------------------------------------------------------------------
     Theta(ndx)  =   (Psi(ndx) ./ A(ndx)) .^ (1 ./ B(ndx));
 end
 clear ndx
 
 ndx = find(Psi >= Psi_e & Psi < 10);
 if ~isempty(ndx)
-    % WATER CONTENT AT 10 kPa (m^3/m^3)
+    % theta at 10 kPa (m^3/m^3)
     Theta_10    =   exp((2.302 - log(A(ndx))) ./ B(ndx));
     % ---------------------------------------------------------------------
     % Psi(ndx) = 10.0 - (Theta(ndx) - Theta_10(ndx)) .* (10.0 - ...
@@ -81,7 +75,7 @@ if ~isempty(ndx)
 end
 clear ndx
 
-%--> hydraulic conductivity (mm/day)
+%--> hydraulic conductivity (mm/day): original equation for mm/s
 K   = 2.778E-6 .*(exp(p.pSoil.p + p.pSoil.q .* SAND + ...
     (p.pSoil.r + p.pSoil.t .* SAND + p.pSoil.u .* CLAY + p.pSoil.v .*...
     CLAY .^ 2) .* (1 ./ Theta))) .* 1000 * 3600 * 24;
--- model/modules/pSoil/pSoil_Saxton2006/extraFunctions/calcKSaxton2006.m
+++ model/modules/pSoil/pSoil_Saxton2006/extraFunctions/calcKSaxton2006.m
@@ -1,22 +1,28 @@
 function K = calcKSaxton2006(s,p,info,sl)
-% calculates the soil hydraulic conductivity for a given moisture content based on Saxton 2006
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% calculates the soil hydraulic conductivity for a given moisture based on Saxton, 2006
 %
 % Inputs:
-%	- info  
-%	- p.pSoil.sand, p.soilTexture.silt and other soil texture-based properties
+%	- s.w.wSoil(:,sl)  
+%	- s.wd.p_wSoilBase_[wSat/Beta/kSat]: hydraulic parameters for each soil layer
 %
 % Outputs:
-%   - properties of moisture-retention curves (Alpha and Beta)
-%   - hydraulic conductivity (k), matric potention (psi) and porosity
-%   (theta) at saturation (Sat), field capacity (FC), and wilting point
-%   (WP)
+%   - K: the hydraulic conductivity at unsaturated s.w.wSoil [in mm/day]
+%       - is calculated using original equation if info.tem.model.flags.useLookupK == 0
+%       - uses precomputed lookup table if info.tem.model.flags.useLookupK ==  1
 %
 % Modifies:
 % 	- None
 %
+% Notes:
+%   - This function is a part of pSoil, but making the looking up table and setting the soil
+%     properties is handled by wSoilBase (by calling this function)
+%   - is also used by all approaches depending on kUnsat within time loop of coreTEM
+%
 % References:
-%  - Saxton, K. E., & Rawls, W. J. (2006). Soil water characteristic estimates by texture and organic matter
-%      for hydrologic solutions. Soil science society of America Journal, 70(5), 1569-1578.
+%  - Saxton, K. E., & Rawls, W. J. (2006). Soil water characteristic estimates by 
+%       texture and organic matter for hydrologic solutions. 
+%       Soil science society of America Journal, 70(5), 1569-1578.
 % 
 % Created by:
 %   - Sujan Koirala (skoirala)
@@ -25,28 +31,27 @@ function K = calcKSaxton2006(s,p,info,sl)
 %   - 1.0 on 22.11.2019 (skoirala):
 %   - 1.1 on 03.12.2019 (skoirala): included the option to handle lookup table when set to true
 %     from modelRun.json
-%% 
+%
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+%% 
+%--> if useLookUp is set to true in modelRun.json, run the original non-linear equation
 wSat            =   s.wd.p_wSoilBase_wSat(:,sl);
 Theta_dos       =   s.w.wSoil(:,sl) ./ wSat;
 if p.wSoilBase.makeLookup
     Beta            =   s.wd.p_wSoilBase_Beta(:,sl);
     kSat            =   s.wd.p_wSoilBase_kSat(:,sl);
     lambda          =   1 ./ Beta;
-    % -------------------------------------------------------------------------
-    % WATER CONDUCTIVITY (mm/day)
     K               =   kSat .* ((Theta_dos) .^ (3 + (2 ./ lambda)));
-    % -------------------------------------------------------------------------
 else
-    rowArray                                    =   1:size(Theta_dos,1);
     Theta_dos(Theta_dos<0)                      =   0;
     Theta_dos(imag(Theta_dos)~=0)               =   0;
     lkDat                                       =   s.wd.p_wSoilBase_kLookUp{sl};
     lkInd                                       =   floor(Theta_dos .* p.wSoilBase.nLookup);
     lkInd(lkInd==0)                             =   1;
     lkInd(lkInd>p.wSoilBase.nLookup)            =   p.wSoilBase.nLookup;
-    idx                                         =   sub2ind(size(lkDat),rowArray',lkInd);
+    rowArray                                    =   1:size(Theta_dos,1);
+    idx                                         =   sub2ind(size(lkDat),rowArray',lkInd); %subscript for all rows and the selected columns
     K                                           =   lkDat(idx);        
 end
-
 end
