79f1738d9f9b868e793b7ab4c3982a6f5c8dd89e
dr-ko <skoirala@bgc-jena.mpg.de>
2019-12-06 06:46:40 +0100

    awcAct to unify plant available water calculation, and ambCO2 to diversify co2 concentrations

--- model/modules/tranSup/tranSup_wAWC/tranSup_wAWC.m
+++ model/modules/tranSup/tranSup_wAWC/tranSup_wAWC.m
@@ -5,7 +5,7 @@ function [f,fe,fx,s,d,p] = tranSup_wAWC(f,fe,fx,s,d,p,info,tix)
 %
 % Inputs:
 %   - s.w.wSoil : total soil moisture
-%   - s.wd.p_rootFrac_fracRoot2SoilD: extractable fraction of water
+%   - s.wd.awcAct: actual extractable water
 %   - s.wd.p_wSoilBase_wAWC: total maximum plant available water (FC-WP)
 %
 % Outputs:
@@ -26,6 +26,6 @@ function [f,fe,fx,s,d,p] = tranSup_wAWC(f,fe,fx,s,d,p,info,tix)
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 %%
-d.tranSup.tranSup(:,tix)     =   minsb(sum(s.wd.p_wSoilBase_wAWC .* s.wd.p_rootFrac_fracRoot2SoilD,2) .* p.tranSup.tranFrac,...
-                                    sum(s.w.wSoil .* s.wd.p_rootFrac_fracRoot2SoilD,2));
+d.tranSup.tranSup(:,tix)     =   minsb(sum(s.wd.awcAct,2) .* p.tranSup.tranFrac,...
+                                    sum(s.wd.p_wSoilBase_wAWC,2));
 end
