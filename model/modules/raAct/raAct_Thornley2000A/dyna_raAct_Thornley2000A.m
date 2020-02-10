0f13dddec6e6f9ccd77e4d01af5e47cde860c37d
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-02-10 16:26:04 +0100

    move C2Nveg param to CycleBase modules

--- model/modules/raAct/raAct_Thornley2000A/dyna_raAct_Thornley2000A.m
+++ model/modules/raAct/raAct_Thornley2000A/dyna_raAct_Thornley2000A.m
@@ -1,12 +1,25 @@
 function [f,fe,fx,s,d,p] = dyna_raAct_Thornley2000A(f,fe,fx,s,d,p,info,tix)
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-% FUNCTION    : dyna_raAct_Thornley2000A
-% 
-% PURPOSE    : estimate autotrophic respiration as maintenance + growth
+% Estimate autotrophic respiration as maintenance + growth
 % respiration according to Thornley and Cannell (2000): MODEL A -
 % maintenance respiration is given priority (check Fig.1 of the paper).
-% 
-% REFERENCES:
+%
+% Inputs:
+%   - fx.gpp:                substrate supply rate: Gross Primary Production (gC.m-2.deltaT-1)
+%   - s.c.cEco:              carbon pools (gC.m-2)
+%   - s.cd.cAlloc(:,zix):    carbon allocation in the different vegetation pools ([])
+%   - p.raAct.YG:            growth yield coefficient - or growth efficiency (gC.gC-1)
+%   - fe.raAct.km(ii).value: maintenance (respiration) coefficient - dependent on
+%                            temperature and, depending on the models, degradable fraction
+%                            (deltaT-1)  
+% Outputs:
+%   - s.cd.cEcoEfflux(:,zix): autotrophic respiration from each plant pools (gC.m-2.deltaT-1)
+%   - s.cd.cNPP:              net primary production for each plant pool (gC.m-2.deltaT-1)          
+%
+% Modifies:
+%  
+%
+% References:
 % Amthor, J. S. (2000), The McCree-de Wit-Penning de Vries-Thornley
 % respiration paradigms: 30 years later, Ann Bot-London, 86(1), 1-20. 
 % Ryan, M. G. (1991), Effects of Climate Change on Plant Respiration, Ecol
@@ -15,41 +28,23 @@ function [f,fe,fx,s,d,p] = dyna_raAct_Thornley2000A(f,fe,fx,s,d,p,info,tix)
 % Thornley, J. H. M., and M. G. R. Cannell (2000), Modelling the components
 % of plant respiration: Representation and realism, Ann Bot-London, 85(1),
 % 55-67.
-% 
-% CONTACT    : Nuno
-% 
-% INPUTS
-% gpp       : substrate supply rate: Gross Primary Production (gC.m-2.deltaT-1)
-%           (fx.gpp) 
-% cEco      : carbon pools (gC.m-2) 
-%           (s.c.cEco)
-% c2pool    : carbon allocation in the different vegetation pools ([])
-%           (s.cd.cAlloc(:,zix))
-% YG        : growth yield coefficient - or growth efficiency (gC.gC-1)
-%           (p.raAct.YG)
-% km        : maintenance (respiration) coefficient - dependent on
-%           temperature and, depending on the models, degradable fraction
-%           (deltaT-1)
-%           (fe.raAct.km(ii).value)
-% 
-% OUTPUTS
-% efflux    : autotrophic respiration from each plant pools (gC.m-2.deltaT-1)
-%           (s.cd.cEcoEfflux(:,zix))
-% cNpp      : net primary production for each plant pool (gC.m-2.deltaT-1)
-%           (s.cd.cNPP)
-% 
-% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-% 
+%
+% Created by:
+%   - Nuno Carvalhais (ncarval)
+%
+% Versions:
+%   - 1.0 on 06.02.2020 (sbesnard): cleaned up the code
+%
+% Notes :
 % Questions - practical - leave raAct per pool, or make a field fx.ra
 % that has all the autotrophic respiration components together?
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 % adjust nitrogen efficiency rate of maintenance respiration to the current
 % model time step
 %sujan RMN     = p.raAct.RMN ./ info.timeScale.stepsPerDay;
 RMN     = p.raAct.RMN ./ info.tem.model.time.nStepsDay;
-
 
 % compute maintenance and growth respiration terms for each vegetation pool
 % according to MODEL A - maintenance respiration is given priority
@@ -57,7 +52,7 @@ for zix = info.tem.model.variables.states.c.zix.cVeg
     
     % scalars of maintenance respiration for models A, B and C
     % km is the maintenance respiration coefficient (d-1)
-    s.cd.p_raAct_km(:,zix)    = 1 ./ s.cd.p_raAct_C2N(:,zix) .* RMN .* fe.rafTair.fT(:,tix);
+    s.cd.p_raAct_km(:,zix)    = 1 ./ s.cd.p_cCycleBase_C2Nveg(:,zix) .* RMN .* fe.rafTair.fT(:,tix);
     s.cd.p_raAct_km4su(:,zix)    = s.cd.p_raAct_km(:,zix) .* p.raAct.YG;
     
     % maintenance respiration first: R_m = km * C
