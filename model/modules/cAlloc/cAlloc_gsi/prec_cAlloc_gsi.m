05d9ee045676cd5d8ff4031e59852557938f6f27
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-02-10 16:30:12 +0100

    add cAlloc modules based on GSI index

---
+++ model/modules/cAlloc/cAlloc_gsi/prec_cAlloc_gsi.m
@@ -0,0 +1,9 @@
+function [f,fe,fx,s,d,p] = prec_cAlloc_gsi(f,fe,fx,s,d,p,info,tix)
+    % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    % FUNCTION	: prec_cAlloc_Dyn
+    
+    % s.cd.cAlloc=zeros(pix,zix);
+    s.cd.cAlloc     =   info.tem.helpers.arrays.zerospixzix.c.cEco; %sbesnard
+    end
+    
+    
