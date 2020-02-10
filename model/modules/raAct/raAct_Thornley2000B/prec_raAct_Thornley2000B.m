aaf021cc96069fb7928fb36cb33a088028653f1d
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-02-10 16:26:04 +0100

    move C2Nveg param to CycleBase modules

--- model/modules/raAct/raAct_Thornley2000B/prec_raAct_Thornley2000B.m
+++ model/modules/raAct/raAct_Thornley2000B/prec_raAct_Thornley2000B.m
@@ -1,10 +1,2 @@
 function [f,fe,fx,s,d,p] = prec_raAct_Thornley2000B(f,fe,fx,s,d,p,info)
-cpNames                 =   {'cVegRootF','cVegRootC','cVegWood','cVegLeaf'};
-s.cd.p_raAct_C2N        =   zeros(info.tem.helpers.sizes.nPix,numel(info.tem.model.variables.states.c.zix.cVeg)); %sujan
-
-for cpn = 1:numel(cpNames)
-    zix                 =   info.tem.model.variables.states.c.zix.(cpNames{cpn});
-    s.cd.p_raAct_C2N(:,zix)    =   p.raAct.C2N(cpn);
-end
-
 end
