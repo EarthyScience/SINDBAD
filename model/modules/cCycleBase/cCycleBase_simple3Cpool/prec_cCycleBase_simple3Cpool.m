3a94d56f582d9851ec71569a71a37303728956d5
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-02-10 16:26:04 +0100

    move C2Nveg param to CycleBase modules

--- model/modules/cCycleBase/cCycleBase_simple3Cpool/prec_cCycleBase_simple3Cpool.m
+++ model/modules/cCycleBase/cCycleBase_simple3Cpool/prec_cCycleBase_simple3Cpool.m
@@ -1,10 +1,11 @@
 function [f,fe,fx,s,d,p] = prec_cCycleBase_simple(f,fe,fx,s,d,p,info)
+
 %carbon to nitrogen ratio (gC.gN-1)
 s.cd.p_cCycleBase_C2Nveg        =   zeros(info.tem.helpers.sizes.nPix,numel(info.tem.model.variables.states.c.zix.cVeg)); %sujan
 for zix = info.tem.model.variables.states.c.zix.cVeg
     s.cd.p_cCycleBase_C2Nveg(:,zix)    =   p.cCycleBase.C2Nveg(zix);
 end
-
+    
 % annual turnover rates
 s.cd.p_cCycleBase_annk = reshape(repelem(p.cCycleBase.annk,info.tem.helpers.sizes.nPix),info.tem.helpers.sizes.nPix,info.tem.model.variables.states.c.nZix.cEco); %sujan
 
