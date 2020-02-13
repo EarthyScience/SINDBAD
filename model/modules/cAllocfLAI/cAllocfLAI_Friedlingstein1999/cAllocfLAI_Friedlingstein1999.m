0c1b7058b268d21cbf6d65d634185743f2fcbc88
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-02-13 14:59:06 +0100

    clean-up cAlloc and cFlow modules

--- model/modules/cAllocfLAI/cAllocfLAI_Friedlingstein1999/cAllocfLAI_Friedlingstein1999.m
+++ model/modules/cAllocfLAI/cAllocfLAI_Friedlingstein1999/cAllocfLAI_Friedlingstein1999.m
@@ -1,4 +1,31 @@
 function [f,fe,fx,s,d,p] = cAllocfLAI_Friedlingstein1999(f,fe,fx,s,d,p,info,tix)
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% compute the light limitation (LL) calculation
+%
+% Inputs:
+%   - s.cd.LAI: values for leaf area index
+%   - p.cAllocfLAI.minL: values for minimum resource availability (severely limited)
+%   - p.cAllocfLAI.maxL: values maximum resource availability (readily available)
+%   - p.cAllocfLAI.kext: values for light extinction coefficient
+%
+% Outputs:
+%   - d.cAllocfLAI.LL: values for light limitation
+%
+% Modifies:
+%   - d.cAllocfLAI.LL
+%
+% References:
+%   -  Friedlingstein, P., G. Joel, C.B. Field, and I.Y. Fung, 1999: Toward an allocation scheme for global terrestrial carbon models. Glob. Change Biol., 5, 755-770, doi:10.1046/j.1365-2486.1999.00269.x.
+%
+% Created by:
+%   - ncarvalhais
+%
+% Versions:
+%   - 1.0 on 12.01.2020 (sbesnard)
+%
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 % light limitation (LL) calculation
 LL                          = exp (-p.cAllocfLAI.kext .* s.cd.LAI); 
 LL(LL <= p.cAllocfLAI.minL) = p.cAllocfLAI.minL(LL <= p.cAllocfLAI.minL);
