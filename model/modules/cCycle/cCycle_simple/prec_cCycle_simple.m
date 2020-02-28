8cfad0bbbbf4590d1ac961e639be44004d0a7348
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-02-28 15:48:29 +0100

    clean-up cCycle module

--- model/modules/cCycle/cCycle_simple/prec_cCycle_simple.m
+++ model/modules/cCycle/cCycle_simple/prec_cCycle_simple.m
@@ -1,4 +1,29 @@
 function [f,fe,fx,s,d,p] = prec_cCycle_simple(f,fe,fx,s,d,p,info)
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    % CALCULATE DECAY RATES FOR THE ECOSYSTEM C POOLS AT APPROPRIATE TIME STEPS
+    %
+    % Inputs:
+    %   - info.tem.model.time.nStepsYear:   number of time steps per year
+    %   - s.cd.p_cCycleBase_annk:               carbon allocation matrix 
+    %
+    % Outputs:
+    %   - s.cd.p_cCycleBase_k:  decay rates for the carbon pool at each time step
+    %   - s.cd.cEcoEfflux:      
+    %
+    % Modifies:
+    %   - 
+    %
+    % References:
+    %
+    %
+    % Created by:
+    %   - ncarvalhais 
+    %
+    % Versions:
+    %   - 1.0 on 28.02.2020 (sbesnard)
+    %
+    % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 % NUMBER OF TIME STEPS PER YEAR
 TSPY                =   info.tem.model.time.nStepsYear;
 % s.prev.cTaufwSoil_fwSoil=info.tem.helpers.arrays.onespixzix.c.cEco; %sujan
