aaaf58250f786afefc3d112b6ae75a1d57b51617
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-02-28 16:08:23 +0100

    clean-up cCycleBase module

--- model/modules/cCycleBase/cCycleBase_simple/prec_cCycleBase_simple.m
+++ model/modules/cCycleBase/cCycleBase_simple/prec_cCycleBase_simple.m
@@ -1,4 +1,34 @@
 function [f,fe,fx,s,d,p] = prec_cCycleBase_simple(f,fe,fx,s,d,p,info)
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    % Compute carbon to nitrogen ratio and  annual turnover rates
+    %
+    % Inputs:
+    %   - p.cCycleBase.C2Nveg:            carbon to nitrogen ratio in vegetation pools
+    %   - p.cCycleBase.annk:              turnover rate of ecosystem carbon pools 
+    %   - p.cCycleBase.cFlowA:            Transfer matrix for carbon at ecosystem level 
+    % 
+    %
+    % Outputs:
+    %   - s.cd.p_cCycleBase_C2Nveg:    carbon to nitrogen ratio in vegetation pools
+    %   - s.cd.p_cCycleBase_annk:      turnover rate of ecosystem carbon pools  
+    %
+    % Modifies:
+    %   - 
+    %
+    % References:
+    %   - Potter, C. S., J. T. Randerson, C. B. Field, P. A. Matson, P. M.
+    %     Vitousek, H. A. Mooney, and S. A. Klooster. 1993.  Terrestrial ecosystem
+    %     production: A process model based on global satellite and surface data.
+    %     Global Biogeochemical Cycles. 7: 811-841.
+    %
+    % Created by:
+    %   - ncarvalhais 
+    %
+    % Versions:
+    %   - 1.0 on 28.02.2020 (sbesnard)
+    %
+    % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 %carbon to nitrogen ratio (gC.gN-1)
 s.cd.p_cCycleBase_C2Nveg        =   zeros(info.tem.helpers.sizes.nPix,numel(info.tem.model.variables.states.c.zix.cVeg)); %sujan
 for zix = info.tem.model.variables.states.c.zix.cVeg
