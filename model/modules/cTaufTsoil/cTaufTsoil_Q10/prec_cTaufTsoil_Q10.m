eeac7af82f7eb964f239d072fbf58272993fc97e
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-03-03 17:52:01 +0100

    clean-up cTaufTsoil

--- model/modules/cTaufTsoil/cTaufTsoil_Q10/prec_cTaufTsoil_Q10.m
+++ model/modules/cTaufTsoil/cTaufTsoil_Q10/prec_cTaufTsoil_Q10.m
@@ -1,18 +1,32 @@
 function [f,fe,fx,s,d,p] = prec_cTaufTsoil_Q10(f,fe,fx,s,d,p,info)
-% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-% FUNCTION    : 
-% 
-% PURPOSE    : 
-% 
-% REFERENCES:
-% 
-% CONTACT    : Nuno
-% 
-% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-% NOTE, WE NEED TO CHECK THIS CODE OUT! NORMALIZATION FOR TREF WHEN
-% OPTIMIZING Q10: NEEDED!!!
+    % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    % Compute effect of temperature on psoil carbon fluxes
+    %
+    % Inputs:
+    %   - f.Tair:            values for air temperature
+    %   - p.cTaufTsoil.Q10:  parameter for 
+    %   - p.cTaufTsoil.Tref: parameter for
+    %
+    % Outputs:
+    %   - fe.cTaufTsoil.fT: air temperature stressor on turnover rates (k)
+    %
+    % Modifies:
+    %   - 
+    %
+    % References:
+    %   - 
+    %
+    % Notes:
+    % - WE NEED TO CHECK THIS CODE OUT! NORMALIZATION FOR TREF WHEN
+    %   OPTIMIZING Q10: NEEDED!!!
+    %
+    % Created by:
+    %   - ncarvalhais
+    %
+    % Versions:
+    %   - 1.0 on 12.01.2020 (sbesnard)
+    %
+    % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 % TrefPap = 30;
 % if ~exist('deltaTref', 'var')
@@ -21,8 +35,6 @@ function [f,fe,fx,s,d,p] = prec_cTaufTsoil_Q10(f,fe,fx,s,d,p,info)
 % else
     TsM     = 1;
 % end
-
-
 
 % CALCULATE EFFECT OF TEMPERATURE ON psoil CARBON FLUXES
 fe.cTaufTsoil.fT    = p.cTaufTsoil.Q10 .^ ((f.Tair - p.cTaufTsoil.Tref) ./ 10) .* TsM; 
