fb67e82a675e5c64ba071a0b540e9abeb5773549
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-03-03 17:52:24 +0100

    clean-up cTaufpSoil

--- model/modules/cTaufpSoil/cTaufpSoil_CASA/prec_cTaufpSoil_CASA.m
+++ model/modules/cTaufpSoil/cTaufpSoil_CASA/prec_cTaufpSoil_CASA.m
@@ -1,6 +1,33 @@
 function [f,fe,fx,s,d,p] = prec_cTaufpSoil_CASA(f,fe,fx,s,d,p,info)
+    % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    % Compute soil texture effects on turnover rates (k) of cMicSoil
+    %
+    % Inputs:
+    %   - s.wd.p_wSoilBase_CLAY:    values for clay soil texture      
+    %   - s.wd.p_wSoilBase_SILT:    values for silt soil texture
+    %   - p.cTaufpSoil.TEXTEFFA:    parameter for the soil texture efficiency
+    %
+    % Outputs:
+    %   - s.cd.p_cTaufpSoil_kfSoil:  Soil texture stressor values on the the turnover rates 
+    %
+    % Modifies:
+    %   - 
+    %
+    % References:
+    %   - Potter, C. S., J. T. Randerson, C. B. Field, P. A. Matson, P. M.
+    %     Vitousek, H. A. Mooney, and S. A. Klooster. 1993.  Terrestrial ecosystem
+    %     production: A process model based on global satellite and surface data.
+    %     Global Biogeochemical Cycles. 7: 811-841. 
+    %
+    % Created by:
+    %   - ncarvalhais
+    %
+    % Versions:
+    %   - 1.0 on 12.01.2020 (sbesnard)
+    %
+    % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    %sujan: moving clay and silt from p.soilTexture to s.wd.p_wSoilBase.
+%sujan: moving clay and silt from p.soilTexture to s.wd.p_wSoilBase.
 CLAY                       =   mean(s.wd.p_wSoilBase_CLAY,2);
 SILT                       =   mean(s.wd.p_wSoilBase_SILT,2);
 
--- model/modules/cTaufpSoil/cTaufpSoil_none/prec_cTaufpSoil_none.m
+++ model/modules/cTaufpSoil/cTaufpSoil_none/prec_cTaufpSoil_none.m
@@ -1,5 +1,5 @@
 function [f,fe,fx,s,d,p] = prec_cTaufpSoil_none(f,fe,fx,s,d,p,info)
-% no effect = 1;
+% Set soil texture effects to ones;
 s.cd.p_cTaufpSoil_kfSoil         = info.tem.helpers.arrays.onespixzix.c.cEco;
 % (ineficient, should be pix zix_mic)
 end %function
