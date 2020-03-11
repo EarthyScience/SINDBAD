3ad5c49682bed9d263c28a0450e527907be35a48
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-03-11 09:40:25 +0100

    small fix gppfwSoil_keenan2009

--- model/modules/gppfwSoil/gppfwSoil_keenan2009/dyna_gppfwSoil_keenan2009.m
+++ model/modules/gppfwSoil/gppfwSoil_keenan2009/dyna_gppfwSoil_keenan2009.m
@@ -3,14 +3,14 @@ function [f,fe,fx,s,d,p] = dyna_gppfwSoil_keenan2009(f,fe,fx,s,d,p,info,tix)
     % calculate the soil moisture stress on gpp
     %
     % Inputs:
-    %   - s.w.wSoil(:,1):             values of soil moisture current time step for the 1st soil layer
-    %   - p.gppfwSoil.q:
-    %   - s.wd.p_wSoilBase_wSat(:,1): the soil water content for the 1st soil layer 
-    %                                 at which reductions in GPP are first evident
-    %   - s.wd.p_wSoilBase_wWP(:,1):  wilting point of the 1st soil layer
+    %   - s.w.wSoil:             values of soil moisture current time step
+    %   - p.gppfwSoil.q:         parameters for sensitivity of GPP to SM 
+    %   - s.wd.p_wSoilBase_wSat: the soil water content at which 
+    %                            reductions in GPP are first evident
+    %   - s.wd.p_wSoilBase_wWP:  wilting point
     %
     % Outputs:
-    %   - d.gppfwSoil.SMScGPP: soil moisture effect on GPP between 0-1
+    %   - d.gppfwSoil.SMScGPP:   soil moisture effect on GPP between 0-1
     %
     % Modifies:
     %   - 
@@ -32,7 +32,8 @@ function [f,fe,fx,s,d,p] = dyna_gppfwSoil_keenan2009(f,fe,fx,s,d,p,info,tix)
     %   - 1.0 on 10.03.2020 (sbesnard) 
     %
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    Smax  = s.wd.p_wSoilBase_wSat(:,1);
-    Smin  = s.wd.p_wSoilBase_wWP(:,1);
-    d.gppfwSoil.SMScGPP(:,tix) = min(max(((max(s.w.wSoil(:,1),Smin)-Smin)./(Smax-Smin)).^ p.gppfwSoil.q,0),1);  
+    Smax  = sum(s.wd.p_wSoilBase_wSat(:), 1);
+    Smin  = sum(s.wd.p_wSoilBase_wWP(:), 1);
+    SM    = sum(s.w.wSoil(:), 1);
+    d.gppfwSoil.SMScGPP(:,tix) = min(max(((max(SM,Smin)-Smin)./(Smax-Smin)).^ p.gppfwSoil.q,0),1);  
 end
