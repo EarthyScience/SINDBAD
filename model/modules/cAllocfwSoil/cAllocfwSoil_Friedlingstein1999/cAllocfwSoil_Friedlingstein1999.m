df17a9b5d7f4894a7eb949710783347ddac535b4
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-03-12 09:47:19 +0100

    fix Friedlingstein1999 variables names

--- model/modules/cAllocfwSoil/cAllocfwSoil_Friedlingstein1999/cAllocfwSoil_Friedlingstein1999.m
+++ model/modules/cAllocfwSoil/cAllocfwSoil_Friedlingstein1999/cAllocfwSoil_Friedlingstein1999.m
@@ -6,13 +6,13 @@ function [f,fe,fx,s,d,p] = cAllocfwSoil_Friedlingstein1999(f,fe,fx,s,d,p,info,ti
     % Inputs:
     %   - d.cTaufwSoil.fwSoil:       values for effect of moisture on soil decomposition
     %   - p.cAllocfwSoil.maxL_fW:    maximum limitation for the moisture function (for pseudo Nutrient Limitation estimations) 
-    %   - p.cAllocfwSoil.minL_fW:    minimum limitation for the moisture function (for pseudo Nutrient Limitation estimations)                
+    %   - p.cAllocfwSoil.mifW:    minimum limitation for the moisture function (for pseudo Nutrient Limitation estimations)                
     %
     % Outputs:
-    %   - d.cAllocfwSoil.NL_fW:      values for moisture stressor on C allocation  
+    %   - d.cAllocfwSoil.fW:      values for moisture stressor on C allocation  
     %
     % Modifies:
-    %   - d.cAllocfwSoil.NL_fW
+    %   - d.cAllocfwSoil.fW
     %
     % References:
     %   -  Friedlingstein, P., G. Joel, C.B. Field, and I.Y. Fung, 1999: Toward an allocation scheme for global terrestrial carbon models. Glob. Change Biol., 5, 755-770, doi:10.1046/j.1365-2486.1999.00269.x.
@@ -26,12 +26,12 @@ function [f,fe,fx,s,d,p] = cAllocfwSoil_Friedlingstein1999(f,fe,fx,s,d,p,info,ti
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 % computation for the moisture effect on decomposition/mineralization
-NL_fW                                   = d.cTaufwSoil.fwSoil(:,tix);
-NL_fW(NL_fW >= p.cAllocfwSoil.maxL_fW)  = p.cAllocfwSoil.maxL_fW;
-NL_fW(NL_fW <= p.cAllocfwSoil.minL_fW)    = p.cAllocfwSoil.minL_fW;
-% NL_fW(NL_fW >= p.cAllocfwSoil.maxL_fW)  = p.cAllocfwSoil.maxL_fW(NL_fW >=
+fW                                   = d.cTaufwSoil.fwSoil(:,tix);
+fW(fW >= p.cAllocfwSoil.maxL_fW)  = p.cAllocfwSoil.maxL_fW;
+fW(fW <= p.cAllocfwSoil.mifW)    = p.cAllocfwSoil.mifW;
+% fW(fW >= p.cAllocfwSoil.maxL_fW)  = p.cAllocfwSoil.maxL_fW(fW >=
 % p.cAllocfwSoil.maxL_fW); %sujan
-% NL_fW(NL_fW <= p.cAllocfwSoil.minL_fW)    = p.cAllocfwSoil.minL_fW(NL_fW <= p.cAllocfwSoil.minL_fW); %sujan
-d.cAllocfwSoil.NL_fW(:,tix)             = NL_fW;
+% fW(fW <= p.cAllocfwSoil.mifW)    = p.cAllocfwSoil.mifW(fW <= p.cAllocfwSoil.mifW); %sujan
+d.cAllocfwSoil.fW(:,tix)             = fW;
 
 end
