1dffba6f928df4b852d0fe6826e1ddd4f1f10135
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-03-12 09:52:32 +0100

    add references to gsi approaches

--- model/modules/cAlloc/cAlloc_gsi/prec_cAlloc_gsi.m
+++ model/modules/cAlloc/cAlloc_gsi/prec_cAlloc_gsi.m
@@ -1,10 +1,6 @@
 function [f,fe,fx,s,d,p] = prec_cAlloc_gsi(f,fe,fx,s,d,p,info,tix)
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    % set to 1 the 1st time step of the moisture, temperature and radiations effects on decomposition/mineralization
-    %d.cAlloc.fWfTfR(:,1) =  1;
-
-    % % set the allocation to zeros
-    % s.cd.cAlloc=zeros(pix,zix);
+    % set the allocation to zeros
     s.cd.cAlloc     =   info.tem.helpers.arrays.zerospixzix.c.cEco; %sbesnard
     end
     
