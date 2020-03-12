0fed4abcba9943354cd8619275634078e1b90859
sbesnard <sbesnard@bgc-jena.mpg.de>
2020-03-12 11:16:39 +0100

    clean-ups on cCycle checks

--- model/modules/cFlowAct/cFlowAct_CASA/prec_cFlowAct_CASA.m
+++ model/modules/cFlowAct/cFlowAct_CASA/prec_cFlowAct_CASA.m
@@ -54,13 +54,5 @@ s.cd.p_cFlowAct_A        = s.cd.p_cFlowAct_F .* s.cd.p_cFlowAct_E;
 [taker,giver]           = find(squeeze(sum(s.cd.p_cFlowAct_A > 0,1)) >= 1);
 s.cd.p_cFlowAct_taker    = taker;
 s.cd.p_cFlowAct_giver   = giver;
-% if there is flux order check that is consistent
-if ~isfield(p.cCycleBase,'fluxOrder')
-    p.cCycleBase.fluxOrder = 1:numel(taker);
-else
-    if numel(p.cCycleBase.fluxOrder) ~= numel(taker)
-        error(['ERR : cFlowAct_simple : '...
-            'numel(p.cCycleBase.fluxOrder) ~= numel(taker)'])
-    end
-end
+
 end %function
