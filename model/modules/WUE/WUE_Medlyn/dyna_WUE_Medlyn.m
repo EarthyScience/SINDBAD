edd4db7a1bdbc2f0959cd19df4680f478a6904dc
dr-ko <skoirala@bgc-jena.mpg.de>
2020-11-02 20:46:39 +0100

    fix the WUE approaches

--- model/modules/WUE/WUE_Medlyn/dyna_WUE_Medlyn.m
+++
@@ -1,6 +0,0 @@
-function [f,fe,fx,s,d,p] = dyna_WUE_Medlyn(f,fe,fx,s,d,p,info,tix)
-
-d.WUE.AoE(:,tix)            =   fe.WUE.AoENoCO2 .* s.cd.ambCO2; 
-d.WUE.ci(:,tix)             =   fe.WUE.ciNoCO2 .* s.cd.ambCO2; 
-
-end
