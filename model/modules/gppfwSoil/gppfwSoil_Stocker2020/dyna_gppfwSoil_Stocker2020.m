6359ea685cce3c7a201f89da9bf5c8d5433cdd5d
dr-ko <skoirala@bgc-jena.mpg.de>
2020-11-04 15:08:17 +0100

    changes in 5 pool ccycle model

--- model/modules/gppfwSoil/gppfwSoil_Stocker2020/dyna_gppfwSoil_Stocker2020.m
+++ model/modules/gppfwSoil/gppfwSoil_Stocker2020/dyna_gppfwSoil_Stocker2020.m
@@ -1,34 +1,35 @@
 function [f,fe,fx,s,d,p] = dyna_gppfwSoil_Stocker2020(f,fe,fx,s,d,p,info,tix)
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    % calculate the soil moisture stress on gpp
-    %
-    % Inputs:
-    %   - s.w.wSoil:             values of soil moisture current time step
-    %   - p.gppfwSoil.q:         parameters for sensitivity of GPP to SM 
-    %   - p.gppfwSoil.Smax
-    %   - p.gppfwSoil.Smin
-    %   - s.wd.p_wSoilBase_wWP:  wilting point
-    %
-    % Outputs:
-    %   - d.gppfwSoil.SMScGPP:   soil moisture effect on GPP between 0-1
-    %
-    % Modifies:
-    %   - 
-    %
-    % References:
-    %    - Stocker B D, Wang H, Smith N G, et al. P-model v1. 0: An optimality-based 
-%     light use efficiency model for simulating ecosystem gross primary production[J]. Geos$
+% calculate the soil moisture stress on gpp
+%
+% Inputs:
+%   - s.w.wSoil:             values of soil moisture current time step
+%   - p.gppfwSoil.q:         parameters for sensitivity of GPP to SM 
+%   - p.gppfwSoil.Smax
+%   - p.gppfwSoil.Smin
+%   - s.wd.p_wSoilBase_wWP:  wilting point
+%
+% Outputs:
+%   - d.gppfwSoil.SMScGPP:   soil moisture effect on GPP between 0-1
+%
+% Modifies:
+%   - 
+%
+% References:
+%    - Stocker B D, Wang H, Smith N G, et al. P-model v1. 0: An 
+%       optimality-based light use efficiency model for simulating ecosystem 
+%       gross primary production[J]. Geos
 % 
-    %
-    % Notes: 
-    %   - 
-    %
-    % Created by:
-    %   - Nuno Carvalhais (ncarval) and Simon Besnard (sbesnard)
-    %
-    % Versions:
-    %   - 1.0 on 10.03.2020 (sbesnard) 
-    %
+%
+% Notes: 
+%   - 
+%
+% Created by:
+%   - Nuno Carvalhais (ncarval) and Shanning Bao (sbao)
+%
+% Versions:
+%   - 1.1 on 03.11.2020 (skoirala): flipped the sing of p.gppfwSoil.q 
+%
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 SM      = sum(s.w.wSoil, 2);
@@ -38,7 +39,7 @@ maxAWC  = max(WFC - WP, 0);
 actAWC  = max(SM - WP, 0);
 SM_nor	= min(actAWC ./ maxAWC, 1);
 
-fW      = (p.gppfwSoil.q .* (SM_nor - p.gppfwSoil.thetastar) .^ 2 + 1) .*...
+fW      = (-p.gppfwSoil.q .* (SM_nor - p.gppfwSoil.thetastar) .^ 2 + 1) .*...
         (SM_nor <= p.gppfwSoil.thetastar)...
         + 1 .* (SM_nor > p.gppfwSoil.thetastar);
 
