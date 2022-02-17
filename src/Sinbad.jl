06e7c34a21ae823a052a747f0309b8a1f2fe2207
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-02-17 14:43:34 +0100

    first experiment with state update

--- src/Sinbad.jl
+++ src/Sinbad.jl
@@ -7,10 +7,14 @@ end
 
 export runEcosystem, evolveEcosystem
 export SnowFrac, rainSnow, snowMelt, run, getforcing
+export evapSoil, transpiration, updateState
 
 include("Ecosystem.jl")
 include("utils.jl")
 include("./models/rainSnow.jl")
 include("./models/snowMelt.jl")
+include("./models/evapSoil.jl")
+include("./models/transpiration.jl")
+include("./models/updateState.jl")
 
 end
