f6f91f71788653a488813d74ab5a08839b55f40c
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-02-17 14:43:34 +0100

    first experiment with state update
    Former-commit-id: 8d0d9ade7f9f93d8b4bd62339440c6ccb59e949b

--- src/models/snowMelt.jl
+++ src/models/snowMelt.jl
@@ -1,15 +1,18 @@
-@with_kw struct snowMelt{T} <: EarthEcosystem
-    melt_T::T = 3.0 # parametric type
-    melt_Rn::T = 2.0
+@with_kw struct snowMelt{type} <: EarthEcosystem
+    melt_T::type = 3.0 # parametric type
+    melt_Rn::type = 2.0
+    wSnow::type = 0.0
 end
 
 SnowFrac(snow) = snow > 0.0 ? 1.0 : 0.0
 
 function run(o::snowMelt, forcing, out)
     @unpack_snowMelt o
-    (; Rn, Tair), snow = forcing, out.snow
+    (; Rn, Tair) = forcing
+    (; snow, wSnow) = out
+
     potMelt = Tair * melt_T + maximum([0.0, Rn * melt_Rn])
     potMelt = Tair < 0.0 ? 0.0 : potMelt
-    wSnow = snow - minimum([snow, potMelt * SnowFrac(snow)])
-    return (; out..., potMelt, wSnow)
+    FracSnowMelt = minimum([1, (potMelt * SnowFrac(snow)) / wSnow])
+    return (; out..., potMelt, FracSnowMelt, wSnow)
 end
