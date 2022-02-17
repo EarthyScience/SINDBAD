dd2085f4e54e63b2689fcbb318598d719afc6c35
dr-ko <skoirala@bgc-jena.mpg.de>
2022-02-17 14:43:34 +0100

    first experiment with state update

---
+++ src/models/updateState.jl
@@ -0,0 +1,14 @@
+@with_kw struct updateState{type} <: EarthEcosystem
+    calcFlux :: type = true
+end
+
+function run(o::updateState, forcing, out)
+    @unpack_updateState o # repetition
+    (; fracTranspiration, fracEvapSoil, FracSnowMelt, rain, snow, wSnow, wSoil) = out
+    snowMelt = FracSnowMelt * wSnow
+    transpiration = fracTranspiration * wSoil
+    evapSoil = fracEvapSoil * wSoil
+    wSnow = wSnow + snow - FracSnowMelt * wSnow
+    wSoil = wSoil + rain + snowMelt - evapSoil - transpiration
+    return (; out..., wSnow, wSoil, snowMelt, evapSoil, transpiration)
+end
