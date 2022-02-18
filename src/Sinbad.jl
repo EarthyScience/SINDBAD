5d9cd5920a2200293eb68aca93c0426eb006d6fd
dr-ko <skoirala@bgc-jena.mpg.de>
2022-02-18 18:32:36 +0100

    re-organize models

--- src/Sinbad.jl
+++ src/Sinbad.jl
@@ -7,14 +7,27 @@ end
 
 export runEcosystem, evolveEcosystem
 export SnowFrac, rainSnow, snowMelt, run, getforcing
-export evapSoil, transpiration, updateState
+export evapSoil, transpiration, updateState, getStates
 
-include("Ecosystem.jl")
-include("utils.jl")
-include("./models/rainSnow.jl")
-include("./models/snowMelt.jl")
-include("./models/evapSoil.jl")
-include("./models/transpiration.jl")
-include("./models/updateState.jl")
+include("tem/Ecosystem.jl")
+include("tools/utils.jl")
+
+
+### the following should come from the model structure json
+include("models/getStates/getStates_simple.jl")
+include("models/rainSnow/rainSnow_Tair.jl")
+include("models/snowMelt/snowMelt_snowFrac.jl")
+include("models/evapSoil/evapSoil_demSup2.jl")
+include("models/transpiration/transpiration_demSup.jl")
+include("models/updateState/updateState_wSimple.jl")
+
+
+
+# include("../tools/utils.jl")
+# include("../models/rainSnow/rainSnow_Tair.jl")
+# include("../models/snowMelt/snowMelt_snowFrac.jl")
+# include("../models/evapSoil/evapSoil_demSup.jl")
+# include("../models/transpiration/transpiration_demSup.jl")
+# include("../models/updateState/updateState_wSimple.jl")
 
 end
