a619e54c6b58b160b8173f0392b7a29e63b5627c
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-02-18 18:32:36 +0100

    re-organize models
    Former-commit-id: 5ed277d34c886148830727911aad8b54c6065571

---
+++ src/models/rainSnow/rainSnow_Tair.jl
@@ -0,0 +1,22 @@
+@with_kw struct rainSnow{type} <: EarthEcosystem
+    Tair_thres::type = 0.5 # parametric
+    para_a::type = 0.5
+    para_b::type = 0.5
+    Tair_thres_bounds = [0.1, 0.7] # parametric
+end
+
+function run(o::rainSnow, forcing, out)
+    @unpack_rainSnow o # repetition
+    (; Tair, rain) = forcing
+    # if Tair < Tair_thres
+    #     snow = rain
+    #     rain = 0.0
+    # else
+    #     snow = 0.0
+    # end
+    snow = Tair < Tair_thres ? rain : 0.0
+    rain = Tair >= Tair_thres ? rain : 0.0
+    # rain = rain - snow
+    precip = rain + snow
+    return (; out..., Tair, snow, rain, precip)
+end
--- src/models/snowMelt/snowMelt_snowFrac.jl
+++ src/models/snowMelt/snowMelt_snowFrac.jl
@@ -1,7 +1,6 @@
 @with_kw struct snowMelt{type} <: EarthEcosystem
     melt_T::type = 3.0 # parametric type
     melt_Rn::type = 2.0
-    wSnow::type = 0.0
 end
 
 SnowFrac(snow) = snow > 0.0 ? 1.0 : 0.0
@@ -13,6 +12,6 @@ function run(o::snowMelt, forcing, out)
 
     potMelt = Tair * melt_T + maximum([0.0, Rn * melt_Rn])
     potMelt = Tair < 0.0 ? 0.0 : potMelt
-    FracSnowMelt = minimum([1, (potMelt * SnowFrac(snow)) / wSnow])
-    return (; out..., potMelt, FracSnowMelt, wSnow)
+    fracSnowMelt = minimum([1, (potMelt * SnowFrac(snow)) / wSnow])
+    return (; out..., potMelt, fracSnowMelt)
 end
