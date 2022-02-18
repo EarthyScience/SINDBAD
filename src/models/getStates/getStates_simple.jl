2d138a8f478eab7ae4e7ed6d7549f2e7b7f2f2e0
dr-ko <skoirala@bgc-jena.mpg.de>
2022-02-18 18:32:36 +0100

    re-organize models
    Former-commit-id: 5ed277d34c886148830727911aad8b54c6065571

---
+++ src/models/getStates/getStates_simple.jl
@@ -0,0 +1,11 @@
+@with_kw struct getStates{type} <: EarthEcosystem
+    updateState::type = true
+    # wSoil::type = 0.0
+    # wSnow::type = 0.0
+end
+
+function run(o::getStates, forcing, out)
+    @unpack_getStates o
+    (; wSoil, wSnow) = out
+    return (; out..., wSoil, wSnow)
+end
