3dcafc5f64081e56f9a545bc0827c7da51b848a8
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-02-18 11:46:36 +0100

    use makie for plotting
    Former-commit-id: 89044caa2995261c1f3e1bd4ff972aaaea9770c1

--- src/workflowExperiment.jl
+++ src/workflowExperiment.jl
@@ -1,6 +1,7 @@
 using Revise
 using Sinbad
 using Test
+using GLMakie
 
 m1 = rainSnow()
 m2 = snowMelt()
@@ -11,10 +12,21 @@ models = (m1, m2, m3, m4, m5)
 forcing, timesteps = getforcing()
 outTable = evolveEcosystem(forcing, models, timesteps)
 
-vname=:wSnow
-plot(@eval outTable.$(vname))
+#vname=:wSoil
+#vname=:wSoil
+#plot(@eval outTable.$(vname))
+plotResults(outTable)
 
-using GR
-for vname in propertynames(outTable)
-    plot(@eval outTable.$(vname))
+function plotResults(outTable, startTime=1, endTime=365)
+    fig = Figure(resolution = (2200, 900))
+    axs = [Axis(fig[i,j]) for i in 1:3 for j in 1:6]
+    for (i, vname) in enumerate(propertynames(outTable))
+        lines!(axs[i], @eval outTable.$(vname))
+        axs[i].title=string(vname)
+        xlims!(axs[i], startTime, endTime)
+    end
+    fig
 end
+# for vname in propertynames(outTable)
+#     plot(@eval outTable.$(vname))
+# end
