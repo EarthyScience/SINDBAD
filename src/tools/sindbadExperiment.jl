3b456b440c84e76024bc5b88259d80e17458c844
dr-ko <skoirala@bgc-jena.mpg.de>
2022-02-24 07:49:58 +0100

    unit conversion of forcing

--- src/tools/sindbadExperiment.jl
+++ src/tools/sindbadExperiment.jl
@@ -14,29 +14,39 @@ forcing = getForcing(info)
 
 ## run TEM => optimization or forward run
 timesteps = size(forcing)[1]
-models = info.tem.models
+m1 = rainSnow()
+m2 = snowMelt()
+m3 = evapSoil()
+m4 = transpiration()
+m5 = updateState()
+models = (m1, m2, m3, m4, m5)
+outTable = evolveEcosystem(forcing, models, timesteps)
 
-outTable = evolveEcosystem(forcing, models, timesteps) # evolve is intransitive, may be use update?
+# models = info.tem.models
+
+# outTable = evolveEcosystem(forcing, models, timesteps) # evolve is intransitive, may be use update?
 
 
-function runExperiment(exp_file, )
-end
-
-## collect data and post process
-"""
-# using GLMakie
-# function plotResults(outTable, startTime=1, endTime=365)
-#     fig = Figure(resolution = (2200, 900))
-#     axs = [Axis(fig[i,j]) for i in 1:3 for j in 1:6]
-#     for (i, vname) in enumerate(propertynames(outTable))
-#         lines!(axs[i], @eval outTable.$(vname))
-#         axs[i].title=string(vname)
-#         xlims!(axs[i], startTime, endTime)
-#     end
-#     fig
+# function runExperiment(exp_file, )
 # end
 
-# plotResults(outTable)
+## collect data and post process
+
+using GLMakie
+function plotResults(outTable; startTime=1, endTime=365)
+    fig = Figure(resolution = (2200, 900))
+    axs = [Axis(fig[i,j]) for i in 1:3 for j in 1:6]
+    for (i, vname) in enumerate(propertynames(outTable))
+        lines!(axs[i], @eval outTable.$(vname))
+        axs[i].title=string(vname)
+        xlims!(axs[i], startTime, endTime)
+    end
+    fig
+end
+
+endTime=3000
+plotResults(outTable; startTime=1,endTime=endTime)
+"""
 # selModels = propertynames(info.modelStructure.modules)
 # corePath = joinpath(pwd(), info.modelStructure.paths.coreTEM)
 # (; info.modelStructure.paths.coreTEM = corePath)
