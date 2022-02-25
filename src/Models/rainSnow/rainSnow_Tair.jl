f660571e691d190ce2bb55ddf4b6a40de5c6d3ea
dr-ko <skoirala@bgc-jena.mpg.de>
2022-02-26 00:24:56 +0100

    add optimization function, and the first run
    Former-commit-id: 14f5d251755d8f655c56159e1b0d56897530b632

--- src/Models/rainSnow/rainSnow_Tair.jl
+++ src/Models/rainSnow/rainSnow_Tair.jl
@@ -1,14 +1,11 @@
 export rainSnow_Tair
 
 @with_kw struct rainSnow_Tair{type} <: LandEcosystem
-    Tair_thres::type = 0.5 # parametric
-    para_a::type = 0.5
-    para_b::type = 0.5
-    Tair_thres_bounds = [0.1, 0.7] # parametric
+    Tair_thres::type = 0.5
 end
 
 function compute(o::rainSnow_Tair, forcing, out)
-    @unpack_rainSnow_Tair o # repetition
+    @unpack_rainSnow_Tair o
     (; Tair, rain) = forcing
     snow = Tair < Tair_thres ? rain : 0.0
     rain = Tair >= Tair_thres ? rain : 0.0
