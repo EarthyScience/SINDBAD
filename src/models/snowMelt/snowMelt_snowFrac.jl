f660a756c3be6b0193db1b31c9fcbe2202059541
SujanKoirala <skoirala@bgc-jena.mpg.de>
2022-03-24 13:50:04 +0100

    separate structs

--- src/models/snowMelt/snowMelt_snowFrac.jl
+++ src/models/snowMelt/snowMelt_snowFrac.jl
@@ -7,23 +7,25 @@ end
 
 SnowFrac(snow) = snow > 0.0 ? 1.0 : 0.0
 
-function compute(o::snowMelt_snowFrac, forcing, out)
+function compute(o::snowMelt_snowFrac, forcing, diagflux, states, info)
     @unpack_snowMelt_snowFrac o
     (; Rn, Tair) = forcing
-    (; snow, wSnow, WBP) = out
+    (; snow, WBP) = diagflux
+    (; wSnow) = states
 
     potMelt = Tair * melt_T + max(0.0, Rn * melt_Rn)
     potMelt = Tair < 0.0 ? 0.0 : potMelt
     fracSnowMelt = min(1, (potMelt * SnowFrac(snow)) / sum(wSnow[:, 1]))
     snowMelt = fracSnowMelt * sum(wSnow[:, 1])
     WBP = WBP + snowMelt
-    return (; out..., potMelt, snowMelt, fracSnowMelt)
+    return (; diagflux..., potMelt, snowMelt, fracSnowMelt)
 end
 
-function update(o::snowMelt_snowFrac, forcing, out)
-    (; fracSnowMelt, snow, wSnow) = out
+function update(o::snowMelt_snowFrac, forcing, diagflux, states, info)
+    (; fracSnowMelt, snow) = diagflux
+    (; wSnow) = states
     snowMelt = fracSnowMelt * sum(wSnow[:, 1])
     @show "update", fracSnowMelt, snowMelt, wSnow[1]
     wSnow[1] = wSnow[1] + snow - snowMelt
-    return (; out..., wSnow, snowMelt)
+    return (; diagflux..., snowMelt; states..., wSnow)
 end
