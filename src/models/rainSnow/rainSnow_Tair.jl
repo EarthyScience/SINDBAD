65b40c99c784bc4ce27f86324018cbfbcf93ed24
SujanKoirala <skoirala@bgc-jena.mpg.de>
2022-03-24 13:50:04 +0100

    separate structs
    Former-commit-id: 2c19066cf74d61408ed02480117ab46d71642933

--- src/models/rainSnow/rainSnow_Tair.jl
+++ src/models/rainSnow/rainSnow_Tair.jl
@@ -4,16 +4,16 @@ export rainSnow_Tair
     Tair_thres::T = 0.5
 end
 
-function compute(o::rainSnow_Tair, forcing, out)
+function compute(o::rainSnow_Tair, forcing, diagflux, states, info)
     @unpack_rainSnow_Tair o
     (; Tair, rain) = forcing
     snow = Tair < Tair_thres ? rain : 0.0
     rain = Tair >= Tair_thres ? rain : 0.0
     # rain = rain - snow
     precip = rain + snow
-    return (; out..., Tair, snow, rain, precip)
+    return (; diagflux..., snow, rain, precip)
 end
 
-function update(o::rainSnow_Tair, forcing, out)
-    return out
+function update(o::rainSnow_Tair, forcing, diagflux, states, info)
+    return (diagflux, states)
 end
