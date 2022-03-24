a5619fcdff580d821a37a620b94cbcc90c5cb1be
dr-ko <skoirala@bgc-jena.mpg.de>
2022-03-24 17:46:18 +0100

    subfields of out
    Former-commit-id: 9d15ac60bec50dd99dbac331d66d4061ca84ab5f

--- src/Ecosystem/runEcosystem.jl
+++ src/Ecosystem/runEcosystem.jl
@@ -1,34 +1,37 @@
 """
-runModels(forcing, models, diagflux, states, info)
+runModels(forcing, models, out, info)
 """
-function runModels(forcing, models, diagflux, states, info)
-    for m in models
-        out = Models.compute(m, forcing, diagflux, states, info)
-        # out = Models.update(m, forcing, diagflux, states, info)
+function runModels(forcing, models, out, info)
+    for model in models
+        out = Models.compute(model, forcing, out, info)
+        out = Models.update(model, forcing, out, info)
+        # @show out
+        # out = Models.update(m, forcing, out, info)
     end
     return out
 end
 
 """
-runForward(selectedModels, forcing, diagflux, states, info)
+runForward(selectedModels, forcing, out, info)
 """
-function runForward(selectedModels, forcing, diagflux, states, info)
-    outtuples = [runModels(forcing[t], selectedModels, diagflux, states, info) for t in 1:size(forcing, 1)]
+function runForward(selectedModels, forcing, out, info)
+    outtuples = [runModels(forcing[t], selectedModels, out, info) for t in 1:size(forcing, 1)]
     return columntable(outtuples)
 end
 
 """
 runSpinup(selectedModels, initStates, forcing, history=false; nspins=3)
 """
-function runSpinup(selectedModels, initStates, forcing, history=false; nspins=3)
-    out = initStates
+function runSpinup(selectedModels, initStates, forcing, info, history=false; nspins=3)
+    out=(; states=(;), diagnostics=(;), fluxes=(;))
+    out = (; out..., states = (; out.states..., initStates...))
     tsteps = size(forcing, 1)
-    spinuplog = history ? [values(diagflux, states, info)[1:length(initStates)]] : nothing
+    spinuplog = history ? [values(out)[1:length(initStates)]] : nothing
     for j in 1:nspins
         for t in 1:tsteps
-            out = runModels(forcing[t], selectedModels, diagflux, states, info)
+            out = runModels(forcing[t], selectedModels, out, info)
             if history
-                push!(spinuplog, values(diagflux, states, info)[1:length(initStates)])
+                push!(spinuplog, values(out)[1:length(initStates)])
             end
         end
     end
@@ -38,7 +41,7 @@ end
 """
 runEcosystem(selectedModels, initStates, forcing, history=false; nspins=3) # forward run
 """
-function runEcosystem(selectedModels, initStates, forcing, history=false; nspins=3) # forward run
-    out, outlog = runSpinup(selectedModels, initStates, forcing, history; nspins=nspins)
-    return runForward(selectedModels, forcing, diagflux, states, info)
+function runEcosystem(selectedModels, initStates, forcing, info, history=false; nspins=3) # forward run
+    out, outlog = runSpinup(selectedModels, initStates, forcing, info, history; nspins=nspins)
+    return runForward(selectedModels, forcing, out, info)
 end
--- src/models/rainSnow/rainSnow_Tair.jl
+++ src/models/rainSnow/rainSnow_Tair.jl
@@ -4,16 +4,16 @@ export rainSnow_Tair
     Tair_thres::T = 0.5
 end
 
-function compute(o::rainSnow_Tair, forcing, diagflux, states, info)
+function compute(o::rainSnow_Tair, forcing, out, info)
     @unpack_rainSnow_Tair o
     (; Tair, rain) = forcing
     snow = Tair < Tair_thres ? rain : 0.0
     rain = Tair >= Tair_thres ? rain : 0.0
-    # rain = rain - snow
     precip = rain + snow
-    return (; diagflux..., snow, rain, precip)
+    out = (; out..., fluxes = (; out.fluxes..., rain, snow, precip))
+    return out
 end
 
-function update(o::rainSnow_Tair, forcing, diagflux, states, info)
-    return (diagflux, states)
+function update(o::rainSnow_Tair, forcing, out, info)
+    return out
 end
