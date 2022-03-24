ed2ca8c1a515519bbfd5ab8d89595592435cae30
dr-ko <skoirala@bgc-jena.mpg.de>
2022-03-24 17:46:18 +0100

    subfields of out

--- src/models/roSat/roSat_Bergstroem.jl
+++ src/models/roSat/roSat_Bergstroem.jl
@@ -5,20 +5,25 @@ export roSat_Bergstroem
     s_max::T2 = 1000.0
 end
 
-function compute(o::roSat_Bergstroem, forcing, diagflux, states, info)
+function compute(o::roSat_Bergstroem, forcing, out, info)
     @unpack_roSat_Bergstroem o
-    (; wSoil) = states
-    #fracRoSat = (sum(wSoil[:,1]) / s_max)^β
+    (; wSoil) = out.states
+    (; WBP) = out.diagnostics
+    # fracRoSat = clamp((sum(wSoil[:,1]) / s_max)^β, 0, 1)
     fracRoSat = 0.2
-    #fracRoSat = fracRoSat < 0.0 ? 0.0 : fracRoSat > 1.0 ? 1.0 : fracRoSat
-    return (; diagflux..., fracRoSat; states)
-end
-
-function update(o::roSat_Bergstroem, forcing, diagflux, states, info)
-    (; fracRoSat, WBP) = diagflux
-    (; wSoil) = states
     roSat = fracRoSat * WBP
     WBP = WBP - roSat
+    out = (; out..., diagnostics = (; out.diagnostics..., WBP, fracRoSat))
+    out = (; out..., fluxes = (; out.fluxes..., roSat))
+    return out
+end
+
+function update(o::roSat_Bergstroem, forcing, out, info)
+    (; WBP) = out.diagnostics
+    (; wSoil) = out.states
     wSoil[1] = wSoil[1] + WBP
-    return (; diagflux..., roSat, WBP; states..., wSoil)
+    WBP = 0.0
+    out = (; out..., states = (; out.states..., wSoil))
+    out = (; out..., diagnostics = (; out.diagnostics..., WBP))
+    return out
 end
