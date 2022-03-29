5fb6d56d693ba111dfd9b04793e3fa98862b4d1a
dr-ko <skoirala@bgc-jena.mpg.de>
2022-03-29 16:52:05 +0200

    no info
    Former-commit-id: 74de954575c54152e22743404dfba60d7a3dac0b

--- src/Ecosystem/runEcosystem.jl
+++ src/Ecosystem/runEcosystem.jl
@@ -1,35 +1,43 @@
 """
-runModels(forcing, models, out, info)
+runModels(forcing, models, out)
 """
-function runModels(forcing, models, out, info)
+function runModels(forcing, models, out)
     for model in models
-        out = Models.compute(model, forcing, out, info)
-        out = Models.update(model, forcing, out, info)
+        out = Models.compute(model, forcing, out)
+        out = Models.update(model, forcing, out)
         # @show out
-        # out = Models.update(m, forcing, out, info)
+        # out = Models.update(m, forcing, out)
     end
     return out
 end
 
 """
-runForward(selectedModels, forcing, out, info)
+runForward(selectedModels, forcing, out)
 """
-function runForward(selectedModels, forcing, out, info)
-    outtuples = [runModels(forcing[t], selectedModels, out, info) for t in 1:size(forcing, 1)]
-    return columntable(outtuples)
+function runForward(selectedModels, forcing, out, modelnames)
+    tsteps = size(forcing, 1)
+    outtemp = []
+    modelnames = tuple(modelnames...)
+    for t in 1:tsteps
+        out = runModels(forcing[t], selectedModels, out)
+        push!(outtemp, NamedTuple{modelnames}(out.fluxes))
+    end
+    #outtuples = [runModels(forcing[t], selectedModels, out, info) for t in 1:size(forcing, 1)]
+    return columntable(outtemp)
 end
+
 
 """
 runSpinup(selectedModels, initStates, forcing, history=false; nspins=3)
 """
-function runSpinup(selectedModels, initStates, forcing, info, history=false; nspins=3)
+function runSpinup(selectedModels, initStates, forcing, history=false; nspins=3)
     out=(; states=(;), diagnostics=(;), fluxes=(;))
     out = (; out..., states = (; out.states..., initStates...))
     tsteps = size(forcing, 1)
     spinuplog = history ? [values(out)[1:length(initStates)]] : nothing
     for j in 1:nspins
         for t in 1:tsteps
-            out = runModels(forcing[t], selectedModels, out, info)
+            out = runModels(forcing[t], selectedModels, out)
             if history
                 push!(spinuplog, values(out)[1:length(initStates)])
             end
@@ -41,7 +49,7 @@ end
 """
 runEcosystem(selectedModels, initStates, forcing, history=false; nspins=3) # forward run
 """
-function runEcosystem(selectedModels, initStates, forcing, info, history=false; nspins=3) # forward run
-    out, outlog = runSpinup(selectedModels, initStates, forcing, info, history; nspins=nspins)
-    return runForward(selectedModels, forcing, out, info)
+function runEcosystem(selectedModels, initStates, forcing, modelnames, history=false; nspins=3) # forward run
+    out, outlog = runSpinup(selectedModels, initStates, forcing, history; nspins=nspins)
+    return runForward(selectedModels, forcing, out, modelnames)
 end
