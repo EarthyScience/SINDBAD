50621d482d8a68cbbf3ba6fb507209fbf8b9703f
dr-ko <skoirala@bgc-jena.mpg.de>
2022-03-30 16:15:26 +0200

    pass modelInfo to models

--- src/models/sumVariables/sumVariables_simple.jl
+++ src/models/sumVariables/sumVariables_simple.jl
@@ -2,18 +2,17 @@
     doSum::T = true
 end
 
-function compute(o::sumVariables_simple, forcing, out)
+function setsubfield(out, varname = :fluxes, vals = (:a, 1))
+    return @eval (; $out..., $varname = (; $out.$varname...,$(vals[1]) = $vals[2]))
+end
+
+function compute(o::sumVariables_simple, forcing, out, modelInfo)
     @unpack_sumVariables_simple o
-
-    function setsubfield(out, varname = :fluxes, vals = (:a, 1))
-        return @eval (; $out..., $varname = (; $out.$varname...,$(vals[1]) = $vals[2]))
-    end
-
-    vars2sum = info.modelRun.varsToSum
-    tarr=propertynames(vars2sum)
+    variablestosum = modelInfo.compute.sum
+    tarr=propertynames(variablestosum)
     for tarname in tarr
-        comps = Symbol.(getfield(vars2sum, tarname).components)
-        outfield = Symbol.(getfield(vars2sum, tarname).outfield)
+        comps = getfield(variablestosum, tarname).components
+        outfield = getfield(variablestosum, tarname).fieldname
         datasubfields = getfield(out, outfield)
         dat = sum([getfield(datasubfields, compname) for compname in comps if compname in propertynames(datasubfields)])
         out = setsubfield(out, outfield, (tarname, dat))
@@ -21,7 +20,7 @@ function compute(o::sumVariables_simple, forcing, out)
     return out
 end
 
-function update(o::sumVariables_simple, forcing, out)
+function update(o::sumVariables_simple, forcing, out, modelInfo)
     return out
 end
 
