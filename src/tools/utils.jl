0f565ddd8e2ed8251ccabae868a484c265607377
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-04-08 01:45:50 +0200

    paramfields for doc
    Former-commit-id: 939354ee453fd166751a1a0e62c1b2e09e1c3ba2

--- src/tools/utils.jl
+++ src/tools/utils.jl
@@ -1,3 +1,4 @@
+export PARAMFIELDS
 function tuple2table(dTuple; colNames=nothing)
     tpNames = propertynames(dTuple)
     tpValues = values(dTuple)
@@ -50,4 +51,40 @@ end
 
 function setTupleField(out, vals = (:a, 1))
     return @eval (; $out..., $(vals[1]) = $vals[2])
+end
+
+struct BoundFields <: DocStringExtensions.Abbreviation
+types::Bool
+end
+const PARAMFIELDS = BoundFields(false)
+
+function DocStringExtensions.format(abbrv::BoundFields, buf, doc)
+    local docs = get(doc.data, :fields, Dict())
+    local binding = doc.data[:binding]
+    local object = Docs.resolve(binding)
+    local fields = isabstracttype(object) ? Symbol[] : fieldnames(object)
+    if !isempty(fields)
+
+        println(buf)
+        for field in fields
+            println(buf, describe(object, field))
+            if abbrv.types
+                println(buf, "  - `", field, "::", fieldtype(object, field), "`")
+            else
+                println(buf, "  - `", field, "=",
+                bounds(object, field),"`")
+            end
+            println(buf)
+            if haskey(docs, field) && isa(docs[field], AbstractString)
+                println(buf)
+                println(docs[field])
+                for line in split(docs[field], ": ")
+                    println(buf, isempty(line) ? "" : "    ", rstrip(line))
+                end
+            end
+            println(buf)
+        end
+        println(buf)
+    end
+    return nothing
 end
