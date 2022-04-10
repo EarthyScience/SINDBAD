6bc3e0d7571cc0e7cd45dfaee50f68c7fa403663
dr-ko <skoirala@bgc-jena.mpg.de>
2022-04-10 14:36:07 +0200

    rename out to land

--- src/models/roSat/roSat_Bergstroem.jl
+++ src/models/roSat/roSat_Bergstroem.jl
@@ -5,37 +5,37 @@ export roSat_Bergstroem
     s_max::T2 = 1000.0 | (100.0, 5000.0) | "maximum storage for calculating relative wetness" | "mm"
 end
 
-function compute(o::roSat_Bergstroem, forcing, out, modelInfo)
+function compute(o::roSat_Bergstroem, forcing, land, infotem)
     @unpack_roSat_Bergstroem o
     @unpack_land begin
-        wSoil ∈ out.pools
-        WBP ∈ out.diagnostics
+        wSoil ∈ land.pools
+        WBP ∈ land.diagnostics
     end
     # fracRoSat = clamp((sum(wSoil[:,1]) / s_max)^β, 0, 1)
     fracRoSat = 0.2
     roSat = fracRoSat * WBP
     WBP = WBP - roSat
     @pack_land begin
-        roSat ∋ out.fluxes
-        (WBP, fracRoSat) ∋ out.diagnostics
+        roSat ∋ land.fluxes
+        (WBP, fracRoSat) ∋ land.diagnostics
     end
 
-    return out
+    return land
 end
 
-function update(o::roSat_Bergstroem, forcing, out, modelInfo)
+function update(o::roSat_Bergstroem, forcing, land, infotem)
     @unpack_land begin
-        wSoil ∈ out.pools
-        WBP ∈ out.diagnostics
+        wSoil ∈ land.pools
+        WBP ∈ land.diagnostics
     end
 
     wSoil[1] = wSoil[1] + WBP
     WBP = 0.0
 
     @pack_land begin
-        wSoil ∋ out.pools
-        WBP ∋ out.diagnostics
+        wSoil ∋ land.pools
+        WBP ∋ land.diagnostics
     end
 
-    return out
+    return land
 end
