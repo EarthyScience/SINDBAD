c24e0f3fcf16bf33daf8a82a663471847d401b44
dr-ko <skoirala@bgc-jena.mpg.de>
2022-04-10 14:24:51 +0200

    add pack and unpack macros

--- test/experiment.jl
+++ test/experiment.jl
@@ -8,21 +8,22 @@ using BenchmarkTools
 expFile = "sandbox/test_json/settings_minimal/experiment.json"
 info = getConfiguration(expFile);
 info = setupModel!(info);
-forcing = getForcing(info)
-observations = getObservation(info) # target observation!!
-optimParams = info.opti.params2opti
-approaches = info.tem.models.forward
+forcing = getForcing(info);
+
+obsvars, modelvars = getConstraintNames(info);
+observations = getObservation(info); # target observation!!
+
+optimParams = info.opti.params2opti;
+approaches = info.tem.models.forward;
+tblParams = getParameters(info.tem.models.forward, info.opti.params2opti);
+info = (;info..., opti = (;));
+info = (;info..., tem = (;));
 
 initPools = getInitPools(info)
-
-obsvars, modelvars = getConstraintNames(info)
-tblParams = getParameters(info.tem.models.forward, info.opti.params2opti)
-
 outsp = runSpinup(approaches, initPools, forcing, info.tem, false)
-# out = runEcosystem(approaches, initPools, forcing, info.tem, false)
 
 outparams, outdata = optimizeModel(forcing, observations, approaches, optimParams, initPools, obsvars, modelvars, info.tem, info.opti; maxfevals=1);
-outparams, outdata = optimizeModel(forcing, observations, approaches, optimParams, initPools, obsvars, modelvars, info.tem, info.opti; maxfevals=300);
+outparams, outdata = optimizeModel(forcing, observations, approaches, optimParams, initPools, obsvars, modelvars, info.tem, info.opti; maxfevals=30);
 outf=columntable(outdata.fluxes)
 fig = Figure(resolution = (2200, 900))
 lines(outf.transpiration)
