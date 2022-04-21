5b5e23be88a3d6892d0656fe43c258b6610342f3
lazarusA <lazarus.alon@gmail.com>
2022-04-21 14:21:39 +0200

    filter variables

--- test/experiment.jl
+++ test/experiment.jl
@@ -5,6 +5,56 @@ using BenchmarkTools
 #using GLMakie
 
 expFile = "sandbox/test_json/settings_minimal/experiment.json"
+import JSON
+function getExperimentConfiguration(expFile)
+    jsonFile = String(JSON.read(expFile))
+    parseFile = JSON.parse(jsonFile)
+    info = Dict()
+    for (k, v) in parseFile
+        info[k] = v
+    end
+    return info
+end
+
+function removeComments(inputDict)
+    newDict = filter(x -> !occursin(".c", first(x)), inputDict)
+    newDict = filter(x -> !occursin("comments", first(x)), newDict)
+    newDict = filter(x -> !occursin("comment", first(x)), newDict)
+    return newDict
+end
+
+function rmComment(input)
+    if input isa Dict
+        return removeComments(input)
+    else
+        return input
+    end
+end
+
+
+info_exp = getExperimentConfiguration(expFile)
+info = Dict()
+info["experiment"] = info_exp
+for (k, v) in info_exp["configFiles"]
+    tmp = JSON.parse(String(JSON.read(v)))
+
+    info[k] = removeComments(tmp)
+end
+
+tmp = JSON.parse(String(JSON.read("sandbox/test_json/settings_minimal/spinup.json")))
+
+
+function readConfiguration(info_exp)
+    info = Dict()
+    info["experiment"] = info_exp
+    for (k, v) in info_exp["configFiles"]
+        tmp = JSON.parse(String(JSON.read(v)))
+        info[k] = removeComments(tmp)
+    end
+    return info
+end
+
+
 info = getConfiguration(expFile);
 info = setupModel!(info);
 
@@ -24,21 +74,34 @@ approaches = info.tem.models.forward;
 # initPools = getInitPools(info)
 out = getInitOut(info);
 
+outsp = runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=1);
+outforw = runForward(approaches, forcing, outsp[1], info.tem.variables, info.tem.helpers);
 
-
-outsp = runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=5);
 # outfor = runEcosystem(approaches, forcing, out, info.tem.helpers);
 pprint(outsp)
 
+
+
+
 for it in 1:10
-    @time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=5);
+    @time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=5)
 end
 outparams, outdata = optimizeModel(forcing, out, observations, approaches, optimParams, obsvars, modelvars, info.tem, info.opti; maxfevals=1);
 
 outparams, outdata = optimizeModel(forcing, out, observations, approaches, optimParams, obsvars, modelvars, info.tem, info.opti; maxfevals=30);
 # outf=columntable(outdata.fluxes)
 using GLMakie
-fig = Figure(resolution = (2200, 900))
+fig = Figure(resolution=(2200, 900))
 lines(outdata.transpiration)
 lines!(outdata.evapotranspiration)
 lines!(observations.evapotranspiration)
+
+
+function filterOut(tpl, out)
+    outs = (;)
+    for (field, vars) in tpl
+        s = NamedTuple{vars}(getfield(out, field))
+        outs = (; outs..., field=s...)
+    end
+    return outs
+end
