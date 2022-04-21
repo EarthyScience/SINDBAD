bdfe83c9f5e3f6092037592f6be6d6f8ee96a759
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-04-21 15:22:27 +0200

    fix water balance error in the first time step

---
+++ test/experiment_skoirala.jl
@@ -0,0 +1,58 @@
+using Revise
+using Sinbad
+# using ProfileView
+using BenchmarkTools
+#using GLMakie
+
+expFile = "sandbox/test_json/settings_minimal/experiment.json"
+
+info = getConfiguration(expFile);
+info = setupModel!(info);
+
+forcing = getForcing(info);
+
+
+obsvars, modelvars = getConstraintNames(info);
+observations = getObservation(info); # target observation!!
+
+optimParams = info.opti.params2opti;
+approaches = info.tem.models.forward;
+# tblParams = getParameters(info.tem.models.forward, info.opti.params2opti);
+# info = (; info..., opti = (;));
+# info = (;info..., tem = (;));
+
+
+# initPools = getInitPools(info)
+out = getInitOut(info);
+
+outsp = runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=1);
+outforw = runForward(approaches, forcing, outsp[1], info.tem.variables, info.tem.helpers);
+
+# outfor = runEcosystem(approaches, forcing, out, info.tem.helpers);
+pprint(outsp)
+
+
+
+
+for it in 1:10
+    @time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=5)
+end
+outparams, outdata = optimizeModel(forcing, out, observations, approaches, optimParams, obsvars, modelvars, info.tem, info.opti; maxfevals=1);
+
+outparams, outdata = optimizeModel(forcing, out, observations, approaches, optimParams, obsvars, modelvars, info.tem, info.opti; maxfevals=30);
+# outf=columntable(outdata.fluxes)
+using GLMakie
+fig = Figure(resolution=(2200, 900))
+lines(outdata.transpiration)
+lines!(outdata.evapotranspiration)
+lines!(observations.evapotranspiration)
+
+
+function filterOut(tpl, out)
+    outs = (;)
+    for (field, vars) in tpl
+        s = NamedTuple{vars}(getfield(out, field))
+        outs = (; outs..., field=s...)
+    end
+    return outs
+end
