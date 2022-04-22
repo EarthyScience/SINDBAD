72a0a232a5a387706e808b5b8f66df8b0e43e7f9
SujanKoirala <skoirala@bgc-jena.mpg.de>
2022-04-22 13:59:24 +0200

    fix drainage approaches
    Former-commit-id: a306ac82776101577af14058f9b677a89d573578

--- src/models/drainage/drainage_wFC.jl
+++ src/models/drainage/drainage_wFC.jl
@@ -3,48 +3,70 @@ export drainage_wFC
 struct drainage_wFC <: drainage
 end
 
+
+function precompute(o::drainage_wFC, forcing, land, helpers)
+	## instantiate drainage
+	drainage = zeros(helpers.numbers.numType, helpers.pools.water.nZix.soilW)
+	## pack land variables
+	@pack_land drainage => land.drainage
+	return land
+end
+
 function compute(o::drainage_wFC, forcing, land, helpers)
 
 	## unpack land variables
 	@unpack_land begin
+		drainage ∈ land.drainage
 		(p_nsoilLayers, p_wFC) ∈ land.soilWBase
 		soilW ∈ land.pools
-		soilWPerc ∈ land.fluxes
+		ΔsoilW ∈ land.states
+		zero ∈ helpers.numbers
 	end
-	# get the number of soil layers
-	helpers.pools.water.nZix.soilW = p_nsoilLayers
-	soilWFlow[1] = soilWPerc
+
+	## calculate drainage
 	for sl in 1:helpers.pools.water.nZix.soilW-1
-		# drain excess moisture in oversaturation
-		maxDrain = max(soilW[sl] - p_wFC[sl], 0)
-		# store the drainage flux
-		soilWFlow[sl+1] = maxDrain
+		holdCap = p_wSat[sl+1] - (soilW[sl+1] + ΔsoilW[sl+1])
+		lossCap = soilW[sl] + ΔsoilW[sl]
+		drainage[sl] = max(soilW[sl] + ΔsoilW[sl] - p_wFC[sl], zero)
+		drainage[sl] = min(drainage[sl], holdCap, lossCap)
+		ΔsoilW[sl] = ΔsoilW[sl] - drainage[sl]
+		ΔsoilW[sl+1] = ΔsoilW[sl+1] + drainage[sl]
 	end
+
 
 	## pack land variables
 	@pack_land begin
-		soilWFlow => land.states
+		drainage => land.drainage
+		ΔsoilW => land.states
 	end
 	return land
 end
 
 function update(o::drainage_wFC, forcing, land, helpers)
-
 	## unpack variables
-	@unpack_land (soilW[sl, 1], maxDrain) ∈ land.fluxes
+	@unpack_land begin
+		soilW ∈ land.pools
+		ΔsoilW ∈ land.states
+	end
 
 	## update variables
-		# update storages
-		soilW[sl] = soilW[sl] - maxDrain
-		soilW[sl+1] = soilW[sl+1] + maxDrain
+	# update soil moisture
+	soilW = soilW + ΔsoilW
+
+	# reset soil moisture changes to zero
+	ΔsoilW = ΔsoilW - ΔsoilW
+
 
 	## pack land variables
-	@pack_land soilW => land.pools
+	@pack_land begin
+		soilW => land.pools
+		ΔsoilW => land.states
+	end
 	return land
 end
 
 @doc """
-computes the downward flow of moisture [drainage] in soil layers based on overflow from the upper layers
+downward flow of moisture [drainage] in soil layers based on overflow over field capacity
 
 ---
 
