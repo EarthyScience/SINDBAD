ce5438629b0920721f3d0acb0015f65b29c7a9fa
lazarusA <lazarus.alon@gmail.com>
2022-04-26 15:35:01 +0200

    fix get obs forcing
    Former-commit-id: dfcbe169fe03f2545288f0b5ca00e3d0e96a8779

--- src/tools/getForcing.jl
+++ src/tools/getForcing.jl
@@ -28,18 +28,17 @@ function getForcing(info)
         @show srcVar
         ds = Dataset(dataPath)
         ds_dat = ds[srcVar][:, :, :]
-        data_tmp =  eval(Meta.parse("$ds_dat" * vinfo.source2sindbadUnit))
+        data_tmp = eval(Meta.parse("$ds_dat" * vinfo.source2sindbadUnit))
         bounds = vinfo.bounds
         data_tmp = clamp.(data_tmp, bounds[1], bounds[2])
+        data_tmp[ismissing.(data_tmp)] .= info.tem.helpers.numbers.sNT(NaN)
+        data_to_push = info.tem.helpers.numbers.numType.(data_tmp[1, 1, :])
         if vinfo.spaceTimeType == "normal"
             push!(varlist, tarVar)
-            push!(dataAr,info.tem.helpers.numbers.numType.(skipmissing(data_tmp[1, 1, :])))
+            push!(dataAr, data_to_push)
         else
             push!(varlist, tarVar)
-            # data_tmp[ismissing.(data_tmp)] = 0.0
-            push!(dataAr,fill(info.tem.helpers.numbers.numType.(skipmissing(data_tmp[1, 1, :])), 14245))
-
-
+            push!(dataAr, fill(data_to_push, 14245))
         end
         @show tarVar, length(data_tmp), vinfo.spaceTimeType, size(dataAr)
     end
