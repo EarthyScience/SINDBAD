a6994ac6422d32e1bd8a9dbfde888a003ea308a7
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-05-02 11:43:04 +0200

    minor changes

--- test/experiment_skoirala.jl
+++ test/experiment_skoirala.jl
@@ -1,7 +1,7 @@
 using Revise
 using Sinbad
 # using ProfileView
-using BenchmarkTools
+# using BenchmarkTools
 #using GLMakie
 
 expFile = "sandbox/test_json/settings_minimal/experiment.json"
@@ -15,19 +15,23 @@ observations = getObservation(info); # target observation!!
 
 optimParams = info.opti.params2opti;
 approaches = info.tem.models.forward;
-# tblParams = getParameters(info.tem.models.forward, info.opti.params2opti);
+tblParams = getParameters(info.tem.models.forward, info.opti.params2opti);
 
+
+# hcat(info.opti.constraints.variables.evapotranspiration.costOptions, info.opti.constraints.variables.transpiration.costOptions)
 # initPools = getInitPools(info)
 # @show out.pools.soilW
 
 outsp = runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=3);
 osp = outsp[1];
 pprint(osp)
-# @time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=1);
+@time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=1);
 
 outforw = runForward(approaches, forcing, outsp[1], info.tem.variables, info.tem.helpers);
 pools = outforw.pools |> columntable
 fluxes = outforw.fluxes |> columntable
+
+pprint(info.opti)
 
 # outevolution = runEcosystem(approaches, forcing, outsp[1], modelvars, info.tem; nspins=3)
 
@@ -41,30 +45,7 @@ fluxes = outforw.fluxes |> columntable
 # for it in 1:10
 #     @time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=4)
 # end
-function rotate!(v,n::Int)
-    l = length(v)
-    l>1 || return v
-    n = n % l
-    n = n < 0 ? n+l : n
-    n==0 && return v
-    for i=1:gcd(n,l)
-      tmp = v[i]
-      dst = i
-      src = dst+n
-      while src != i
-        v[dst] = v[src]
-        dst = src
-        src += n
-        if src > l
-          src -= l
-        end
-      end
-      v[dst] = tmp
-    end
-    return v
-  end
-a=rand(6)
-rotate!(a, 3)
+
 # outf=columntable(outdata.fluxes)
 using GLMakie
 fig = Figure(resolution=(2200, 900))
