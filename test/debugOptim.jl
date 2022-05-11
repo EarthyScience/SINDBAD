377b3fbe26483a161fb527c033038dc6fc7f9f35
lazarusA <lazarus.alon@gmail.com>
2022-05-11 15:56:52 +0200

    added simple sum losses

--- test/debugOptim.jl
+++ test/debugOptim.jl
@@ -20,10 +20,9 @@ tblParams = getParameters(info.tem.models.forward, info.opti.params2opti);
 outevolution = runEcosystem(approaches, forcing, out, modelvars, info.tem; nspins=3)
 (y, ŷ) = getSimulationData(outevolution, observations, optimvars, obsvars)
 
-idxs = (.!isnan.(y)) .& (.!isnan.(ŷ))
-
-mean(abs2.(y[idxs] .- ŷ[idxs]))
-cor(y[idxs], ŷ[idxs])
+#idxs = (.!isnan.(y)) .& (.!isnan.(ŷ))
+#mean(abs2.(y[idxs] .- ŷ[idxs]))
+#cor(y[idxs], ŷ[idxs])
 
 outparams, outdata = optimizeModel(forcing, out, observations, approaches, optimParams,
-    obsvars, modelvars, optimvars, info.tem, info.opti; maxfevals=50);
+    obsvars, modelvars, optimvars, info.tem, info.opti; maxfevals=10, lossym=(:mse, :cor));
