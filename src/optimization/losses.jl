ea5df8134ea76a3747e9c7a575ac1712ab196f6a
lazarusA <lazarus.alon@gmail.com>
2022-05-12 13:09:09 +0200

    add more options
    Former-commit-id: e6e2ca9965109e785444bcb90c2d96d0b1b1f8b5

--- src/optimization/losses.jl
+++ src/optimization/losses.jl
@@ -6,11 +6,26 @@ function loss(y::Matrix, ŷ::Matrix, ::Val{:mse})
     return mean(abs2.(y[idxs] .- ŷ[idxs]))
 end
 
-function loss(y::Matrix, ŷ::Matrix, ::Val{:cor})
+function loss(y::Matrix, ŷ::Matrix, ::Val{:pcor})
     idxs = (.!isnan.(y)) .& (.!isnan.(ŷ))
     return cor(y[idxs], ŷ[idxs])
 end
 
+function loss(y::Matrix, ŷ::Matrix, ::Val{:pcor2})
+    idxs = (.!isnan.(y)) .& (.!isnan.(ŷ))
+    return 1.0 - cor(y[idxs], ŷ[idxs])^2
+end
+
 function loss(y::Matrix, ŷ::Matrix, symbs)
     return sum([loss(y, ŷ, Val(s)) for s in symbs])
+end
+
+function loss(y::Matrix, ŷ::Matrix, yσ, ::Val{:nse})
+    idxs = (.!isnan.(y)) .& (.!isnan.(ŷ))
+    return 1 .- (1 .- sum(abs2.(y[idxs] .- mean(ŷ[idxs])))/(sum(abs2.(y[idxs] .- mean(y[idxs]))./yσ)))
+end
+
+function loss(y::Matrix, ŷ::Matrix, ::Val{:nse})
+    idxs = (.!isnan.(y)) .& (.!isnan.(ŷ))
+    return 1 .- (1 .- sum(abs2.(y[idxs] .- mean(ŷ[idxs]))) / (sum(abs2.(y[idxs] .- mean(y[idxs])))))
 end
