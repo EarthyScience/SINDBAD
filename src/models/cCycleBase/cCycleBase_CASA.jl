25c7a8223c591de5a7162a9bfa98aee7a5636128
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-05-12 16:26:10 +0200

    clean ccyclebase
    Former-commit-id: 1a36eb4d9f992f0f82a731b53a0803dd7a1e1124

--- src/models/cCycleBase/cCycleBase_CASA.jl
+++ src/models/cCycleBase/cCycleBase_CASA.jl
@@ -2,35 +2,44 @@ export cCycleBase_CASA
 
 @bounds @describe @units @with_kw struct cCycleBase_CASA{T1, T2, T3, T4, T5, T6, T7} <: cCycleBase
 	annk::T1 = [1, 0.03, 0.03, 1, 14.8, 3.9, 18.5, 4.8, 0.2424, 0.2424, 6, 7.3, 0.2, 0.0045] | ([0.05, 0.002, 0.002, 0.05, 1.48, 0.39, 1.85, 0.48, 0.02424, 0.02424, 0.6, 0.73, 0.02, 0.0045], [3.3, 0.5, 0.5, 3.3, 148.0, 39.0, 185.0, 48.0, 2.424, 2.424, 60.0, 73.0, 2.0, 0.045]) | "turnover rate of ecosystem carbon pools" | "yr-1"
-	cFlowE::T2 = 	[-1  0  0  0  0  0  0  0  0  0  0  0  0  0;
-					0  -1  0  0  0  0  0  0  0  0  0  0  0  0;
-					0  0  -1  0  0  0  0  0  0  0  0  0  0  0;
-					0  0  0  -1  0  0  0  0  0  0  0  0  0  0;
-					0  0  0  0  -1  0  0  0  0  0  0  0  0  0;
-					0  0  0  0  0  -1  0  0  0  0  0  0  0  0;
-					0  0  0  0  0  0  -1  0  0  0  0  0  0  0;
-					0  0  0  0  0  0  0  -1  0  0  0  0  0  0;
-					0  0  0  0  0  0  0  0  -1  0  0  0  0  0;
-					0  0  0  0  0  0  0  0  0  -1  0  0  0  0;
-					0  0  0  0  0.4  0.4  0  0  0.4  0  -1  0  0  0;
-					0  0  0  0  0  0  0.45  0.45  0  0.4  0  -1  0.45  0.45;
-					0  0  0  0  0  0.6  0  0.55  0.6  0.6  0.4  0  -1  0;
-					0  0  0  0  0  0  0  0  0  0  0  0  0.45  -1] | nothing | "Transfer matrix for carbon at ecosystem level" | ""
+	cFlowE::T2 = 	[-1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.4  0.4  0.0  0.0  0.4  0.0  -1.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  0.45  0.45  0.0  0.4  0.0  -1.0  0.45  0.45;
+					0.0  0.0  0.0  0.0  0.0  0.6  0.0  0.55  0.6  0.6  0.4  0.0  -1.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.45  -1.0] | nothing | "Transfer matrix for carbon at ecosystem level" | ""
 	cVegRootF_AGE_per_PFT::T3 = [1.8, 1.2, 1.2, 5.0, 1.8, 1.0, 1.0, 0.0, 1.0, 2.8, 1.0, 1.0] | ([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0]) | "mean age of fine roots" | "yr"
 	cVegRootC_AGE_per_PFT::T4 = [41.0, 58.0, 58.0, 42.0, 27.0, 25.0, 25.0, 0.0, 5.5, 40.0, 1.0, 40.0] | ([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0]) | "mean age of coarse roots" | "yr"
 	cVegWood_AGE_per_PFT::T5 = [41.0, 58.0, 58.0, 42.0, 27.0, 25.0, 25.0, 0.0, 5.5, 40.0, 1.0, 40.0] | ([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0]) | "mean age of wood" | "yr"
 	cVegLeaf_AGE_per_PFT::T6 = [1.8, 1.2, 1.2, 5.0, 1.8, 1.0, 1.0, 0.0, 1.0, 2.8, 1.0, 1.0] | ([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0]) | "mean age of leafs" | "yr"
-	C2Nveg::T7 = [25, 260, 260, 25] | nothing | "carbon to nitrogen ratio in vegetation pools" | "gC/gN"
+	C2Nveg::T7 = [25.0, 260.0, 260.0, 25.0] | nothing | "carbon to nitrogen ratio in vegetation pools" | "gC/gN"
 end
 
 function precompute(o::cCycleBase_CASA, forcing, land::NamedTuple, helpers::NamedTuple)
 	@unpack_cCycleBase_CASA o
 
-	## instantiate variables
-	p_C2Nveg = ones(helpers.numbers.numType, length(land.pools.cVeg)); #sujan
+	@unpack_land begin
+		numType ∈ helpers.numbers
+		cEco ∈ land.pools
+	end
 
-	## pack land variables
-	@pack_land p_C2Nveg => land.cCycleBase
+    ## instantiate variables
+    p_C2Nveg = ones(numType, length(cEco)) #sujan
+    cEcoEfflux = zeros(numType, length(land.pools.cEco)) #sujan moved from get states
+
+    ## pack land variables
+    @pack_land begin
+		(p_C2Nveg, cFlowA) => land.cCycleBase
+		cEcoEfflux => land.states
+	end
 	return land
 end
 
@@ -38,24 +47,27 @@ function compute(o::cCycleBase_CASA, forcing, land::NamedTuple, helpers::NamedTu
 	## unpack parameters
 	@unpack_cCycleBase_CASA o
 
-	## unpack land variables
-	@unpack_land p_C2Nveg ∈ land.cCycleBase
+    ## unpack land variables
+    @unpack_land begin
+		p_C2Nveg ∈ land.cCycleBase
+		𝟙 ∈ helpers.numbers
+	end
 
 	## calculate variables
 	# carbon to nitrogen ratio [gC.gN-1]
-	for zix in helpers.pools.carbon.zix.cVeg
-		p_C2Nveg[zix] = C2Nveg[zix]
-	end
-	# annual turnover rates
-	p_annk = reshape(repelem[annk], length(land.pools.cEco)); #sujan
+    p_C2Nveg[getzix(land.pools.cVeg)] .= C2Nveg
+
+	# turnover rates
+    TSPY = helpers.dates.nStepsYear
+    p_k_base = 𝟙 .- (exp.(-𝟙 .* annk).^(𝟙 / TSPY))
 
 	## pack land variables
-	@pack_land (p_C2Nveg, p_annk) => land.cCycleBase
+    @pack_land (p_C2Nveg, p_k_base, cFlowE) => land.cCycleBase
 	return land
 end
 
 @doc """
-Compute carbon to nitrogen ratio & annual turnover rates
+Compute carbon to nitrogen ratio & base turnover rates
 
 # Parameters
 $(PARAMFIELDS)
@@ -83,7 +95,7 @@ precompute/instantiate time-invariant variables for cCycleBase_CASA
  - Potter; C. S.; Randerson; J. T.; Field; C. B.; Matson; P. A.; Vitousek; P. M.; Mooney; H. A.  & Klooster, S. A. (1993). Terrestrial ecosystem production: a process model based on global  satellite & surface data. Global Biogeochemical Cycles, 7[4], 811-841.
 
 *Versions*
- - 1.0 on 28.02.2020 [sbesnard]  
+ - 1.0 on 28.05.2022 [skoirala]: migrate to julia  
 
 *Created by:*
  - ncarvalhais
