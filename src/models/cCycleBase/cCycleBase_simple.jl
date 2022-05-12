cc4a6dba44e56289852bf9af27b5be558a4f8dca
dr-ko <skoirala@bgc-jena.mpg.de>
2022-05-12 16:26:10 +0200

    clean ccyclebase
    Former-commit-id: 1a36eb4d9f992f0f82a731b53a0803dd7a1e1124

--- src/models/cCycleBase/cCycleBase_simple.jl
+++ src/models/cCycleBase/cCycleBase_simple.jl
@@ -2,31 +2,39 @@ export cCycleBase_simple
 
 @bounds @describe @units @with_kw struct cCycleBase_simple{T1, T2, T3} <: cCycleBase
 	annk::T1 = [1, 0.03, 0.03, 1, 14.8, 3.9, 18.5, 4.8, 0.2424, 0.2424, 6, 7.3, 0.2, 0.0045] | ([0.05, 0.002, 0.002, 0.05, 1.48, 0.39, 1.85, 0.48, 0.02424, 0.02424, 0.6, 0.73, 0.02, 0.0045], [3.3, 0.5, 0.5, 3.3, 148.0, 39.0, 185.0, 48.0, 2.424, 2.424, 60.0, 73.0, 2.0, 0.045]) | "turnover rate of ecosystem carbon pools" | "yr-1"
-	cFlowA::T2 = 	[-1  0  0  0  0  0  0  0  0  0  0  0  0  0;
-					0  -1  0  0  0  0  0  0  0  0  0  0  0  0;
-					0  0  -1  0  0  0  0  0  0  0  0  0  0  0;
-					0  0  0  -1  0  0  0  0  0  0  0  0  0  0;
-					0  0  0  0.54  -1  0  0  0  0  0  0  0  0  0;
-					0  0  0  0.46  0  -1  0  0  0  0  0  0  0  0;
-					0.54  0  0  0  0  0  -1  0  0  0  0  0  0  0;
-					0.46  0  0  0  0  0  0  -1  0  0  0  0  0  0;
-					0  0  1  0  0  0  0  0  -1  0  0  0  0  0;
-					0  1  0  0  0  0  0  0  0  -1  0  0  0  0;
-					0  0  0  0  0.4  0.15  0  0  0.24  0  -1  0  0  0;
-					0  0  0  0  0  0  0.45  0.17  0  0.24  0  -1  0  0;
-					0  0  0  0  0  0.43  0  0.43  0.28  0.28  0.4  0.43  -1  0;
-					0  0  0  0  0  0  0  0  0  0  0  0.005  0.0026  -1] | nothing | "Transfer matrix for carbon at ecosystem level" | ""
-	C2Nveg::T3 = [25, 260, 260, 25] | nothing | "carbon to nitrogen ratio in vegetation pools" | "gC/gN"
+	cFlowA::T2 = 	[-1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.54  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.46  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.54  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.46  0.0  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  1.0  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0  0.0;
+					0.0  1.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  -1.0  0.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.4  0.15  0.0  0.0  0.24  0.0  -1.0  0.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  0.45  0.17  0.0  0.24  0.0  -1.0  0.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.43  0.0  0.43  0.28  0.28  0.4  0.43  -1.0  0.0;
+					0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.005  0.0026  -1.0] | nothing | "Transfer matrix for carbon at ecosystem level" | ""
+	C2Nveg::T3 = [25.0, 260.0, 260.0, 25.0] | nothing | "carbon to nitrogen ratio in vegetation pools" | "gC/gN"
 end
 
 function precompute(o::cCycleBase_simple, forcing, land::NamedTuple, helpers::NamedTuple)
 	@unpack_cCycleBase_simple o
 
-	## instantiate variables
-	p_C2Nveg = ones(helpers.numbers.numType, length(land.pools.cVeg)); #sujan
+	@unpack_land begin
+		numType ∈ helpers.numbers
+		cEco ∈ land.pools
+	end
+    ## instantiate variables
+    p_C2Nveg = ones(numType, length(cEco)) #sujan
+    cEcoEfflux = zeros(numType, length(land.pools.cEco)) #sujan moved from get states
 
-	## pack land variables
-	@pack_land p_C2Nveg => land.cCycleBase
+    ## pack land variables
+    @pack_land begin
+		(p_C2Nveg, cFlowA) => land.cCycleBase
+		cEcoEfflux => land.states
+	end
 	return land
 end
 
@@ -35,18 +43,22 @@ function compute(o::cCycleBase_simple, forcing, land::NamedTuple, helpers::Named
 	@unpack_cCycleBase_simple o
 
 	## unpack land variables
-	@unpack_land p_C2Nveg ∈ land.cCycleBase
+    @unpack_land begin
+		p_C2Nveg ∈ land.cCycleBase
+		𝟙 ∈ helpers.numbers
+	end
 
 	## calculate variables
 	#carbon to nitrogen ratio [gC.gN-1]
-	for zix in helpers.pools.carbon.zix.cVeg
-		p_C2Nveg[zix] = C2Nveg[zix]
-	end
-	# annual turnover rates
-	p_annk = reshape(repelem[annk], length(land.pools.cEco)); #sujan
+    p_C2Nveg[getzix(land.pools.cVeg)] .= C2Nveg
+
+	# turnover rates
+    TSPY = helpers.dates.nStepsYear
+    p_k_base = 𝟙 .- (exp.(-𝟙 .* annk).^(𝟙 / TSPY))
 
 	## pack land variables
-	@pack_land (p_C2Nveg, p_annk) => land.cCycleBase
+    @pack_land (p_C2Nveg, p_k_base, cFlowA) => land.cCycleBase
+
 	return land
 end
 
@@ -77,7 +89,7 @@ precompute/instantiate time-invariant variables for cCycleBase_simple
  - Potter; C. S.; J. T. Randerson; C. B. Field; P. A. Matson; P. M.  Vitousek; H. A. Mooney; & S. A. Klooster. 1993. Terrestrial ecosystem  production: A process model based on global satellite & surface data.  Global Biogeochemical Cycles. 7: 811-841.
 
 *Versions*
- - 1.0 on 28.02.2020 [sbesnard]  
+ - 1.0.0 on 28.02.2020.0 [sbesnard]  
 
 *Created by:*
  - ncarvalhais
