e740edc094a8c8c941c950a045aff70a746f37b5
lazarusA <lazarus.alon@gmail.com>
2022-05-18 10:50:03 +0200

    Move experiments to examples folder

--- examples/experiment_skoirala.jl
+++ examples/experiment_skoirala.jl
@@ -86,3 +86,63 @@ info.tem, info.optim; maxfevals=10, nspins=1);
 #         info.tem, info.optim; maxfevals=10, nspins=1);
 #     # CSV.write("../data/outparams.csv", outparams)
 # end
+# using ProfileView
+# using BenchmarkTools
+#using GLMakie
+expFilejs = "settings_minimal/experiment.json"
+#local_root ="/Users/skoirala/research/sjindbad/sinbad.jl/"
+local_root = dirname(Base.active_project())
+expFile = joinpath(local_root,expFilejs)
+
+expFile = "sandbox/test_json/settings_minimal/experiment.json"
+
+info = getConfiguration(expFile);
+info = setupModel!(info);
+out = createInitOut(info);
+forcing = getForcing(info);
+obsvars, modelvars, optimvars = getConstraintNames(info);
+observations = getObservation(info); # target observation!!
+
+optimParams = info.opti.params2opti;
+approaches = info.tem.models.forward;
+tblParams = getParameters(info.tem.models.forward, info.opti.params2opti);
+
+
+# hcat(info.opti.constraints.variables.evapotranspiration.costOptions, info.opti.constraints.variables.transpiration.costOptions)
+# initPools = getInitPools(info)
+# @show out.pools.soilW
+
+outsp = runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=3);
+osp = outsp[1];
+pprint(osp)
+@time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=1);
+
+@time outforw = runForward(approaches, forcing, outsp[1], info.tem.variables, info.tem.helpers);
+pools = outforw.pools |> columntable
+fluxes = outforw.fluxes |> columntable
+
+pprint(info.opti)
+
+# outevolution = runEcosystem(approaches, forcing, outsp[1], modelvars, info.tem; nspins=3)
+
+# outfor = runEcosystem(approaches, forcing, out, info.tem.helpers);
+#pprint(outsp)
+
+# outparams, outdata = optimizeModel(forcing, out, observations, approaches, optimParams,
+    # obsvars, modelvars, optimvars, info.tem, info.opti; maxfevals=1);
+
+
+# for it in 1:10
+#     @time runSpinup(approaches, forcing, out, info.tem.helpers, false; nspins=4)
+# end
+
+# outf=columntable(outdata.fluxes)
+using GLMakie
+fig = Figure(resolution=(2200, 900))
+# lines(pools.snowW)
+lines(fluxes.gpp)
+lines!(fluxes.NEE)
+lines!(fluxes.NPP)
+# lines!(fluxes.evapotranspiration)
+# lines!(observations.evapotranspiration)
+
