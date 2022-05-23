340cab277aafc6aa0ff11f874946b6d378fb9274
Fabian Gans <fgans@bgc-jena.mpg.de>
2022-05-23 15:29:15 +0200

    changes for yaxarray run

--- src/tools/getForcing.jl
+++ src/tools/getForcing.jl
@@ -1,5 +1,12 @@
 export getForcing
 
+function clean_inputs(datapoint,vinfo,::Val{T}) where T
+    datapoint = applyUnitConversion(datapoint, vinfo.source2sindbadUnit, vinfo.additiveUnitConversion)
+    bounds = vinfo.bounds
+    datapoint = clamp(datapoint, bounds[1], bounds[2])
+    return ismissing(datapoint) ? T(NaN) : T(datapoint) 
+end
+
 """
 getForcing(info)
 """
@@ -29,11 +36,7 @@ function getForcing(info)
         srcVar = vinfo.sourceVariableName
         tarVar = Symbol(v)
         ds_dat = ds[srcVar][:, :, :]
-        data_tmp = applyUnitConversion(ds_dat, vinfo.source2sindbadUnit, vinfo.additiveUnitConversion)
-        bounds = vinfo.bounds
-        data_tmp = clamp.(data_tmp, bounds[1], bounds[2])
-        data_tmp[ismissing.(data_tmp)] .= info.tem.helpers.numbers.sNT(NaN)
-        data_to_push = info.tem.helpers.numbers.numType.(data_tmp[1, 1, :])
+        data_to_push = clean_inputs.(ds_dat,Ref(vinfo),Val{info.tem.helpers.numbers.numType}())[1, 1, :]
         if vinfo.spaceTimeType == "normal"
             push!(varlist, tarVar)
             push!(dataAr, data_to_push)
