b00f23cd3ccb6bcf9874131ea03b01a5e3bbf8bc
lazarusA <lazarus.alon@gmail.com>
2022-05-25 17:44:26 +0200

    fixed by adding YAXArrayBase#master

--- examples/experiment_fgans.jl
+++ examples/experiment_fgans.jl
@@ -7,12 +7,14 @@ using ProgressMeter
 expFilejs = "settings_minimal/experiment.json"
 #local_root ="/Users/skoirala/research/sjindbad/sinbad.jl/"
 local_root = @__DIR__
-expFile = joinpath(local_root, expFilejs)
+expFile = joinpath(local_root, expFilejs);
 
 info = getConfiguration(expFile, local_root);
 info = setupModel!(info);
 #observations = getObservation(info); # target observation!!
-forcing = getForcing(info, Val(:yaxarray))
-output = setupOutput(info)
+forcing = getForcing(info, Val(:yaxarray));
+output = setupOutput(info);
 
 mapRunEcosystem(forcing, output, info.tem)
+
+outcubes[1]
