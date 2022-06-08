d8ae443b955f0f96c6e5c0e5a3b8ab8d035fc27c
Sujan Koirala <skoirala@bgc-jena.mpg.de>
2022-06-08 13:17:02 +0200

    cleanup of project

--- examples/experiment_fgans.jl
+++ examples/experiment_fgans.jl
@@ -15,6 +15,6 @@ info = setupModel!(info);
 forcing = getForcing(info, Val(:yaxarray));
 output = setupOutput(info);
 
-outcubes = mapRunEcosystem(forcing, output, info.tem)
+@time outcubes = mapRunEcosystem(forcing, output, info.tem);
 
 outcubes[2]
