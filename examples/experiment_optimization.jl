17115990c762bb85d311e3dc0c4ec9ab027a8a77
dr-ko <skoirala@bgc-jena.mpg.de>
2022-06-13 09:57:14 +0200

    clean optimizers and losses

--- examples/experiment_optimization.jl
+++ examples/experiment_optimization.jl
@@ -2,6 +2,11 @@ using Revise
 using Sindbad
 # using Suppressor
 # using Optimization
+using Tables:
+    columntable,
+    matrix
+using TableOperations:
+    select
 expFilejs = "settings_minimal/experiment.json"
 local_root = dirname(Base.active_project())
 expFile = joinpath(local_root, expFilejs)
@@ -16,6 +21,25 @@ info = setupOptimization(info);
 out = createInitOut(info);
 outparams, outdata = optimizeModel(forcing, out, observations,
 info.tem, info.optim; nspins=1);    
-outdata.fluxes
+
+outdata = runEcosystem(info.tem.models.forward, forcing, out, info.tem; nspins=1);
+outsmodel=outdata
+obsV = :gpp
+modelVarInfo = [:fluxes, :gpp]
+ŷField = getfield(outsmodel, modelVarInfo[1]) |> columntable;
+ŷ = hcat(getfield(ŷField, modelVarInfo[2])...)' |> Matrix;
+y = observations |> select(obsV) |> matrix;
+yσ = observations |> select(Symbol(string(obsV)*"_σ")) |> matrix;
+
 using Plots
-plot(outdata.fluxes)
+plot(ŷ)
+plot!(y)
+plot!(yσ)
+
+states = outdata.states |> columntable;
+pools = outdata.pools |> columntable;
+fluxes = outdata.fluxes |> columntable;
+
+using Plots
+plot(fluxes.NEE)
+plot!(observations.nee)
