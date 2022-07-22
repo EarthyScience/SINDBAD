7ae31a7332aa69ac7c29c05f071a9e69aae54ede
dr-ko <skoirala@bgc-jena.mpg.de>
2022-07-22 09:39:19 +0200

    fix models being returned as Any type

--- examples/exp_mapEco/experiment_mapEco.jl
+++ examples/exp_mapEco/experiment_mapEco.jl
@@ -1,7 +1,7 @@
 using Revise
 using Sindbad
 using ProgressMeter
-Base.show(io::IO,nt::Type{<:LandEcosystem}) = print(io,supertype(nt))
+# Base.show(io::IO,nt::Type{<:LandEcosystem}) = print(io,supertype(nt))
 Base.show(io::IO,nt::Type{<:NamedTuple}) = print(io,"NT")
 
 expFile = "exp_mapEco/settings_mapEco/experiment.json"
@@ -24,5 +24,23 @@ outcubes[2]
 # optimization
 info = setupOptimization(info);
 observations = getObservation(info, Val(:yaxarray_s)); 
+
+
+info_optim = info.optim;
+tem = info.tem;
+optimVars = info_optim.variables.optim;
+# get the list of observed variables, model variables to compare observation against, 
+# obsVars, optimVars, storeVars = getConstraintNames(info);
+
+# get the subset of parameters table that consists of only optimized parameters
+tblParams = getParameters(tem.models.forward)
+tblParams = getParameters(tem.models.forward, Symbol.(info_optim.optimized_paramaters))
+
+# get the defaults and bounds
+default_values = tem.helpers.numbers.sNT.(tblParams.defaults)
+lower_bounds = tem.helpers.numbers.sNT.(tblParams.lower)
+upper_bounds = tem.helpers.numbers.sNT.(tblParams.upper)
+
+
 tmp = observations.data[1]
 outparams, outsmodel = optimizeModel(forcing, output, info.tem, info.optim, observations);  
