be4ad42c8f4f3800e2cc556e3d87ff4414ad77ac
dr-ko <skoirala@bgc-jena.mpg.de>
2022-08-10 17:50:11 +0200

    change variable names

--- examples/exp_steadyState/experiment_steadyState.jl
+++ examples/exp_steadyState/experiment_steadyState.jl
@@ -10,10 +10,10 @@ using TableOperations:
 
 using Plots
 
-expFile = "exp_steadyState/settings_steadyState/experiment.json"
+experiment_json = "exp_steadyState/settings_steadyState/experiment.json"
 
 
-info = getConfiguration(expFile);
+info = getConfiguration(experiment_json);
 info = setupExperiment(info);
 forcing = getForcing(info, Val(Symbol(info.modelRun.rules.data_backend)));
 
@@ -23,26 +23,26 @@ out = createInitOut(info);
 # spinup_forcing = forcing[[selTimeStep]];
 spinup_forcing = getSpinupForcing(forcing, info.tem);
 
-init_out = runPrecompute(forcing[1], info.tem.models.forward, out, info.tem.helpers);
+land_init = runPrecompute(forcing[1], info.tem.models.forward, out, info.tem.helpers);
 sel_pool = :cEco
 
 spinup_models = info.tem.models.forward[info.tem.models.is_spinup .== 1];
 
-plot(getfield(init_out.pools, sel_pool),linewidth=5,title="Steady State Solution",
+plot(getfield(land_init.pools, sel_pool),linewidth=5,title="Steady State Solution",
      xaxis="Pool#",yaxis="C", label="Init") # legend=false
 
 sp = :ODE_Tsit5
-out_sp = doSpinup(spinup_models, spinup_forcing.yearOne, deepcopy(init_out), info.tem, Val(sp));
+out_sp = doSpinup(spinup_models, spinup_forcing.yearOne, deepcopy(land_init), info.tem, Val(sp));
 plot!(getfield(out_sp.pools, sel_pool),linewidth=5, label=string(sp)) # legend=false
 
 sp = :SSP_DynamicSS_Tsit5
-out_sp = doSpinup(spinup_models, spinup_forcing.yearOne, deepcopy(init_out), info.tem, Val(sp));
+out_sp = doSpinup(spinup_models, spinup_forcing.yearOne, deepcopy(land_init), info.tem, Val(sp));
 plot!(getfield(out_sp.pools, sel_pool),linewidth=5, label=string(sp)) # legend=false
 
 sp = :SSP_SSRootfind
-out_sp = doSpinup(spinup_models, spinup_forcing.yearOne, deepcopy(init_out), info.tem, Val(sp));
+out_sp = doSpinup(spinup_models, spinup_forcing.yearOne, deepcopy(land_init), info.tem, Val(sp));
 plot!(getfield(out_sp.pools, sel_pool),linewidth=5, label=string(sp)) # legend=false
 
 
-outsp_full = runSpinup(info.tem.models.forward, forcing, init_out, info.tem; spinup_forcing=spinup_forcing);
+outsp_full = runSpinup(info.tem.models.forward, forcing, land_init, info.tem; spinup_forcing=spinup_forcing);
 plot!(getfield(outsp_full.pools, sel_pool),linewidth=5, label=string(sp)) # legend=false
