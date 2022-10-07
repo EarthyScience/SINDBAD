64b8ba0062656885ada970c506a2753ed7555074
Lazaro Alonso <lazarus.alon@gmail.com>
2022-10-07 09:44:44 +0200

    hybrid
    template

---
+++ HybridSindbad/src/HybridSindbad.jl
@@ -0,0 +1,9 @@
+module HybridSindbad
+using ForwardSindbad
+using Lux, ComponentArrays
+using Zygote, Optim, Optimization
+using Random, StatsBase
+
+greet() = print("Hello World!")
+
+end # module HybridSindbad
