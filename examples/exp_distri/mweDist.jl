46c23148b8c87f5738387499e33cecf5eecb8ccb
Lazaro Alonso <lazarus.alon@gmail.com>
2022-10-21 15:12:14 +0200

    minor model fix

--- examples/exp_distri/mweDist.jl
+++ examples/exp_distri/mweDist.jl
@@ -1,7 +1,19 @@
 using Sindbad
 using ForwardSindbad
 using HybridSindbad
+#=
+using Distributed
+addprocs(3)
 
+@everywhere using Pkg
+@everywhere Pkg.activate(".")
+
+@everywhere begin
+    using Sindbad
+    using ForwardSindbad
+    using HybridSindbad
+end
+=#
 # copy data from 
 # rsync -avz lalonso@atacama:/Net/Groups/BGI/work_1/scratch/lalonso/fluxnet_observations.zarr
 experiment_json = "./settings_optiSpace/experiment.json"
@@ -12,6 +24,11 @@ ds = "/Users/lalonso/Documents/SindbadThreads/dev/Sindbad/examples/data/fluxnet_
 
 forcing = HybridSindbad.getForcing(info, ds, Val{:zarr}());
 output = setupOutput(info);
-
-outcubes = mapRunEcosystem(forcing, output, info.tem, info.tem.models.forward;
+GC.gc()
+@profview outcubes = mapRunEcosystem(forcing, output, info.tem, info.tem.models.forward;
     max_cache=info.modelRun.rules.yax_max_cache);
+
+
+
+gpp = outcubes[9]
+gpp.data[:,1]
