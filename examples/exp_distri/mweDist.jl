56329aab31a2ff0911aa6a44d6905e9655301db9
Lazaro Alonso <lazarus.alon@gmail.com>
2022-10-21 16:17:22 +0200

    malloc
    maybe

--- examples/exp_distri/mweDist.jl
+++ examples/exp_distri/mweDist.jl
@@ -19,13 +19,12 @@ end
 experiment_json = "./settings_optiSpace/experiment.json"
 info = getConfiguration(experiment_json);
 info = setupExperiment(info);
-
 ds = "/Users/lalonso/Documents/SindbadThreads/dev/Sindbad/examples/data/fluxnet_forcing.zarr/"
-
 forcing = HybridSindbad.getForcing(info, ds, Val{:zarr}());
 output = setupOutput(info);
 GC.gc()
-@profview outcubes = mapRunEcosystem(forcing, output, info.tem, info.tem.models.forward;
+GC.enable_logging(true)
+outcubes = mapRunEcosystem(forcing, output, info.tem, info.tem.models.forward;
     max_cache=info.modelRun.rules.yax_max_cache);
 
 
