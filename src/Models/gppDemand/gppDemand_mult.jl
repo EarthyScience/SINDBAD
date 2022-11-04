08b000afb30dd27d77616753730b1e6caa0a119e
Lazaro Alonso <lazarus.alon@gmail.com>
2022-11-04 20:23:27 +0100

    up models

--- src/Models/gppDemand/gppDemand_mult.jl
+++ src/Models/gppDemand/gppDemand_mult.jl
@@ -10,8 +10,9 @@ function precompute(o::gppDemand_mult, forcing::NamedTuple, land::NamedTuple, he
 
 	# set 3d scalar matrix with current scalars
 	scall = ones(helpers.numbers.numType, 4)
-
-	@pack_land scall => land.gppDemand
+	AllDemScGPP = helpers.numbers.𝟙
+	gppE = helpers.numbers.𝟘
+	@pack_land (scall,AllDemScGPP, gppE) => land.gppDemand
 
 	return land
 end
