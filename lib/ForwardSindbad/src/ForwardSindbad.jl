96b6215999b086d69aa0c501d2727cd34d8a71ef
dr-ko <skoirala@bgc-jena.mpg.de>
2022-11-10 19:29:47 +0100

    move sub packages to lib

--- lib/ForwardSindbad/src/ForwardSindbad.jl
+++ lib/ForwardSindbad/src/ForwardSindbad.jl
@@ -1,28 +1,30 @@
 module ForwardSindbad
 using Reexport: @reexport
 using Sindbad
-using DimensionalData
-using InteractiveUtils
-using YAXArrays, NetCDF, DiskArrayTools, Zarr
-using YAXArrayBase
-using RecursiveArrayTools
-using AxisKeys, FillArrays
-using ThreadPools 
-using StatsBase:
-    mean,
-    percentile,
-    cor
-using YAXArrays: savecube
+
 using AxisKeys: KeyedArray, AxisKeys
+using AxisKeys, FillArrays
+using DimensionalData
+using DiskArrayTools
+using JLD2: @save
+using NetCDF 
+using RecursiveArrayTools
+using TypedTables:
+    Table
+using YAXArrays
+using YAXArrayBase
+using YAXArrays: savecube
 using YAXArrayBase: getdata
+using Zarr
+
+using InteractiveUtils
+using ThreadPools 
+
 using Flatten:
     flatten,
     metaflatten,
     fieldnameflatten,
     parentnameflatten
-using TypedTables:
-    Table
-using JLD2: @save
 using ProgressMeter
 
 
