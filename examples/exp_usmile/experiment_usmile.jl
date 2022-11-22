144d200cef8cc3b1819fe15a35b5dce4b7513959
skoirala <skoirala@bgc-jena.mpg.de>
2022-11-22 11:46:35 +0100

    fix plotting

--- examples/exp_usmile/experiment_usmile.jl
+++ examples/exp_usmile/experiment_usmile.jl
@@ -3,6 +3,7 @@ using Sindbad
 using ForwardSindbad
 using OptimizeSindbad
 noStackTrace()
+using FileIO
 
 experiment_json = "../exp_usmile/settings_lue/experiment.json"
 # experiment_json = "../exp_usmile/settings_w/experiment.json"
@@ -37,44 +38,40 @@ tblParams.optim .= outparams; # update the parameters with pVector
 
 newApproaches = updateParameters(tblParams, info.tem.models.forward);
 runEcosystem!(output.data, newApproaches, forc, info.tem, info.tem.helpers.run.parallelization);
-model_data = (; Pair.(output.variables, output.data)...);
+
+model_data_d = (; Pair.(output.variables, outcubes)...);
+model_data_o = (; Pair.(output.variables, output.data)...);
 
 ## some plotting
 
 using CairoMakie, AlgebraOfGraphics, DataFrames, Dates
 
 spacesize = (2, 2)
-sel_str = join(spacesize, "x")
-sel_str = join(sel_ind, "x")
 
 sp_it = Iterators.product(Base.OneTo.(spacesize)...)
 for sel_ind in sp_it
     for obsvar in info.optim.variables.obs
         modelvar = getfield(info.optim.variables.optim, obsvar)[2]
         obsdat = Array(getfield(obs, obsvar)[sel_ind..., :])
-        moddat = Array(getfield(model_data, modelvar)[:, sel_ind...])
+        moddat_o = Array(getfield(model_data_o, modelvar)[:, sel_ind...])
+        moddat_d = Array(getfield(model_data_d, modelvar)[:, sel_ind...])
+        df = DataFrame(time = info.tem.helpers.dates.vector, obs = obsdat, def=moddat_d, opt=moddat_o);
+        # @show df
+        keysd = (:obs, :def, :opt)
         if !all(isnan, obsdat)
-            # fig = with_theme(theme_ggplot2(), resolution = (1200,400)) do
-                    # draw(obsdat)
-                    # draw(moddat)
-                # end
-            sel_str = join(sel_ind, ",")
+            figin = nothing
+            with_theme(theme_ggplot2(), resolution = (1200,400)) do 
+                figin = Figure()
+                ax = Axis(figin[1,1], ylabel=string(obsvar), xlabel = "time")
+                for ind in eachindex(keysd)
+                    lines!(ax, df[!, keysd[ind]], label=string(keysd[ind]))
+                end
+            end
+            axislegend()
+            sel_str = join(sel_ind, "-")
             filename = "$(obsvar)_$(sel_str).png"
-            @show size(obsdat), size(moddat), "plotting", filename
+            @show filename
+            save("$(obsvar)_$(sel_str).png", figin)
         end
-            # save("$(obsvar)_$(sel_str).png", fig)
     end
 end
-
-# for site in 1:16
-#     df = DataFrame(time = 1:730, gpp = plotdat[end-1][:,site], nee = plotdat[end][:,site], soilw1 = plotdat[2][1,:,site]);
-
-#     for var = (:gpp, :nee, :soilw1)
-#         d = data(df)*mapping(:time, var)*visual(Lines, linewidth=0.5);
-
-#         fig = with_theme(theme_ggplot2(), resolution = (1200,400)) do
-#             draw(d)
-#         end
-#         save("testfig_$(var)_$(site).png", fig)
-#     end
-# end
