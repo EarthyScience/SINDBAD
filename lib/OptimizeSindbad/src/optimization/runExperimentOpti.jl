145ce794dce531dde183481eeecef997766ee9d3
dr-ko <skoirala@bgc-jena.mpg.de>
2023-05-30 11:14:41 +0200

    optimization clean up

--- lib/OptimizeSindbad/src/optimization/runExperimentOpti.jl
+++ lib/OptimizeSindbad/src/optimization/runExperimentOpti.jl
@@ -15,7 +15,7 @@ function runExperiment(info::NamedTuple, forcing::NamedTuple, output, output_var
         @info "runExperiment: do spatial optimization..."
         forc_array = getKeyedArrayFromYaxArray(forcing);
         obs_array = getKeyedArrayFromYaxArray(observations);
-        optim_params = optimizeModelArray(forc_array, output.data, output_vars, obs_array, info.tem, info.optim)
+        optim_params = optimizeModelArray(forc_array, output, output_vars, obs_array, info.tem, info.optim)
         run_output =  optim_params.optim
     end    
     return run_output
