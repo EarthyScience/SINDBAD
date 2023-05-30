0b97260498035beb581ddf083ea2d94e20287644
dr-ko <skoirala@bgc-jena.mpg.de>
2023-05-30 18:09:20 +0200

    clean up and run optim

--- examples/exp_modelOrder/experiment_modelOrder.jl
+++
@@ -1,7 +0,0 @@
-using Revise
-using Sindbad
-experiment_json = "exp_modelOrder/settings_modelOrder/experiment.json"
-
-# do the full experiment at once based purely on json
-run_output = runExperiment(experiment_json);
-
