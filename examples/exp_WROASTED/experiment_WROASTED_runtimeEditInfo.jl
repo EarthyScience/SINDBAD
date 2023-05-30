dc5136a5fe08045d9c99d7bc683a2eca57ea71a6
dr-ko <skoirala@bgc-jena.mpg.de>
2023-05-30 21:51:36 +0200

    patch mismatch of dimensions of model output and obs

--- examples/exp_WROASTED/experiment_WROASTED_runtimeEditInfo.jl
+++ examples/exp_WROASTED/experiment_WROASTED_runtimeEditInfo.jl
@@ -10,15 +10,15 @@ sYear = "1979"
 eYear = "2017"
 
 # inpath = "/Net/Groups/BGI/scratch/skoirala/wroasted/fluxNet_0.04_CLIFF/fluxnetBGI2021.BRK15.DD/data/ERAinterim.v2/daily/DE-Hai.1979.2017.daily.nc"
-inpath = "../data/BE-Vie.1979.2017.daily.nc"
-forcingConfig = "forcing_erai.json"
-# inpath = "../data/DE-2.1979.2017.daily.nc"
-# forcingConfig = "forcing_DE-2.json"
+# inpath = "../data/BE-Vie.1979.2017.daily.nc"
+# forcingConfig = "forcing_erai.json"
+inpath = "../data/DE-2.1979.2017.daily.nc"
+forcingConfig = "forcing_DE-2.json"
 # inpath = "/Net/Groups/BGI/scratch/skoirala/sindbad.jl/examples/data/DE-2.1979.2017.daily.nc"
 # forcingConfig = "forcing_DE-2.json"
 obspath = inpath
 optimize_it = true
-# optimize_it = false
+optimize_it = false
 outpath = nothing
 
 domain = "DE-Hai"
@@ -51,6 +51,9 @@ linit= createLandInit(info.pools, info.tem);
 
 Sindbad.eval(:(error_catcher = []))    
 loc_space_maps, land_init_space, f_one  = prepRunEcosystem(output.data, output.land_init, info.tem.models.forward, forc, info.tem);
+
+observations = getObservation(info, Val(Symbol(info.modelRun.rules.data_backend)));
+obs = getKeyedArrayFromYaxArray(observations);
 
 @time runEcosystem!(output.data, output.land_init, info.tem.models.forward, forc, info.tem, loc_space_maps, land_init_space, f_one)
 @time outcubes = runExperimentOpti(experiment_json; replace_info=replace_info);  
