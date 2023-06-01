d1690ec17633abcac9974d2020a792fb42c2bf3e
dr-ko <skoirala@bgc-jena.mpg.de>
2023-06-01 13:21:17 +0200

    run wroasted and example with _priors

--- examples/exp_WROASTED/experiment_WROASTED.jl
+++ examples/exp_WROASTED/experiment_WROASTED.jl
@@ -1,26 +1,62 @@
+# modification to develop getting priors
 using Revise
 using Sindbad
+#using DistributionFits
+using ForwardSindbad
+using OptimizeSindbad
+# noStackTrace()
+experiment_json = "../exp_WROASTED/settings_WROASTED/experiment.json"
+sYear = "1979"
+eYear = "2017"
 
-noStackTrace()
-#experiment_json = "exp_WROASTED/settings_WROASTED/experiment.json"
-experiment_json = "examples/exp_WROASTED/settings_WROASTED/experiment.json"
+# inpath = "/Net/Groups/BGI/scratch/skoirala/wroasted/fluxNet_0.04_CLIFF/fluxnetBGI2021.BRK15.DD/data/ERAinterim.v2/daily/DE-Hai.1979.2017.daily.nc"
+# forcingConfig = "forcing_erai.json"
+# inpath = "/Net/Groups/BGI/scratch/skoirala/sindbad.jl/examples/data/DE-2.1979.2017.daily.nc"
+# forcingConfig = "forcing_DE-2.json"
+inpath = "../data/BE-Vie.1979.2017.daily.nc"
+forcingConfig = "forcing_erai.json"
+obspath = inpath
+optimize_it = true
+optimize_it = false
+outpath = nothing
 
-run_output = runExperiment(experiment_json);
+domain = "DE-Hai"
+pl = "threads"
+replace_info = Dict(
+    "modelRun.time.sDate" => sYear * "-01-01",
+    "experiment.configFiles.forcing" => forcingConfig,
+    "experiment.domain" => domain,
+    "modelRun.time.eDate" => eYear * "-12-31",
+    "modelRun.flags.runOpti" => optimize_it,
+    "modelRun.flags.calcCost" => true,
+    "spinup.flags.saveSpinup" => false,
+    "modelRun.flags.runSpinup" => true,
+    "modelRun.flags.debugit" => false,
+    "spinup.flags.doSpinup" => true,
+    "forcing.defaultForcing.dataPath" => inpath,
+    "modelRun.output.path" => outpath,
+    "modelRun.mapping.parallelization" => pl,
+    "opti.constraints.oneDataPath" => obspath
+);
+
+info = getExperimentInfo(experiment_json; replace_info=replace_info); # note that this will modify info
+forcing = getForcing(info, Val(Symbol(info.modelRun.rules.data_backend)));
+# spinup_forcing = getSpinupForcing(forcing, info.tem);
+output = setupOutput(info);
+
+forc = getKeyedArrayFromYaxArray(forcing);
+linit= createLandInit(info.pools, info.tem);
+
+# Sindbad.eval(:(error_catcher = []))    
+loc_space_maps, land_init_space, f_one  = prepRunEcosystem(output.data, output.land_init, info.tem.models.forward, forc, info.tem);
+
+observations = getObservation(info, Val(Symbol(info.modelRun.rules.data_backend)));
+obs = getKeyedArrayFromYaxArray(observations);
+
+@time runEcosystem!(output.data, output.land_init, info.tem.models.forward, forc, info.tem, loc_space_maps, land_init_space, f_one)
 
 
-doitstepwise = false
-if doitstepwise
-    info = getConfiguration(experiment_json);
-    info = setupExperiment(info);
-    forcing = getForcing(info, Val(Symbol(info.modelRun.rules.data_backend)));
-    # spinup_forcing = getSpinupForcing(forcing, info.tem);
-    output = setupOutput(info);
+@time outcubes = runExperimentForward(experiment_json; replace_info=replace_info);  
 
-    # forward run
-    outcubes = mapRunEcosystem(forcing, output, info.tem, info.tem.models.forward; max_cache=info.modelRun.rules.yax_max_cache);
 
-    # optimization
-    observations = getObservation(info, Val(Symbol(info.modelRun.rules.data_backend))); 
-    res = mapOptimizeModel(forcing, output, info.tem, info.optim, observations,
-        ; spinup_forcing=nothing, max_cache=info.modelRun.rules.yax_max_cache)
-end
+@time outcubes = runExperimentOpti(experiment_json; replace_info=replace_info);  
