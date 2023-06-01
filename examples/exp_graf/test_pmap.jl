79e6737ee8595f38ebdb71b9e324ddc60680f7e5
dr-ko <skoirala@bgc-jena.mpg.de>
2023-06-01 12:51:25 +0200

    Revert "Revert "Merge branch 'main' into twutz_priors""
    This reverts commit 4e47e321fcc33f0c2eccf6fd46b502663c77844e.

--- examples/exp_graf/test_pmap.jl
+++ examples/exp_graf/test_pmap.jl
@@ -48,12 +48,12 @@ forcing = ForwardSindbad.getForcing(info, Val(Symbol(info.modelRun.rules.data_ba
 output = setupOutput(info);
 
 forc = getKeyedArrayFromYaxArray(forcing);
-# @code_warntype runEcosystem!(output.data, info.tem.models.forward, forc, info.tem);
-# @profview runEcosystem!(output.data, info.tem.models.forward, forc, info.tem);
+# @code_warntype runEcosystem!(output.data, output.land_init, info.tem.models.forward, forc, info.tem);
+# @profview runEcosystem!(output.data, output.land_init, info.tem.models.forward, forc, info.tem);
 # @benchmark $runEcosystem!($output.data, $info.tem.models.forward, $forc, $info.tem)
 # @btime $runEcosystem!($output.data, $info.tem.models.forward, $forc, $info.tem, land_init);
 
-@time runEcosystem!(output.data, info.tem.models.forward, forc, info.tem);
+@time runEcosystem!(output.data, output.land_init, info.tem.models.forward, forc, info.tem);
 
 
 
