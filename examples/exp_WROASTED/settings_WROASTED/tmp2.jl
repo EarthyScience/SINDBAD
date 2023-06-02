a00b807995b4dff78b7c800970048709c09cffb8
dr-ko <skoirala@bgc-jena.mpg.de>
2023-06-02 08:28:00 +0200

    verify updated params

---
+++ examples/exp_WROASTED/settings_WROASTED/tmp2.jl
@@ -0,0 +1,122 @@
+(rainSnow_Tair{Float32}
+  Tair_thres: Float32 0.0f0
+, PET_Lu2005{Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32}
+  α: Float32 1.26f0
+  svp_1: Float32 0.2f0
+  svp_2: Float32 0.00738f0
+  svp_3: Float32 0.8072f0
+  svp_4: Float32 7.0f0
+  svp_5: Float32 0.000116f0
+  sh_cp: Float32 0.001013f0
+  elev: Float32 0.0f0
+  pres_sl: Float32 101.3f0
+  pres_elev: Float32 0.01055f0
+  λ_base: Float32 2.501f0
+  λ_tair: Float32 0.002361f0
+  γ_resistance: Float32 0.622f0
+  Δt: Float32 2.0f0
+  G_base: Float32 4.2f0
+, ambientCO2_forcing(), soilTexture_forcing(), soilProperties_Saxton2006{Float32, Float32, Float32, Float32, Float32}
+  DF: Float32 1.0f0
+  Rw: Float32 0.0f0
+  matricSoilDensity: Float32 2.65f0
+  gravelDensity: Float32 2.65f0
+  EC: Float32 36.0f0
+, soilWBase_uniform(), getPools_simple(), rootMaximumDepth_fracSoilD{Float32}
+  fracRootD2SoilD: Float32 0.8f0
+, rootFraction_expCvegRoot{Float32, Float32, Float32}
+  k_cVegRoot: Float32 0.001f0
+  fracRoot2SoilD_max: Float32 0.95f0
+  fracRoot2SoilD_min: Float32 0.1f0
+, fAPAR_cVegLeaf{Float32}
+  kEffExt: Float32 0.03876686f0
+, LAI_cVegLeaf{Float32}
+  SLA: Float32 0.016f0
+, treeFraction_constant{Float32}
+  constantTreeFrac: Float32 0.6458481f0
+, snowFraction_HTESSEL{Float32}
+  CoverParam: Float32 15.0f0
+, snowMelt_TairRn{Float32, Float32}
+  melt_T: Float32 0.01f0
+  melt_Rn: Float32 3.0f0
+, runoffSaturationExcess_Bergstroem1992{Float32}
+  β: Float32 0.66225874f0
+, runoffOverland_Sat(), runoffSurface_all(), runoffBase_Zhang2008{Float32}
+  bc: Float32 0.02f0
+, percolation_WBP(), evaporation_fAPAR{Float32, Float32}
+  α: Float32 1.0922176f0
+  supLim: Float32 0.08f0
+, drainage_dos{Float32}
+  dos_exp: Float32 1.1f0
+, capillaryFlow_VanDijk2010{Float32}
+  max_frac: Float32 0.95f0
+, groundWRecharge_dos{Float32}
+  dos_exp: Float32 2.1574278f0
+, groundWSoilWInteraction_VanDijk2010{Float32}
+  max_fraction: Float32 0.5f0
+, vegAvailableWater_rootFraction(), transpirationSupply_wAWC{Float32}
+  tranFrac: Float32 0.1f0
+, gppPotential_Monteith{Float32}
+  εmax: Float32 1.540383f0
+, gppDiffRadiation_GSI{Float32, Float32, Float32}
+  fR_τ: Float32 0.2f0
+  fR_slope: Float32 58.0f0
+  fR_base: Float32 59.78f0
+, gppDirRadiation_none(), gppAirT_CASA{Float32, Float32, Float32, Float32}
+  Topt: Float32 20.263027f0
+  ToptA: Float32 0.11422756f0
+  ToptB: Float32 0.43978313f0
+  Texp: Float32 10.0f0
+, gppVPD_PRELES{Float32, Float32, Float32, Float32}
+  κ: Float32 0.29698515f0
+  Cκ: Float32 3.5623498f0
+  Ca0: Float32 380.0f0
+  Cm: Float32 400.0f0
+, gppDemand_mult(), gppSoilW_Stocker2020{Float32, Float32}
+  q: Float32 2.2118213f0
+  θstar: Float32 0.1f0
+, WUE_expVPDDayCo2{Float32, Float32, Float32, Float32}
+  WUEatOnehPa: Float32 6.644842f0
+  κ: Float32 0.29329106f0
+  Ca0: Float32 380.0f0
+  Cm: Float32 500.0f0
+, gpp_coupled(), transpiration_coupled(), rootWaterUptake_proportion(), cCycleBase_GSI{Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Matrix{Float32}, Vector{Float32}, Float32, Float32}
+  annk_Root: Float32 2.3338482f0
+  annk_Wood: Float32 0.001f0
+  annk_Leaf: Float32 1.0f0
+  annk_Reserve: Float32 1.0f-11
+  annk_LitSlow: Float32 39.0f0
+  annk_LitFast: Float32 100.26403f0
+  annk_SoilSlow: Float32 2.0f0
+  annk_SoilOld: Float32 0.04187936f0
+  cFlowA: Array{Float32}((8, 8)) Float32[-1.0 0.0 … 0.0 0.0; 0.0 -1.0 … 0.0 0.0; … ; 0.0 0.0 … -1.0 0.0; 0.0 0.0 … 1.0 -1.0]
+  C2Nveg: Array{Float32}((4,)) Float32[25.0, 260.0, 260.0, 10.0]
+  etaH: Float32 0.01f0
+  etaA: Float32 1.0f0
+, cCycleDisturbance_constant{Float32}
+  carbon_remain: Float32 10.0f0
+, cTauSoilT_Q10{Float32, Float32, Float32}
+  Q10: Float32 1.05f0
+  Tref: Float32 30.0f0
+  Q10_base: Float32 10.0f0
+, cTauSoilW_GSI{Float32, Float32, Float32, Float32, Float32}
+  Wopt: Float32 87.64241f0
+  WoptA: Float32 0.2f0
+  WoptB: Float32 0.3f0
+  Wexp: Float32 10.0f0
+  frac2perc: Float32 100.0f0
+, cTauLAI_none(), cTauSoilProperties_none(), cTauVegProperties_none(), cTau_mult(), aRespirationAirT_Q10{Float32, Float32, Float32}
+  Q10_RM: Float32 2.5961406f0
+  Tref_RM: Float32 20.0f0
+  Q10_base: Float32 10.0f0
+, cAllocationLAI_none(), cAllocationRadiation_gpp(), cAllocationSoilW_gpp(), cAllocationSoilT_gpp(), cAllocationNutrients_none(), cAllocation_GSI(), cAllocationTreeFraction_Friedlingstein1999{Float32}
+  Rf2Rc: Float32 1.0f0
+, aRespiration_Thornley2000A{Float32, Float32}
+  RMN: Float32 0.075402394f0
+  YG: Float32 0.92704415f0
+, cFlowSoilProperties_none(), cFlowVegProperties_none(), cFlow_GSI{Float32, Float32, Float32, Float32}
+  LR2ReSlp: Float32 0.35055f0
+  Re2LRSlp: Float32 0.25480357f0
+  kShed: Float32 0.31024346f0
+  f_τ: Float32 0.5513162f0
+, cCycle_simple(), evapotranspiration_sum(), runoff_sum(), wCycle_combined(), totalTWS_sumComponents(), waterBalance_simple())
