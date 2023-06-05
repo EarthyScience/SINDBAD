31c21b297d32d0ed3eb21f56ad695acb0e519a2c
dr-ko <skoirala@bgc-jena.mpg.de>
2023-06-06 00:31:31 +0200

    garbage garbage

--- lib/ForwardSindbad/src/Ecosystem/runEcosystemArray.jl
+++ lib/ForwardSindbad/src/Ecosystem/runEcosystemArray.jl
@@ -126,6 +126,7 @@ function timeLoopForward!(loc_output, forward_models, forcing, out, tem_variable
             @time out = runModels!(out, f, forward_models, tem_helpers, Val(:debugit))
             println("-------------")
             @show "all models"
+            # @code_warntype runModels!(out, f, forward_models, tem_helpers)
             @time out = runModels!(out, f, forward_models, tem_helpers)
             println("-------------")
             @show "out"
