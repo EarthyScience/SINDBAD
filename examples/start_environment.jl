68fefeab165672a911ca751c4a590ecf1117dfd4
skoirala <skoirala@bgc-jena.mpg.de>
2023-06-13 09:56:17 +0200

    remove dep on hybrid lib

--- examples/start_environment.jl
+++ examples/start_environment.jl
@@ -1,9 +1,10 @@
 # run(`module load proxy`)
-run(`cp -f ../base_experiment.toml Project.toml`)
+# run(`cp -f ../base_experiment.toml Project.toml`)
 using Pkg
+# dev ../../ ../../lib/ForwardSindbad ../../lib/OptimizeSindbad ../../lib/HybridSindbad/
 Pkg.activate(".")
 Pkg.develop(path="../../")
 Pkg.develop(path="../../lib/ForwardSindbad")
 Pkg.develop(path="../../lib/OptimizeSindbad")
-Pkg.develop(path="../../lib/HybridSindbad")
 Pkg.instantiate()
+Pkg.add("Revise")
