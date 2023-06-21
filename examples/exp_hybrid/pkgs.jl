fd978d866b484e68b762a2d5c4a79700a0167e30
Lazaro Alonso <lazarus.alon@gmail.com>
2023-06-21 10:03:41 +0200

    running hybrid, with latest main, as of now

---
+++ examples/exp_hybrid/pkgs.jl
@@ -0,0 +1,9 @@
+using YAXArrays, Zarr, YAXArrayBase, AxisKeys
+using Flux, Optimisers, Zygote
+using ForwardDiff
+using Sindbad
+using ForwardSindbad
+using OptimizeSindbad
+using Random
+using ProgressMeter
+include("new_functions.jl")
---
+++ examples/exp_plots/plot_outdata.jl
@@ -0,0 +1,60 @@
+using Revise
+using ForwardDiff
+
+using Sindbad
+using ForwardSindbad
+using ForwardSindbad: timeLoopForward
+using OptimizeSindbad
+#using AxisKeys: KeyedArray as KA
+#using Lux, Zygote, Optimisers, ComponentArrays, NNlib
+#using Random
+noStackTrace()
+#Random.seed!(7)
+
+
+experiment_json = "../exp_gradWroasted/settings_gradWroasted/experiment.json"
+info = getExperimentInfo(experiment_json);#; replace_info=replace_info); # note that this will modify info
+
+info, forcing = getForcing(info, Val{:zarr}());
+
+# Sindbad.eval(:(error_catcher = []));
+land_init = createLandInit(info.pools, info.tem);
+output = setupOutput(info);
+forc = getKeyedArrayFromYaxArray(forcing);
+observations = getObservation(info, Val(Symbol(info.modelRun.rules.data_backend)));
+obs = getKeyedArrayFromYaxArray(observations);
+
+@time loc_space_names, loc_space_inds, loc_forcings, loc_outputs, land_init_space, f_one = prepRunEcosystem(output.data, output.land_init, info.tem.models.forward, forc, forcing.sizes, info.tem);
+
+@time runEcosystem!(output.data, info.tem.models.forward, forc, info.tem, loc_space_names, loc_space_inds, loc_forcings, loc_outputs, land_init_space, f_one)
+
+using GLMakie
+using Colors
+Makie.inline!(false)
+lines(1:10)
+
+names_pair = Dict(output.variables .=> 1:4)
+
+var_name = Observable(1)
+gpp = @lift(output.data[$var_name]);
+s = Observable(9)
+gpp_site = @lift($gpp[:,1,$s])
+
+fig = Figure(resolution = (1200, 600))
+menu = Menu(fig, options = output.variables,
+    cell_color_hover = RGB(0.7, 0.3, 0.25),
+    cell_color_active= RGB(0.2, 0.3, 0.5))
+ax = Axis(fig[1,1])
+lines!(ax, gpp_site)
+
+fig[1, 1, TopRight()] = vgrid!(
+    Label(fig, "Variables", width = nothing,
+        font=:bold, fontsize=18, color=:orangered),
+    menu; tellheight = false, width = 150, valign = :top)
+sl = Slider(fig[0, 1], range = 1:10, startvalue = 9, color_active_dimmed = RGB(0.81, 0.81, 0.2))
+connect!(s, sl.value)
+on(menu.selection) do s
+    var_name[] = names_pair[s]
+    autolimits!(ax)
+end
+fig
