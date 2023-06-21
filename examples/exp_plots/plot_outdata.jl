63202a2696e872b68c61b1362a216b625f4321b3
Lazaro Alonso <lazarus.alon@gmail.com>
2023-06-21 12:16:31 +0200

    small fix

--- examples/exp_plots/plot_outdata.jl
+++ examples/exp_plots/plot_outdata.jl
@@ -26,7 +26,7 @@ obs = getKeyedArrayFromYaxArray(observations);
 
 @time loc_space_maps, loc_space_names, loc_space_inds, loc_forcings, loc_outputs, land_init_space, f_one = prepRunEcosystem(output.data, output.land_init, info.tem.models.forward, forc, forcing.sizes, info.tem);
 
-@time runEcosystem!(output.data, info.tem.models.forward, forc, info.tem, loc_space_maps, loc_space_names, loc_space_inds, loc_forcings, loc_outputs, land_init_space, f_one)
+@time runEcosystem!(output.data, info.tem.models.forward, forc, info.tem, loc_space_names, loc_space_inds, loc_forcings, loc_outputs, land_init_space, f_one)
 
 using GLMakie
 using Colors
