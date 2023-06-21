0ce715bfe075f14ecde33e2e5e80266a8697262b
Lazaro Alonso <lazarus.alon@gmail.com>
2023-06-21 17:32:18 +0200

    prev synt with new loss

--- examples/exp_hybrid/running_mwe.jl
+++ examples/exp_hybrid/running_mwe.jl
@@ -7,8 +7,20 @@ include("obs_synt.jl")
 include("nn_machine.jl")
 
 nn_args = (; n_bs_feat, n_neurons, n_params, extra_layer=2, nn_opt = Optimisers.Adam(),)
-grads_args = (; tblParams, sites_f, land_init_space, output, forc, obs_synt, forward, helpers, spinup, models, out_vars, f_one);
+
+tem_variables = info.tem.variables
+tem_optim = info.optim
+out_variables = info.tem.variables
+
+tem_helpers =info.tem.helpers
+tem_spinup=info.tem.spinup
+tem_models=info.tem.models
+tem_variables=info.tem.variables
+tem_optim=info.optim
+out_variables=output.variables
+
+grads_args = (; tblParams, sites_f, land_init_space, output, forc, obs_synt, forward, tem_helpers, tem_spinup, tem_models, tem_variables, tem_optim, out_variables, f_one);
 
 x_args = (; shuffle=true, bs=16, sites=sites)
 
-nn_machine(nn_args, x_args,  xfeatures; nepochs=10)
+nn_machine(nn_args, x_args,  xfeatures, grads_args; nepochs=2)
