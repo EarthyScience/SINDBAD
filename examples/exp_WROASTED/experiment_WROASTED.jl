30295a160d9dbe3d38cb201087ee85a6781ad10d
dr-ko <skoirala@bgc-jena.mpg.de>
2023-06-22 10:39:27 +0200

    add formatter script

--- examples/exp_WROASTED/experiment_WROASTED.jl
+++ examples/exp_WROASTED/experiment_WROASTED.jl
@@ -132,4 +132,5 @@ for (vi, v) ∈ enumerate(out_vars)
 end
 
 # using JuliaFormatter
+# format(".", MinimalStyle(), margin=100, always_for_in=true, for_in_replacement="∈", format_docstrings=true, yas_style_nesting=true, import_to_using=true, remove_extra_newlines=true, trailing_comma=false)
 # format(".", margin = 100, always_for_in=true, for_in_replacement="∈", format_docstrings=true, yas_style_nesting=true, import_to_using=true, remove_extra_newlines=true)
