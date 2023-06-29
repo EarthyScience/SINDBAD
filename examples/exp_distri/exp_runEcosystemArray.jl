e898b7d578bb4f87725883e577b033958e9e8be5
dr-ko <skoirala@bgc-jena.mpg.de>
2023-06-29 17:42:27 +0200

    steady state examples with nlsove

--- examples/exp_distri/exp_runEcosystemArray.jl
+++ examples/exp_distri/exp_runEcosystemArray.jl
@@ -17,13 +17,8 @@ forc = getKeyedArrayFromYaxArray(forcing);
 observations = getObservation(info, Val(Symbol(info.modelRun.rules.data_backend)));
 obs = getKeyedArrayFromYaxArray(observations);
 
-loc_space_maps, loc_space_names, loc_space_inds, loc_forcings, loc_outputs, land_init_space, f_one =
-    prepRunEcosystem(output.data,
-        output.land_init,
-        info.tem.models.forward,
-        forc,
-        forcing.sizes,
-        info.tem);
+loc_space_maps, loc_space_names, loc_space_inds, loc_forcings, loc_outputs, land_init_space, tem_vals, f_one =
+    prepRunEcosystem(output, forc, info.tem);
 
 @time runEcosystem!(output.data,
     info.tem.models.forward,
