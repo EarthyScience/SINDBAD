dabd65e66a5b4627a30047f08e8d679aa30120d2
Lazaro Alonso <lazarus.alon@gmail.com>
2023-06-30 18:49:42 +0200

    calculate gradient for minimized wroasted

--- src/Models/cTauLAI/cTauLAI_none.jl
+++ src/Models/cTauLAI/cTauLAI_none.jl
@@ -5,7 +5,7 @@ struct cTauLAI_none <: cTauLAI end
 function define(o::cTauLAI_none, forcing, land, helpers)
 
     ## calculate variables
-    p_kfLAI = zero(land.pools.cEco) .+ helpers.numbers.𝟙 #(ineficient, should be pix zix_veg)
+    p_kfLAI = zero(land.pools.cEco) .+ helpers.numbers.𝟙
 
     ## pack land variables
     @pack_land p_kfLAI => land.cTauLAI
