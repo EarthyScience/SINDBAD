5dab5fc1a692758d24efebcbfc2a594c42aba326
dr-ko <skoirala@bgc-jena.mpg.de>
2023-07-08 22:30:38 +0200

    job array

--- examples/exp_WROASTED/wroasted_slurm_jobarray.sh
+++ examples/exp_WROASTED/wroasted_slurm_jobarray.sh
@@ -2,13 +2,11 @@
 #SBATCH --job-name wroasted
 #SBATCH -o ./run_logs/wroasted-%A_%a.o.log
 #SBATCH -e ./run_logs/wroasted-%A_%a.e.log
-#SBATCH -p interactive
+#SBATCH -p work
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-205%100
 
-module load julia/1.7.3
-
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
-julia --project=../ experiment_WROASTED_jobarray.jl
+/Net/Groups/BGI/scratch/skoirala/.juliaup/bin/julia --project=../exp_distri experiment_WROASTED_jobarray.jl
