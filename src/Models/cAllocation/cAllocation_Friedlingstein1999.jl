9518c77719dc3edbce467de048ea2e85502356ba
SujanKoirala <skoirala@bgc-jena.mpg.de>
2023-07-10 17:12:54 +0200

    fix allocation and respiration models

--- src/Models/cAllocation/cAllocation_Friedlingstein1999.jl
+++ src/Models/cAllocation/cAllocation_Friedlingstein1999.jl
@@ -13,9 +13,23 @@ function define(p_struct::cAllocation_Friedlingstein1999, forcing, land, helpers
 
     ## instantiate variables
     c_allocation = zero(land.pools.cEco) #sujan
-
+    c_allocation_to_veg = zero(land.pools.cEco)
+    cVeg_names = (:cVegRoot, :cVegWood, :cVegLeaf)
+    cVeg_nzix = []
+    cVeg_zix = []
+    for cpName ∈ cVeg_names
+        zix = getzix(getfield(land.pools.carbon, cpName), helpers.pools.zix, cpName)
+        nZix = helpers.numbers.sNT(length(zix))
+        push!(cVeg_nzix, nZix)
+        push!(cVeg_zix, zix)
+    end
+    cVeg_nzix = Tuple(cVeg_nzix)
+    cVeg_zix = Tuple(cVeg_zix)
     ## pack land variables
-    @pack_land c_allocation => land.states
+    @pack_land begin
+        c_allocation => land.states
+        (cVeg_names, cVeg_nzix, cVeg_zix, c_allocation_to_veg) => land.cAllocation
+    end
     return land
 end
 
@@ -26,25 +40,30 @@ function compute(p_struct::cAllocation_Friedlingstein1999, forcing, land, helper
     ## unpack land variables
     @unpack_land begin
         c_allocation ∈ land.states
-        𝟙 ∈ helpers.numbers
-    end
-    ## unpack land variables
-    @unpack_land begin
+        (cVeg_names, cVeg_nzix, cVeg_zix, c_allocation_to_veg) ∈ land.cAllocation
         c_allocation_f_W_N ∈ land.cAllocationNutrients
         c_allocation_f_LAI ∈ land.cAllocationLAI
     end
+    ## unpack land variables
     # allocation to root; wood & leaf
-    cVegRoot = ro * (RelY + 𝟙) * c_allocation_f_LAI / (c_allocation_f_LAI + RelY * c_allocation_f_W_N)
-    cVegWood = so * (RelY + 𝟙) * c_allocation_f_W_N / (RelY * c_allocation_f_LAI + c_allocation_f_W_N)
-    cVegLeaf = 𝟙 - cVegRoot - cVegWood
-    cf2 = (; cVegLeaf=cVegLeaf, cVegWood=cVegWood, cVegRoot=cVegRoot)
+    o_one = one(ro)
+    a_cVegRoot = ro * (RelY + o_one) * c_allocation_f_LAI / (c_allocation_f_LAI + RelY * c_allocation_f_W_N)
+    a_cVegWood = so * (RelY + o_one) * c_allocation_f_W_N / (RelY * c_allocation_f_LAI + c_allocation_f_W_N)
+    a_cVegLeaf = o_one - cVegRoot - cVegWood
+
+    @rep_elem a_cVegRoot => (c_allocation_to_veg, 1, :cEco)
+    @rep_elem a_cVegWood => (c_allocation_to_veg, 2, :cEco)
+    @rep_elem a_cVegLeaf => (c_allocation_to_veg, 3, :cEco)
+
 
     # distribute the allocation according to pools
-    cVeg_names = (:cVegRoot, :cVegWood, :cVegLeaf)
-    for cpName ∈ cVeg_names
-        zix = getzix(getfield(pools.carbon, cpName), helpers.pools.zix, cpName)
-        nZix = length(zix) * 𝟙
-        c_allocation[zix] .= getfield(cf2, cpName) / nZix
+    for cl in eachindex(cVeg_names)
+        zix = cVeg_zix[cl]
+        nZix = cVeg_nzix[cl]
+        for ix ∈ zix
+            c_allocation_to_veg_ix = c_allocation_to_veg[cl] / nZix
+            @rep_elem c_allocation_to_veg_ix => (c_allocation, ix, :cEco)
+        end
     end
 
     ## pack land variables
