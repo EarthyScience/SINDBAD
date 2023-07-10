8bd4cc956ab6cd4aca9468a3d67e23d0e435d51d
dr-ko <skoirala@bgc-jena.mpg.de>
2023-07-10 11:49:29 +0200

    fix hardcoded ones in losses

--- lib/OptimizeSindbad/src/optimization/losses.jl
+++ lib/OptimizeSindbad/src/optimization/losses.jl
@@ -17,10 +17,10 @@ end
 
 Relative normalized model absolute error
 
-``nmae1r = \\frac{(|y - ŷ|)}{1.0 + y}``
+``nmae1r = \\frac{(|y - ŷ|)}{one(eltype(ŷ)) + y}``
 """
 function loss(y, yσ, ŷ, ::Val{:nmae1r})
-    nmae1r = abs(y - ŷ) / (typeof(ŷ)(1.0) + y)
+    nmae1r = abs(y - ŷ) / (oftype(ŷ, one(eltype(ŷ))) + y)
     return nmae1r
 end
 
@@ -29,12 +29,12 @@ end
 
 Relative normalized model absolute error
 
-``nmae1r = \\frac{mean(|y - ŷ|)}{1.0 + mean(y)}``
+``nmae1r = \\frac{mean(|y - ŷ|)}{one(eltype(ŷ)) + mean(y)}``
 """
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nmae1r})
     μ_y = mean(y)
     μ_ŷ = mean(ŷ)
-    nmae1r = abs(μ_ŷ - μ_y) / (eltype(ŷ)(1.0) + μ_y)
+    nmae1r = abs(μ_ŷ - μ_y) / (one(eltype(ŷ)) + μ_y)
     return nmae1r
 end
 
@@ -43,56 +43,56 @@ function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:p
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:pcor2})
-    pcor2 = loss(y, yσ, ŷ, Val(:pcor))^2.0
-    return pcor2
+    pcor = loss(y, yσ, ŷ, Val(:pcor))
+    return pcor * pcor
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:pcor2inv})
-    pcor2inv = 1.0 - loss(y, yσ, ŷ, Val(:pcor2))
+    pcor2inv = one(eltype(ŷ)) - loss(y, yσ, ŷ, Val(:pcor2))
     return pcor2inv
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nseσ})
     nse =
-        1.0 .-
+        one(eltype(ŷ)) .-
         sum(abs2.((y .- ŷ) ./ yσ)) /
         sum(abs2.((y .- mean(y)) ./ yσ))
     return nse
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nseσinv})
-    nseinv = 1.0 - loss(y, yσ, ŷ, Val(:nseσ))
+    nseinv = one(eltype(ŷ)) - loss(y, yσ, ŷ, Val(:nseσ))
     return nseinv
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nnseσ})
     nse_v = loss(y, yσ, ŷ, Val(:nseσ))
-    nnse = 1.0 / (2.0 - nse_v)
+    nnse = one(eltype(ŷ)) / (one(eltype(ŷ)) + one(eltype(ŷ)) - nse_v)
     return nnse
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nnseσinv})
-    nnseinv = 1.0 - loss(y, yσ, ŷ, Val(:nnseσ))
+    nnseinv = one(eltype(ŷ)) - loss(y, yσ, ŷ, Val(:nnseσ))
     return nnseinv
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nse})
-    nse = 1.0 .- sum(abs2.((y .- ŷ))) / sum(abs2.((y .- mean(y))))
+    nse = one(eltype(ŷ)) .- sum(abs2.((y .- ŷ))) / sum(abs2.((y .- mean(y))))
     return nse
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nseinv})
-    nseinv = 1.0 - loss(y, yσ, ŷ, Val(:nse))
+    nseinv = one(eltype(ŷ)) - loss(y, yσ, ŷ, Val(:nse))
     return nseinv
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nnse})
     nse_v = loss(y, yσ, ŷ, Val(:nse))
-    nnse = 1.0 / (2.0 - nse_v)
+    nnse = one(eltype(ŷ)) / (one(eltype(ŷ)) + one(eltype(ŷ)) - nse_v)
     return nnse
 end
 
 function loss(y::AbstractArray, yσ::AbstractArray, ŷ::AbstractArray, ::Val{:nnseinv})
-    nnseinv = 1.0 - loss(y, yσ, ŷ, Val(:nnse))
+    nnseinv = one(eltype(ŷ)) - loss(y, yσ, ŷ, Val(:nnse))
     return nnseinv
 end
