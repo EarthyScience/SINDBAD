39314f9ea72be2721f8af3e59a5d057e6b56a533
skoirala <skoirala@bgc-jena.mpg.de>
2023-07-16 20:48:19 +0200

    submit wroasted

--- examples/exp_WROASTED/wroasted_slurm_jobarray.sh
+++ examples/exp_WROASTED/wroasted_slurm_jobarray.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=3000
-#SBATCH --array=1-205%100
+#SBATCH --array=1-205%205
 
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 /Net/Groups/Services/HPC_22/apps/julia/julia-1.9.0/bin/julia --project=../exp_distri --heap-size-hint=2G experiment_WROASTED_jobarray.jl
