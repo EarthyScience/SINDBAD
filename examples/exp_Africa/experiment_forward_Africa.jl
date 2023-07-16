804842493342b337e6abed0a5bcc585d9914f90f
dr-ko <skoirala@bgc-jena.mpg.de>
2023-07-16 16:11:54 +0200

    quick fixes

--- examples/exp_Africa/experiment_forward_Africa.jl
+++ examples/exp_Africa/experiment_forward_Africa.jl
@@ -13,7 +13,7 @@ optimize_it = false;
 
 replace_info_spatial = Dict("experiment.domain" => domain * "_spatial",
     "model_run.flags.run_optimization" => optimize_it,
-    "model_run.flags.run_forward_and_cost" => true,
+    "model_run.flags.run_forward_and_cost" => false,
     "model_run.mapping.yaxarray" => [],
     "model_run.mapping.run_ecosystem" => ["time", "latitude", "longitude"],
     "model_run.flags.spinup.run_spinup" => true,
@@ -50,4 +50,4 @@ plotdat = output.data;
 # pd = mean(plotdat[1], dims=1)
 fig, ax, obj = heatmap(mean(plotdat[1]; dims=1)[1, 1, :, :])
 Colorbar(fig[1, 2], obj)
-save("afroca_gpp.png", fig)
+save(joinpath(info.output.figure, "africa_gpp.png"), fig)
