2b8eb3865e095a7fdbaf8ee8538c1b9a5266a4a9
skoirala <skoirala@bgc-jena.mpg.de>
2023-07-20 08:52:25 +0200

    check models

--- src/Models/autoRespiration/autoRespiration.jl
+++ src/Models/autoRespiration/autoRespiration.jl
@@ -1,11 +1,11 @@
-export aRespiration
+export autoRespiration
 
-abstract type aRespiration <: LandEcosystem end
+abstract type autoRespiration <: LandEcosystem end
 
-include("aRespiration_none.jl")
-include("aRespiration_Thornley2000A.jl")
-include("aRespiration_Thornley2000B.jl")
-include("aRespiration_Thornley2000C.jl")
+include("autoRespiration_none.jl")
+include("autoRespiration_Thornley2000A.jl")
+include("autoRespiration_Thornley2000B.jl")
+include("autoRespiration_Thornley2000C.jl")
 
 @doc """
 Determine growth and maintenance respiration -> npp
@@ -16,4 +16,4 @@ Determine growth and maintenance respiration -> npp
  - Thornley2000B: Precomputations to estimate autotrophic respiration as maintenance + growth respiration according to Thornley & Cannell (2000): MODEL B - growth respiration is given priority (check Fig.1 of the paper). Computes the km [maintenance [respiration] coefficient]
  - Thornley2000C: Precomputations to estimate autotrophic respiration as maintenance + growth respiration according to Thornley & Cannell (2000): MODEL C - growth, degradation & resynthesis view of respiration (check Fig.1 of the paper). Computes the km [maintenance [respiration] coefficient]
 """
-aRespiration
+autoRespiration
