eeca61cee617ba1538ce1eb84a84d8d4f0878309
dr-ko <skoirala@bgc-jena.mpg.de>
2023-07-29 06:07:58 +0200

    fix run scripts

--- examples/exp_WROASTED/wroasted_jobarray_erai.sh
+++ examples/exp_WROASTED/wroasted_jobarray_erai.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name wroasted
+#SBATCH --job-name e_wr
 #SBATCH -o ./run_logs/wroasted-%A_%a.o.log
 #SBATCH -e ./run_logs/wroasted-%A_%a.e.log
 #SBATCH -p big
@@ -7,7 +7,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=3000
 #SBATCH --array=1-205%205
+#SBATCH --time=3-00:00:00
 
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-/Net/Groups/Services/HPC_22/apps/julia/julia-1.9.0/bin/julia --project=../exp_distri --heap-size-hint=3G WROASTED_jobarray_erai.jl
+/Net/Groups/Services/HPC_22/apps/julia/julia-1.9.0/bin/julia --project=../exp_distri --heap-size-hint=5G WROASTED_jobarray_erai.jl
 # /Net/Groups/BGI/scratch/skoirala/.juliaup/bin/julia --project=../exp_distri --heap-size-hint=2G experiment_WROASTED_jobarray.jl
