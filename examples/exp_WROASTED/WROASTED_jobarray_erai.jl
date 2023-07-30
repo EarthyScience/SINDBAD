e195ab6255247a294b210c7c010a4b6d708c4a73
dr-ko <skoirala@bgc-jena.mpg.de>
2023-07-30 19:47:35 +0200

    get ready to run

--- examples/exp_WROASTED/WROASTED_jobarray_erai.jl
+++ examples/exp_WROASTED/WROASTED_jobarray_erai.jl
@@ -93,10 +93,10 @@ opti_sets = Dict(
 
 forcingConfig = "forcing_$(forcing_set).json";
 pl = "threads"
-exp_main = "wroasted_no_unc_nse"
+exp_main = "wroasted_no_unc_nnse"
 
-# opti_set = (:set1, :set2, :set3, :set4, :set5, :set6, :set7, :set9, :set10,)
-opti_set = (:set1,)
+opti_set = (:set1, :set2, :set3, :set4, :set5, :set6, :set7, :set9, :set10,)
+# opti_set = (:set1,)
 optimize_it = true;
 for o_set in opti_set
     path_output = "/Net/Groups/BGI/scratch/skoirala/$(exp_main)_sjindbad/$(forcing_set)/$(o_set)"
