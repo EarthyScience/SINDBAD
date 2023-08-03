566d774a6e939b4db39f27a68272637c29a1ca5c
dr-ko <skoirala@bgc-jena.mpg.de>
2023-08-03 11:14:55 +0200

    here and there

--- lib/OptimizeSindbad/src/optimization/temporalAggregation.jl
+++ lib/OptimizeSindbad/src/optimization/temporalAggregation.jl
@@ -7,38 +7,18 @@ function Base.view(x::AbstractArray, v::Sindbad.TimeAggregator; dim=1)
     Sindbad.TimeAggregatorViewInstance{t,ndims(x),dim,typeof(x),typeof(v)}(x, v, Val{dim}())
 end
 
-function doAnomaly(dat, ::Val{:mean})
-    return dat .- mean(dat)
-end
-
-function doAnomaly(dat, ::Val{:nanmean})
-    return dat .- Sindbad.nanmean(dat)
-end
-
-function doMean(dat, ::Val{:mean})
-    return mean(dat)
-end
-
-function doMean(dat, ::Val{:nanmean})
-    return Sindbad.nanmean(dat)
-end
-
 # works for everything for which only aggregation is needed
 function temporalAggregation(dat, temporal_aggregator, dim = 1)
     dat = view(dat, temporal_aggregator, dim=dim)
     return dat
 end
 
-function temporalAggregation(dat, temporal_aggregator)
-    return temporalAggregation(dat, first(temporal_aggregator))
+function temporalAggregation(dat, temporal_aggregators, ::Val{:no_diff})
+    return temporalAggregation(dat, first(temporal_aggregators))
 end
 
-function temporalAggregation(dat, temporal_aggregator, ::Val{:no_diff})
-    return temporalAggregation(dat, first(temporal_aggregator))
-end
-
-function temporalAggregation(dat, temporal_aggregator, ::Val{:diff})
-    dat_agg = temporalAggregation(dat, first(temporal_aggregator))
-    dat_agg_to_remove = temporalAggregation(dat, last(temporal_aggregator))
+function temporalAggregation(dat, temporal_aggregators, ::Val{:diff})
+    dat_agg = temporalAggregation(dat, first(temporal_aggregators))
+    dat_agg_to_remove = temporalAggregation(dat, last(temporal_aggregators))
     return dat_agg .- dat_agg_to_remove
 end
