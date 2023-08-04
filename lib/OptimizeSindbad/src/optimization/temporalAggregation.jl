9640966d4fac6fbe6a9a75d367d155859ea2a9b8
dr-ko <skoirala@bgc-jena.mpg.de>
2023-08-04 13:28:27 +0200

    instantiate arrays for time aggegator

--- lib/OptimizeSindbad/src/optimization/temporalAggregation.jl
+++ lib/OptimizeSindbad/src/optimization/temporalAggregation.jl
@@ -7,9 +7,29 @@ function Base.view(x::AbstractArray, v::Sindbad.TimeAggregator; dim=1)
     Sindbad.TimeAggregatorViewInstance{t,ndims(x),dim,typeof(x),typeof(v)}(x, v, Val{dim}())
 end
 
+
+
+function getTimeAggrArray(_dat::AbstractArray{T,2}) where {T}
+    return _dat[:, :]
+end
+
+function getTimeAggrArray(_dat::AbstractArray{T,3}) where {T}
+    return _dat[:, :, :]
+end
+
+function getTimeAggrArray(mod_dat::AbstractArray{T,4}) where {T}
+    return _dat[:, :, :, :]
+end
+
+
 # works for everything for which only aggregation is needed
-function temporalAggregation(dat, temporal_aggregator, dim = 1)
+function temporalAggregation(dat, temporal_aggregator::Sindbad.TimeAggregator, dim = 1)
     dat = view(dat, temporal_aggregator, dim=dim)
+    return getTimeAggrArray(dat)
+end
+
+# works for everything for which only aggregation is needed
+function temporalAggregation(dat, temporal_aggregator::Nothing, dim = 1)
     return dat
 end
 
