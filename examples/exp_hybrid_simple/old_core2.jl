8614823842b1ad4e6ed7818a176d09726be8fc97
dr-ko <skoirala@bgc-jena.mpg.de>
2023-08-06 21:44:32 +0200

    loss vector

--- examples/exp_hybrid_simple/old_core2.jl
+++ examples/exp_hybrid_simple/old_core2.jl
@@ -99,8 +99,8 @@ function get_loc_loss(
         tem_models,
         loc_land_init,
         f_one)
-    lossVec = getLossVector(loc_obs, landWrapper(big_land), tem_optim)
-    t_loss = combineLoss(lossVec, Val{:sum}())
+    loss_vector = getLossVector(loc_obs, landWrapper(big_land), tem_optim)
+    t_loss = combineLoss(loss_vector, Val{:sum}())
     return t_loss
 end
 
@@ -145,8 +145,8 @@ function get_loc_loss(
         tem_models,
         loc_land_init,
         f_one)
-    lossVec = getLossVector(loc_obs, landWrapper(big_land), tem_optim)
-    t_loss = combineLoss(lossVec, Val{:sum}())
+    loss_vector = getLossVector(loc_obs, landWrapper(big_land), tem_optim)
+    t_loss = combineLoss(loss_vector, Val{:sum}())
     return t_loss
 end
 
