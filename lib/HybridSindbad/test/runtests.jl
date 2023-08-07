4d3b1a18712743e55298e9c5edd95a343d2973b2
Lazaro Alonso <lazarus.alon@gmail.com>
2023-08-07 12:06:31 +0200

    add hybrid pkg

---
+++ lib/HybridSindbad/test/runtests.jl
@@ -0,0 +1,27 @@
+using HybridSindbad
+using Test
+
+@testset "HybridSindbad.jl" begin
+    list = string.('a':'d')
+    @test shuffle_list(list) == ["a", "d", "b", "c"]
+    @test shuffle_list(list; seed = 312) == ["c", "b", "d", "a"]
+    @test length(bs_iter(10; batch_size=5)) == 2
+    batch_test = batch_shuffle(list, 2; seed=1)
+    @test length(batch_test) == 2
+    @test length(batch_test[1]) == 2
+
+    # test nn_dense
+    m1 = DenseNN(5, 2, 1)
+    @test length(m1(rand(5))) == 1
+    m2 = DenseNN(5, 2, 2)
+    @test size(m2(rand(Float32, 5))) == (2,)
+    dense_flat = DenseFlattened(m2)
+    @test typeof(dense_flat[1]) <: AbstractArray
+    @test typeof(dense_flat[2]) <: HybridSindbad.Optimisers.Restructure
+    @test typeof(dense_flat[3]) <: HybridSindbad.Optimisers.Leaf
+
+    # ForwardDiff_grads
+    f_loss(x, a, b) = a*x[1]^2 + b*x[2]
+    args = (; a=2, b = 1)
+    @test ForwardDiff_grads(f_loss, [1,0], args...) == [4,1]
+end
