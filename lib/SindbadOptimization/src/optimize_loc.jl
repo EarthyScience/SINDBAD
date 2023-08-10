8fd9fe54ce76f1a75e9e6706f628e3743c2590d4
Lazaro Alonso <lazarus.alon@gmail.com>
2023-08-10 14:29:22 +0200

    refactor DiffCache level and loss calls

--- lib/SindbadOptimization/src/optimize_loc.jl
+++ lib/SindbadOptimization/src/optimize_loc.jl
@@ -113,3 +113,53 @@ function loc_loss_inner(up_params,
         tem_optim,
         f_one)
 end
+
+# simpler forms
+
+"""
+    getSiteLoss(inits, data, data_optim, tem)
+
+- inits = (; init_land, approaches)
+- data = (; allocated_output, site_forcings)
+- data_optim = (; site_obs, site_covariates)
+- tem = (; helpers, spinup, models, optim, f_one)
+"""
+
+function getSiteLoss(inits, data, data_optim, tem) 
+    coreEcosystem!(inits..., data..., tem...)
+    lossVec = getLossVectorArray(data_optim.site_obs, data.allocated_output, tem.optim.cost_options)
+    t_loss = combineLossArray(lossVec, Val{:sum}())
+    return t_loss
+end
+
+function siteLossInner(up_params, inits, data_cache, data_optim, tem, tblParams)
+    out_data = get_tmp.(data_cache.allocated_output, (up_params,))
+    new_apps = updateModelParametersType(tblParams, inits.approaches, up_params)
+    inits = (; inits..., approaches = new_apps)
+    data = (; allocated_output = out_data, site_forcings)
+    return getSiteLoss(inits, data, data_optim, tem)
+end
+
+
+function gradsBatch!(loss_function::Function, up_params_now, f_grads, xbatch, sites_f, data, data_optim,
+    tem, tblParams, land_init_space, approaches; logging=true)
+
+    p = Progress(length(xbatch); desc="Computing batch grads...", offset=1, color=:yellow, enabled=logging)
+    for idx ∈ eachindex(xbatch)
+
+        site_name, new_vals = scaledParams(up_params_now, tblParams, xbatch, idx)
+        site_location = name_to_id(site_name, sites_f)
+        init_land = land_init_space[site_location[1][2]]
+
+        loc_output, loc_forcing, loc_obs = getLocDataObsN(data..., data_optim.site_obs, site_location) # check output order in original definition
+
+        inits = (; init_land, approaches)
+        data_optim = (; site_obs = loc_obs, )
+        data_cache = (; allocated_output = DiffCache.(loc_output), site_forcings= loc_forcing)
+
+        gg = ForwardDiffGrads(loss_function, new_vals, inits, data_cache, data_optim, tem, tblParams)
+        f_grads[:, idx] = gg
+
+        next!(p; showvalues=[(:site_name, site_name)])
+    end
+end
