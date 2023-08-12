2d824c87ce9be1c744f706888662a774e103d61d
skoirala <skoirala@bgc-jena.mpg.de>
2023-08-12 11:13:01 +0200

    fix slurm scripts

--- examples/exp_WROASTED/wroasted_jobarray_erai.sh
+++ examples/exp_WROASTED/wroasted_jobarray_erai.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=3000
 #SBATCH --array=1-205%205
-#SBATCH --time=3-00:00:00
+#SBATCH --time=15-00:00:00
 
 export JULIA_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 /Net/Groups/Services/HPC_22/apps/julia/julia-1.9.0/bin/julia --project=../exp_distri --heap-size-hint=5G WROASTED_jobarray_erai.jl
