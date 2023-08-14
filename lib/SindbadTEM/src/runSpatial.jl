679a0cd56b1378d207e8824f31124636105c1926
Lazaro Alonso <lazarus.alon@gmail.com>
2023-08-14 17:31:49 +0200

    definitions

--- lib/SindbadTEM/src/runSpatial.jl
+++ lib/SindbadTEM/src/runSpatial.jl
@@ -49,64 +49,36 @@ function ids_location(cov_sites, sites_f)
     return ids
 end
 
-function pixel_run!(output,
-    forc,
-    obs,
-    site_location,
-    tblParams,
-    forward,
-    upVector,
-    tem_helpers,
-    tem_spinup,
-    tem_models,
-    land_init_site,
-    f_one)
-
-    loc_forcing, loc_output, _ = getLocDataObsN(output.data, forc, obs, site_location)
-    up_apps = updateModelParametersType(tblParams, forward, upVector)
-    return coreEcosystem!(loc_output,
-        up_apps,
-        loc_forcing,
-        tem_helpers,
-        tem_spinup,
-        tem_models,
-        land_init_site,
-        f_one)
+function pixel_run!(inits, data, tem)
+    return coreTEM!(inits.selected_models, data..., inits.land_init, tem...)
 end
 
-function space_run!(up_params,
+function space_run!(
+    up_params,
     tblParams,
     sites_f,
     land_init_space,
-    cov_sites,
-    output,
-    forc,
-    obs,
-    forward,
-    tem_helpers,
-    tem_spinup,
-    tem_models,
-    f_one)
+    data,
+    data_optim,
+    tem
+)
     #Threads.@threads for site_index ∈ eachindex(cov_sites)
-    p = Progress(size(cov_sites,1))
-    for site_index ∈ eachindex(cov_sites)
-        site_name = cov_sites[site_index]
-        x_params = up_params(; site=site_name)
+    p = Progress(size(data_optim.cov_sites,1))
+    for site_index ∈ eachindex(data_optim.cov_sites)
+        site_name = data_optim.cov_sites[site_index]
+        new_params = up_params(; site=site_name) |> getParamsAct
+
         site_location = name_to_id(site_name, sites_f)
         loc_land_init = land_init_space[site_location[1][2]]
-        pixel_run!(output,
-            forc,
-            obs,
-            site_location,
-            tblParams,
-            forward,
-            x_params,
-            tem_helpers,
-            tem_spinup,
-            tem_models,
-            loc_land_init,
-            f_one
-        )
+        
+        loc_forcing, loc_output, _ =
+            getLocDataObsN(data.allocated_output, data.forcing, data_optim.obs, site_location)
+
+        new_approaches = updateModelParametersType(tblParams, inits.selected_models, new_params)
+        inits = (; selected_models=new_approaches, land_init=loc_land_init)
+        data = (; loc_forcing, forcing_one_timestep, allocated_output = loc_output)
+
+        pixel_run!(inits, data, tem)
         next!(p)
     end
 end
