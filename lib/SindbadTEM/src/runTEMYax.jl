2c796ddd5a02aaa8b5fed462de8c98c113c9b1bc
dr-ko <skoirala@bgc-jena.mpg.de>
2023-08-15 23:11:34 +0200

    yax forward run

--- lib/SindbadTEM/src/runTEMYax.jl
+++ lib/SindbadTEM/src/runTEMYax.jl
@@ -1,4 +1,4 @@
-export runTEMYAX
+export runTEMYax
 
 
 """
@@ -14,28 +14,27 @@ export runTEMYAX
 - `forcing_variables`: DESCRIPTION
 """
 function TEMYax(args...;
+    selected_models::Tuple,
+    forcing_variables::AbstractArray,
+    forcing_one_timestep,
     land_init::NamedTuple,
     tem::NamedTuple,
-    selected_models::Tuple,
-    forcing_variables::AbstractArray)
-    outputs, inputs = unpackYaxForward(args; tem, forcing_variables)
+    out_variables)
+    outputs, inputs = unpackYaxForward(args; out_variables, forcing_variables)
     forcing = (; Pair.(forcing_variables, inputs)...)
 
     land_out = coreTEM(selected_models, forcing, forcing_one_timestep, land_init, tem.helpers, tem.models, tem.spinup, tem.helpers.run.spinup.spinup_TEM)
 
     i = 1
-    tem_variables = tem.variables
-    for group ∈ keys(tem_variables)
-        data = land_out[group]
-        for k ∈ tem_variables[group]
-            viewCopyYax(outputs[i], data[k])
+    foreach(out_variables) do var_pair
+        data = land_out[first(var_pair)][last(var_pair)]
+            viewCopyYax(outputs[i], data)
             i += 1
-        end
     end
 end
 
 """
-    runTEMYAX(forcing::NamedTuple, output::NamedTuple, tem::NamedTuple, selected_models::Tuple; max_cache = 1.0e9)
+    runTEMYax(forcing::NamedTuple, output::NamedTuple, tem::NamedTuple, selected_models::Tuple; max_cache = 1.0e9)
 
 
 
@@ -46,17 +45,21 @@ end
 - `selected_models`: a tuple of all models selected in the given model structure
 - `max_cache`: DESCRIPTION
 """
-function runTEMYAX(forcing::NamedTuple,
-    output::NamedTuple,
-    tem::NamedTuple,
+function runTEMYax(
     selected_models::Tuple;
-    max_cache=1e9)
+    forcing::NamedTuple,
+    info::NamedTuple)
+
+    # forcing/input information
     incubes = forcing.data
     indims = forcing.dims
-    # innames = get_indim_symbiols
     forcing_variables = collect(forcing.variables)
-    outdims = output.dims
-    land_init = deepcopy(output.land_init)
+
+    # information for running model
+    run_helpers = prepTEM(forcing, info)
+    outdims = run_helpers.output.dims
+    land_one = deepcopy(run_helpers.land_one)
+    out_variables = valToSymbol(run_helpers.tem_with_types.helpers.vals.output_vars)
     #additionaldims = setdiff(keys(tem.helpers.run.loop),[:time])
     #nthreads = 1 ? !isempty(additionaldims) : Threads.nthreads()
     # @show "I am here"
@@ -64,13 +67,14 @@ function runTEMYAX(forcing::NamedTuple,
     # @show outdims
     outcubes = mapCube(TEMYax,
         (incubes...,);
-        land_init=land_init,
-        tem=tem,
+        land_init=land_one,
+        tem=run_helpers.tem_with_types,
         selected_models=selected_models,
         forcing_variables=forcing_variables,
+        out_variables = out_variables,
         indims=indims,
         outdims=outdims,
-        max_cache=max_cache,
+        max_cache=info.experiment.exe_rules.yax_max_cache,
         ispar=false,
         #nthreads = [1],
     )
@@ -88,9 +92,9 @@ end
 - `tem`: a nested NT with necessary information of helpers, models, and spinup needed to run SINDBAD TEM and models
 - `forcing_variables`: DESCRIPTION
 """
-function unpackYaxForward(args; tem::NamedTuple, forcing_variables::AbstractArray)
+function unpackYaxForward(args; out_variables::NamedTuple, forcing_variables::AbstractArray)
     nin = length(forcing_variables)
-    nout = sum(length, tem.variables)
+    nout = length(out_variables)
     outputs = args[1:nout]
     inputs = args[(nout+1):(nout+nin)]
     return outputs, inputs
