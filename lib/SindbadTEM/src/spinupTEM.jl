6b59ee3f011e5cfabd4ed78b6341eeddd55eb776
dr-ko <skoirala@bgc-jena.mpg.de>
2023-09-07 09:14:00 +0200

    clean getforcing and prep tem

--- lib/SindbadTEM/src/spinupTEM.jl
+++ lib/SindbadTEM/src/spinupTEM.jl
@@ -889,8 +889,9 @@ function timeLoopTEMSpinup(
     forcing_one_timestep,
     land,
     tem_helpers)
-    num_timesteps = getForcingTimeSize(spinup_forcing, tem_helpers.vals.forc_vars)
-    for ts ∈ 1:num_timesteps
+    # num_timesteps = getForcingTimeSize(spinup_forcing, tem_helpers.vals.forc_vars)
+    for ts ∈ eachindex(spinup_forcing[1])
+        # f_ts = getForcingForTimeStep(spinup_forcing, forcing_one_timestep, ts)
         f_ts = getForcingForTimeStep(spinup_forcing, forcing_one_timestep, ts, tem_helpers.vals.forc_vars)
         land = computeTEM(spinup_models, f_ts, land, tem_helpers)
     end
