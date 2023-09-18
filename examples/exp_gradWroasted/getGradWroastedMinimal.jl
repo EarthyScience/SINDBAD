4aea773352eb41122d8ad28d318fe438c5d52393
dr-ko <skoirala@bgc-jena.mpg.de>
2023-09-18 12:02:50 +0200

    some internal refactor

--- examples/exp_gradWroasted/getGradWroastedMinimal.jl
+++ examples/exp_gradWroasted/getGradWroastedMinimal.jl
@@ -8,7 +8,8 @@ toggleStackTraceNT()
 
 experiment_json = "../exp_gradWroasted/settings_gradWroasted/experiment.json"
 replace_info = Dict(
-"experiment.exe_rules.model_number_type" => "Real")
+    "experiment.flags.debug_model" => false,
+"experiment.exe_rules.model_number_type" => "Float32")
 
 info = getExperimentInfo(experiment_json; replace_info=replace_info); # note that this will modify information from json with the replace_info
 
@@ -30,6 +31,15 @@ run_helpers = prepTEM(forcing, info);
     run_helpers.loc_outputs,
     run_helpers.land_init_space,
     run_helpers.tem_with_types)
+
+@time spinupTEM(
+    info.tem.models.forward,
+    run_helpers.loc_spinup_forcings[1],
+    run_helpers.forcing_one_timestep,
+    run_helpers.land_init_space[1],
+    run_helpers.tem_with_types.helpers,
+    run_helpers.tem_with_types.models,
+    run_helpers.tem_with_types.spinup);
 
 tbl_params = getParameters(info.tem.models.forward,
     info.optim.model_parameter_default,
@@ -84,40 +94,47 @@ function l1(p)
         cost_options,
         info.optim.multi_constraint_method)
 end
-l1(tbl_params.default .* rand())
-
-l1(tbl_params.default)
-
-
-
-l1(p_vec)
 
 
 p_vec = tbl_params.default;
+
+l1(p_vec)
+
 CHUNK_SIZE = length(p_vec)
 CHUNK_SIZE = 10
 
 cfg = ForwardDiff.GradientConfig(l1, p_vec, ForwardDiff.Chunk{CHUNK_SIZE}());
 
-# op = prepTEMOut(info, forcing.helpers);
-# output_array = [Array{ForwardDiff.Dual{ForwardDiff.Tag{typeof(l1),info.tem.helpers.numbers.num_type},info.tem.helpers.numbers.num_type,CHUNK_SIZE}}(undef, size(od)) for od in run_helpers.output_array];
-output_array = [Array{Any}(undef, size(od)) for od in run_helpers.output_array];
-# op = (; op..., data=op_dat);
-# output_array = op_dat;
+dualDefs = ForwardDiff.Dual{ForwardDiff.Tag{typeof(l1),info.tem.helpers.numbers.num_type},info.tem.helpers.numbers.num_type,CHUNK_SIZE}.(tbl_params.default);
+@time mods = updateModelParameters(info.tem.models.forward, dualDefs, info.optim.param_model_id_val);
 
-# dualDefs = ForwardDiff.Dual{ForwardDiff.Tag{typeof(l1),info.tem.helpers.numbers.num_type},info.tem.helpers.numbers.num_type,CHUNK_SIZE}.(tbl_params.default);
-dualDefs = ForwardDiff.Dual.(tbl_params.default);
-mods = updateModelParametersType(tbl_params, mods, dualDefs);
+run_helpers_d = prepTEM(mods, forcing, info);
 
-run_helpers = prepTEM(forcing, info);
+# @time lw_timeseries_prep = runTEM(info.tem.models.forward, run_helpers.loc_forcings[1], run_helpers.loc_spinup_forcings[1], run_helpers.forcing_one_timestep, run_helpers.land_one, run_helpers.tem_with_types);
 
 @time runTEM!(mods,
-    run_helpers.loc_forcings,
-    run_helpers.loc_spinup_forcings,
-    run_helpers.forcing_one_timestep,
-    run_helpers.loc_outputs,
-    run_helpers.land_init_space,
-    run_helpers.tem_with_types)
+    run_helpers_d.loc_forcings,
+    run_helpers_d.loc_spinup_forcings,
+    run_helpers_d.forcing_one_timestep,
+    run_helpers_d.loc_outputs,
+    run_helpers_d.land_init_space,
+    run_helpers_d.tem_with_types)
+
+@time spinupTEM(
+    mods,
+    run_helpers_d.loc_spinup_forcings[1],
+    run_helpers_d.forcing_one_timestep,
+    run_helpers_d.land_init_space[1],
+    run_helpers_d.tem_with_types.helpers,
+    run_helpers_d.tem_with_types.models,
+    run_helpers_d.tem_with_types.spinup);
+
+# op = prepTEMOut(info, forcing.helpers);
+# op_dat = [Array{ForwardDiff.Dual{ForwardDiff.Tag{typeof(l1),tem_with_types.helpers.numbers.num_type},tem_with_types.helpers.numbers.num_type,10}}(undef, size(od)) for od in run_helpers.output_array];
+# op = (; op..., data=op_dat);
+
+@time grad = ForwardDiff.gradient(l1, dualDefs)
+
 
 
 # op = prepTEMOut(info, forcing.helpers);
