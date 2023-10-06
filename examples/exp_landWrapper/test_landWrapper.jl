dfaf9ec229183c899a36a69cceda2b146337181d
dr-ko <skoirala@bgc-jena.mpg.de>
2023-10-06 17:27:09 +0200

    fix land wrapper tests

--- examples/exp_landWrapper/test_landWrapper.jl
+++ examples/exp_landWrapper/test_landWrapper.jl
@@ -2,6 +2,7 @@ using Revise
 using SindbadData
 using SindbadTEM
 using SindbadMetrics
+using SindbadExperiment
 using Plots
 toggleStackTraceNT()
 experiment_json = "../exp_landWrapper/settings_landWrapper/experiment.json"
@@ -47,30 +48,15 @@ run_helpers = prepTEM(forcing, info);
 
 @time runTEM!(info.tem.models.forward, run_helpers.loc_forcings, run_helpers.loc_spinup_forcings, run_helpers.forcing_one_timestep, run_helpers.loc_outputs, run_helpers.land_init_space, run_helpers.tem_with_types)
 
+@time land_stacked_ts = runTEM(info.tem.models.forward, run_helpers.loc_forcings[1], run_helpers.loc_spinup_forcings[1], run_helpers.forcing_one_timestep, deepcopy(run_helpers.land_one), run_helpers.tem_with_types);
 
-@time lw_timeseries_prep = runTEM(info.tem.models.forward, run_helpers.loc_forcings[1], run_helpers.loc_spinup_forcings[1], run_helpers.forcing_one_timestep, run_helpers.land_one, run_helpers.tem_with_types);
+land_stacked_prealloc = Vector{typeof(run_helpers.land_one)}(undef, info.tem.helpers.dates.size);
 
-@time lw_timeseries = runTEM(forcing, info);
+@time land_stacked_prealloc = runTEM(info.tem.models.forward, run_helpers.loc_forcings[1], run_helpers.loc_spinup_forcings[1], run_helpers.forcing_one_timestep, land_stacked_prealloc, deepcopy(run_helpers.land_one), run_helpers.tem_with_types);
 
-land_timeseries = Vector{typeof(run_helpers.land_one)}(undef, info.tem.helpers.dates.size);
 
-@time lw_timeseries_vec = runTEM(info.tem.models.forward, run_helpers.loc_forcings[1], run_helpers.loc_spinup_forcings[1], run_helpers.forcing_one_timestep, land_timeseries, run_helpers.land_one, run_helpers.tem_with_types);
+tbl_params = getParameters(info.tem.models.forward, info.optimization.model_parameter_default, info.optimization.model_parameters_to_optimize, info.tem.helpers.numbers.sNT);
 
-tbl_params = getParameters(info.tem.models.forward,
-    info.optimization.model_parameter_default,
-    info.optimization.model_parameters_to_optimize,
-    info.tem.helpers.numbers.sNT);
-selected_models = info.tem.models.forward;
-
-rand_m = rand()
-param_vector = tbl_params.default .* rand_m;
-@time selected_models = updateModelParameters(info.tem.models.forward, param_vector, info.optim.param_model_id_val);
-
-run_helpers_s = prepTEM(selected_models, forcing, info);
-
-land_timeseries_s = Vector{typeof(run_helpers_s.land_one)}(undef, info.tem.helpers.dates.size);
-
-@time lw_timeseries_vec = runTEM(selected_models, run_helpers_s.loc_forcings[1], run_helpers_s.loc_spinup_forcings[1], run_helpers_s.forcing_one_timestep, land_timeseries_s, run_helpers_s.land_one, run_helpers_s.tem_with_types);
 
 # calculate the losses
 observations = getObservation(info, forcing.helpers);
@@ -80,9 +66,8 @@ cost_options = prepCostOptions(obs_array, info.optim.cost_options);
 # setLogLevel(:debug)
 # @profview getLossVector(obs_array, run_helpers.output_array, cost_options) # |> sum
 @time getLossVector(run_helpers.output_array, obs_array, cost_options) # |> sum
-@time getLossVector(lw_timeseries_prep, obs_array, cost_options) # |> sum
-@time getLossVector(lw_timeseries, obs_array, cost_options) # |> sum
-@time getLossVector(lw_timeseries_vec, obs_array, cost_options) #|> sum
+@time getLossVector(land_stacked_ts, obs_array, cost_options) # |> sum
+@time getLossVector(land_stacked_prealloc, obs_array, cost_options) # |> sum
 
 
 tbl_params = getParameters(info.tem.models.forward, info.optim.model_parameter_default, info.optim.model_parameters_to_optimize, info.tem.helpers.numbers.sNT)
@@ -93,6 +78,5 @@ defaults = tbl_params.default;
 
 @time getLoss(defaults, info.tem.models.forward, run_helpers.loc_forcings[1], run_helpers.loc_spinup_forcings[1], run_helpers.forcing_one_timestep, run_helpers.land_one, run_helpers.tem_with_types, obs_array, tbl_params, cost_options, info.optim.multi_constraint_method)
 
-@time getLoss(defaults, info.tem.models.forward, run_helpers.loc_forcings[1], run_helpers.loc_spinup_forcings[1], run_helpers.forcing_one_timestep, land_timeseries, run_helpers.land_one, run_helpers.tem_with_types, obs_array, tbl_params, cost_options, info.optim.multi_constraint_method)
 
 
