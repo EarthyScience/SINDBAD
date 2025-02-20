75345bebc0bf198d9e21a0bde1ad7e2b668aa2b9
Lazaro Alonso <lazarus.alon@gmail.com>
2025-02-20 09:28:12 +0100

    do core

--- lib/SindbadML/src/SindbadML.jl
+++ lib/SindbadML/src/SindbadML.jl
@@ -1,24 +1,35 @@
 module SindbadML
 
-using Distributed: @distributed, @sync
-using SharedArrays: SharedArray
+using Distributed:
+    nworkers,
+    pmap,
+    workers,
+    nprocs,
+    CachingPool
 
 using Reexport: @reexport
 using SindbadTEM
+using SindbadData.YAXArrays
+using SindbadData.Zarr
+using SindbadData.AxisKeys
+using SindbadData: AllNaN
 using Flux
 using Optimisers
 using FiniteDiff
 using FiniteDifferences
 using ForwardDiff
+using PolyesterForwardDiff
 using Zygote
 using Statistics
-using ProgressMeter
+import ProgressMeter: @showprogress, Progress, next!, progress_pmap, progress_map
 using PreallocationTools
 using Base.Iterators: repeated, partition
 using Random
 using JLD2
 
 include("utilsML.jl")
-include("runSindbadML.jl")
+include("diffCaches.jl")
+include("neuralNetwork.jl")
+include("trainPBM.jl")
 
 end # module SindbadML
---
+++ lib/SindbadML/src/trainPBM.jl
@@ -0,0 +1,173 @@
+export gradientPolyester
+export gradientBatchPolyester!
+export trainPBM
+export gradsNaNCheck!
+export loadTrainedNN
+
+"""
+    trainPBM(grads_lib, nn_model, train_refs, test_val_refs, loss_fargs, forward_args; n_epochs=3, optimizer=Optimisers.Adam(), path_experiment="/")
+
+Mixed-gradient training. Computes model parameters from a neural network which in turn are used by PBMs to estimate parameter gradients. 
+The product of these gradients with the neural network Jacobian output the corresponding neural network weights updates.
+
+Inputs:
+- `grads_lib`: Library to compute PBMs parameter gradients.
+- `nn_model`: A `Flux.Chain` neural network.
+- `train_refs`: training data features.
+- `test_val_refs`: test and validation data features.
+- `loss_fargs`: functions used to calculate the loss.
+- `forward_args`: arguments to evaluate the PBMs.
+- `path_experiment="/"`: save model to path.
+
+"""
+function trainPBM(grads_lib, nn_model, train_refs, test_val_refs, total_constraints, loss_fargs, forward_args;
+    n_epochs=3, optimizer=Optimisers.Adam(), path_experiment="/")
+    
+    sites_training, indices_sites_training, xfeatures, tbl_params, batch_size, chunk_size, metadata_global = train_refs
+    sites_validation, indices_sites_validation, sites_testing, indices_sites_testing = test_val_refs
+
+    lossSite, getInnerArgs = loss_fargs
+    flat, re, opt_state = destructureNN(nn_model; nn_opt=optimizer)
+    n_params = length(nn_model[end].bias)
+
+    loss_training = fill(zero(Float32), length(sites_training), n_epochs)
+    loss_validation = fill(zero(Float32), length(sites_validation), n_epochs)
+    loss_testing = fill(zero(Float32), length(sites_testing), n_epochs)
+    # ? save also the individual losses
+    loss_split_training = fill(zero(Float32), length(sites_training), total_constraints, n_epochs)
+    loss_split_validation = fill(zero(Float32), length(sites_validation), total_constraints, n_epochs)
+    loss_split_testing = fill(zero(Float32), length(sites_testing), total_constraints, n_epochs)
+
+    path_checkpoint = joinpath(path_experiment, "checkpoint")
+    f_path = mkpath(path_checkpoint)
+
+    @showprogress desc="training..." for epoch ∈ 1:n_epochs
+        x_batches, idx_xbatches = batchShuffler(sites_training, indices_sites_training, batch_size; bs_seed=epoch)
+
+        for (sites_batch, indices_sites_batch) in zip(x_batches, idx_xbatches)
+            
+            grads_batch = zeros(Float32, n_params, length(sites_batch))
+            x_feat_batch = xfeatures(; site=sites_batch)
+            new_params, pullback_func = Zygote.pullback(p -> re(p)(x_feat_batch), flat)            
+            _params_batch = getParamsAct(new_params, tbl_params)
+
+            input_args = (_params_batch, forward_args..., indices_sites_batch, sites_batch)
+            gradientBatchPolyester!(grads_lib, grads_batch, chunk_size, lossSite, getInnerArgs, input_args...)
+            gradsNaNCheck!(grads_batch, _params_batch, sites_batch, tbl_params) #? checks for NaNs and if any replace them with 0.0f0
+            # Jacobian-vector product
+            ∇params = pullback_func(grads_batch)[1]
+            opt_state, flat = Optimisers.update(opt_state, flat, ∇params)
+        end
+        # calculate losses for all sites!
+        _params_epoch = re(flat)(xfeatures)
+        params_epoch = getParamsAct(_params_epoch, tbl_params)
+        getLossForSites(grads_lib, lossSite, loss_training, loss_split_training, epoch, params_epoch, sites_training, indices_sites_training, forward_args...)
+        # ? validation
+        getLossForSites(grads_lib, lossSite, loss_validation, loss_split_validation, epoch, params_epoch, sites_validation, indices_sites_validation, forward_args...)
+        # ? test 
+       getLossForSites(grads_lib, lossSite, loss_testing, loss_split_testing, epoch, params_epoch, sites_testing, indices_sites_testing, forward_args...)
+
+        jldsave(joinpath(f_path, "checkpoint_epoch_$(epoch).jld2");
+            lower_bound=tbl_params.lower, upper_bound=tbl_params.upper, ps_names=tbl_params.name,
+            tbl_params=tbl_params,
+            metadata_global=metadata_global,
+            loss_training=loss_training[:, epoch],
+            loss_validation=loss_validation[:, epoch],
+            loss_testing=loss_testing[:, epoch],
+            loss_split_training=loss_split_training[:,:, epoch],
+            loss_split_validation=loss_split_validation[:,:, epoch],
+            loss_split_testing=loss_split_testing[:,:, epoch],
+            re=re,
+            flat=flat)
+    end
+    return nothing
+end
+
+function batchShuffler(x_forcings, ids_forcings, batch_size; bs_seed=1456)
+    x_batches = shuffleBatches(x_forcings, batch_size; seed=bs_seed)
+    ids_batches = shuffleBatches(ids_forcings, batch_size; seed=bs_seed)
+    return x_batches, ids_batches
+end
+
+"""
+    gradientSitePolyester(grads_lib::ForwardDiffGrad, x_vals, chunk_size::Int, loss::F, args...)
+
+Computes gradients using `PolyesterForwardDiff.jl` for multi-threaded chunk splits. The optimal speed is ideally achieved with `one thread` when `chunk_size=1` and `n-threads` for `n` parameters.
+However, a good compromise between memory allocations and speed could be to set `chunk_size=3` and use `n-threads` for `2n parameters`.
+
+!!! warning
+    For M1 systems we default to ForwardDiff.gradient! single-threaded. And we let the `GradientConfig` constructor to automatically select the appropiate `chunk_size`.
+
+Returns: a `∇x` array with all parameter's gradients.
+"""
+function gradientPolyester(grads_lib::ForwardDiffGrad, x_vals, chunk_size::Int, loss_f::F, args...) where {F}
+    loss_tmp(x) = loss_f(x, grads_lib, args...)
+    ∇x = similar(x_vals) # pre-allocate
+    if occursin("arm64-apple-darwin", Sys.MACHINE) # fallback due to closure issues on M1 systems
+        # cfg = ForwardDiff.GradientConfig(loss_tmp, x_vals, Chunk{chunk_size}());
+        ForwardDiff.gradient!(∇x, loss_tmp, x_vals) # ?, add `cfg` at the end if further control is needed.
+    else
+        PolyesterForwardDiff.threaded_gradient!(loss_tmp, ∇x, x_vals, ForwardDiff.Chunk(chunk_size));
+    end
+    return ∇x
+end
+
+"""
+    gradientBatchPolyester!(grads_lib::ForwardDiffGrad, dx_batch, chunk_size::Int, loss_f::Function, get_inner_args::Function, input_args...; showprog=false)
+
+# Computes gradients for a batch of samples.
+
+Inputs:
+- `grads_lib`: uses ForwardDiff.jl for gradients computation.
+- `dx_batch`: pre-allocated array for batched gradients.
+- `chunk_size`: Int, chunk size for PolyesterForwardDiff's threads.
+- `loss_f`: loss function to be applied.
+- `get_inner_args`: function to obtain inner values of loss function.
+- `input_args`: global input arguments.
+
+# Returns: A  `n x m` matrix for `n parameters gradients` and `m` samples.
+"""
+function gradientBatchPolyester!(grads_lib::ForwardDiffGrad, dx_batch, chunk_size::Int,
+    loss_f::Function, get_inner_args::Function, input_args...; showprog=false)
+    
+    mapfun = showprog ? progress_pmap : pmap
+    result = mapfun(CachingPool(workers()), axes(dx_batch, 2)) do idx
+        x_vals, inner_args = get_inner_args(idx, grads_lib, input_args...)
+        gradientPolyester(grads_lib, x_vals, chunk_size, loss_f, inner_args...)
+    end
+    for idx in axes(dx_batch, 2)
+        dx_batch[:, idx] = result[idx]
+    end
+end
+
+"""
+    gradsNaNCheck!(grads_batch, _params_batch, sites_batch, tbl_params; show_params_for_nan=false)
+
+Utility function to check if some calculated gradients were NaN (if found please double check your approach).
+This function will replace those NaNs with 0.0f0.
+"""
+function gradsNaNCheck!(grads_batch, _params_batch, sites_batch, tbl_params; show_params_for_nan=false)
+    if sum(isnan.(grads_batch))>0
+        if show_params_for_nan
+            foreach(findall(x->isnan(x), grads_batch)) do ci
+                p_index_tmp, si = ci
+                site_name_tmp = sites_batch[si]
+                p_vec_tmp = _params_batch(site=site_name_tmp)
+                param_values =  Pair(tbl_params.name[p_index_tmp], (p_vec_tmp[p_index_tmp], tbl_params.lower[p_index_tmp], tbl_params.upper[p_index_tmp]))
+                @info "site: $site_name_tmp, parameter: $param_values"
+            end
+        end
+        @warn "NaNs in grads, replacing all by 0.0f0"
+        replace!(grads_batch, NaN => 0.0f0)
+    end
+end
+
+function loadTrainedNN(path_model)
+    model_props = JLD2.load(path_model)
+    return (;
+        trainedNN=model_props["re"](model_props["flat"]), # ? model structure and trained weights
+        lower_bound=model_props["lower_bound"],  # ? parameters' attributes    
+        upper_bound=model_props["upper_bound"],
+        ps_names=model_props["ps_names"],
+        metadata_global=model_props["metadata_global"])
+end
