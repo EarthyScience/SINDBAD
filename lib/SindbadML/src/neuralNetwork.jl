e6f038a1044b214f10ac181776043d85f7915cbb
Lazaro Alonso <lazarus.alon@gmail.com>
2025-02-20 11:35:11 +0100

    adds core ML functions

--- lib/SindbadML/src/neuralNetwork.jl
+++ lib/SindbadML/src/neuralNetwork.jl
@@ -2,16 +2,16 @@ export denseNN
 export destructureNN
 
 """
-    `denseNN`(`in_dim::Int`, `n_neurons::Int`, `out_dim::Int`; `extra_hlayers`=0, `activation_hidden`=Flux.relu, `activation_out`= Flux.sigmoid, seed=1618)
+    denseNN(in_dim::Int, n_neurons::Int, out_dim::Int; extra_hlayers=0, activation_hidden=Flux.relu, activation_out= Flux.sigmoid, seed=1618)
 
-Inputs:
-- `in_dim` :: input dimension
-- `n_neurons` :: number of neurons in each hidden layer
-- `out_dim` :: output dimension
-- `extra_hlayers`=0 :: controls the number of extra hidden layers, default is `zero` 
-- `activation_hidden`=Flux.relu :: activation function within hidden layers, default is Relu
-- `activation_out`= Flux.sigmoid :: activation of output layer, default is sigmoid
-- seed=1618 :: Random seed, default is ~ (1+√5)/2
+# Arguments
+- `in_dim`: input dimension
+- `n_neurons`: number of neurons in each hidden layer
+- `out_dim`: output dimension
+- `extra_hlayers`=0: controls the number of extra hidden layers, default is `zero` 
+- `activation_hidden`=Flux.relu: activation function within hidden layers, default is Relu
+- `activation_out`= Flux.sigmoid: activation of output layer, default is sigmoid
+- `seed=1618`: Random seed, default is ~ (1+√5)/2
 
 Returns a `Flux.Chain` neural network.
 """
@@ -32,9 +32,9 @@ end
 
 Given a `model` returns a `flat` vector with all weights, a `re` structure of the neural network and the current `state`.
 
-Inputs:
-- model: a Flux.Chain neural network.
-- nn_opt: Optimiser, the default is `Optimisers.Adam()`.
+# Arguments
+- `model`: a Flux.Chain neural network.
+- `nn_opt`: Optimiser, the default is `Optimisers.Adam()`.
 
 Returns: flat, re, opt_state
 """
