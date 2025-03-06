87b1f7f957d1faa3d7129cd8e23d7812d0332f03
Lazaro Alonso <lazarus.alon@gmail.com>
2025-03-06 11:30:11 +0100

    exp settings
    clean up

--- examples/exp_fluxnet_hybrid/exp_fluxnet_hybrid.jl
+++ examples/exp_fluxnet_hybrid/exp_fluxnet_hybrid.jl
@@ -2,11 +2,22 @@ using SindbadData
 using SindbadData.DimensionalData
 using SindbadData.AxisKeys
 using SindbadData.YAXArrays
-
 using SindbadTEM
 using SindbadML
+using SindbadML.JLD2
 using ProgressMeter
 include("load_covariates.jl")
+
+# load folds # $nfold $nlayer $neuron $batchsize
+_nfold = 5 #B ase.parse(Int, ARGS[1])
+nlayers = 3 # Base.parse(Int, ARGS[2])
+n_neurons = 32 # Base.parse(Int, ARGS[3])
+batch_size = 32 # Base.parse(Int, ARGS[4])
+
+batch_seed = 123 * batch_size
+
+file_folds = load(joinpath(@__DIR__, "nfolds_sites_indices.jld2"))
+xtrain, xval, xtest = file_folds["unfold_training"][_nfold], file_folds["unfold_validation"][_nfold], file_folds["unfold_tests"][_nfold]
 
 experiment_json = "../exp_fluxnet_hybrid/settings_fluxnet_hybrid/experiment.json"
 # for remote node
@@ -73,7 +84,10 @@ n_features = length(nor_names_order)
 n_neurons = 32;
 n_params = sum(tbl_params.is_ml);
 batch_seed = 123;
+
+# encode-decode architecture!
 mlBaseline = denseNN(n_features, n_neurons, n_params; extra_hlayers=2, seed=batch_seed * 2);
+# 
 parameters_sites = mlBaseline(xfeatures);
 
 ## test for gradients in batch
@@ -90,6 +104,32 @@ scaled_params_batch = getParamsAct(params_batch, tbl_params);
 # TODO: debug and benchmark again, one site!
 tem_info = run_helpers.tem_info;
 tem_run_spinup = run_helpers.tem_info.run.spinup_TEM;
+
+# ! full training
+# ? training
+sites_training = sites_forcing[xtrain];
+indices_sites_training = siteNameToID.(sites_training, Ref(sites_forcing));
+
+# # ? validation
+sites_validation = sites_forcing[xval];
+indices_sites_validation = siteNameToID.(sites_validation, Ref(sites_forcing));
+
+# # ? test
+sites_testing = sites_forcing[xtest];
+indices_sites_testing = siteNameToID.(sites_testing, Ref(sites_forcing));
+
+# NN 
+n_params = sum(tbl_params.is_ml);
+shuffle_opt = true;
+mlBaseline = denseNN(n_features, n_neurons, n_params; extra_hlayers=nlayers, seed=batch_seed * 2);
+parameters_sites = mlBaseline(xfeatures);
+
+## test for gradients in batch
+grads_batch = zeros(Float32, n_params, length(sites_training));
+sites_batch = sites_training;#[1:n_sites_train];
+indices_sites_batch = indices_sites_training;
+params_batch = parameters_sites(; site=sites_batch);
+scaled_params_batch = getParamsAct(params_batch, tbl_params);
 
 input_args = (
     scaled_params_batch,
@@ -114,35 +154,39 @@ loc_params, inner_args = getInnerArgs(1, grads_lib, input_args...);
 
 @time gg = gradientPolyester(grads_lib, loc_params, 2, lossSite, inner_args...)
 
+gradientBatchPolyester!(grads_lib, grads_batch, 2, lossSite, getInnerArgs,
+    input_args...; showprog=true)
 
-# ! full training
-# get site splits
+# ? training arguments
+chunk_size = 2
+metadata_global = info.output.file_info.global_metadata
 
-# train_split = 0.82;
-# valid_split = 0.1;
-# batch_size = 32;
-# shuffle_opt = true;
-# batch_size = min(batch_size, trunc(Int, 1/3*length(sites_common)));
+in_gargs=(;
+    train_refs = (; sites_training, indices_sites_training, xfeatures, tbl_params, batch_size, chunk_size, metadata_global),
+    test_val_refs = (; sites_validation, indices_sites_validation, sites_testing, indices_sites_testing),
+    total_constraints = length(info.optimization.observational_constraints),
+    forward_args = (selected_models,
+        space_forcing,
+        space_spinup_forcing,
+        loc_forcing_t,
+        space_output,
+        land_init,
+        tem_info,
+        tem_run_spinup,
+        param_to_index,
+        loc_observations,
+        cost_options,
+        constraint_method
+        ),
+    loss_fargs = (lossSite, getInnerArgs)
+);
 
+# ? now, this should take ~ 60 minutes(multi-threaded + multi-process). Before it was ~ 33 hrs (sequential).
+# checkpoint_path = joinpath(@__DIR__, "../analysis/training_all_features/")
+remote_raven = "/ptmp/lalonso/HybridOutput/HyALL_ALL_fold_$(_nfold)_nlayers_$(nlayers)_n_neurons_$(n_neurons)_batch_size_$(batch_size)/"
+mkpath(remote_raven)
+checkpoint_path = remote_raven
 
-# n_sites = length(sites_common);
-# n_batches = trunc(Int, n_sites * train_split/batch_size);
-# n_sites_train = n_batches * batch_size;
-# n_sites_valid = trunc(Int, n_sites * valid_split);
-# n_sites_test = n_sites - n_sites_valid - n_sites_train;
-
-# # filter and shuffle sites and subset
-# all_sites = shuffleList(sites_common; seed=batch_seed)
-# sites_training = all_sites[1:n_sites_train];
-# indices_sites_training = siteNameToID.(sites_training, Ref(sites_forcing));
-
-# # TODO: validation and testing
-# # ? validation
-# nfsvalid = all_sites[n_sites_train+1:n_sites_train+n_sites_valid]
-# sites_validation = shuffleList(nfsvalid; seed=batch_seed);
-# indices_sites_validation = siteNameToID.(sites_validation, Ref(sites_forcing));
-
-# # ? test
-# nfstest =  all_sites[n_sites_train+n_sites_valid+1:end]
-# sites_testing = shuffleList(nfstest; seed=batch_seed);
-# indices_sites_testing = siteNameToID.(sites_testing, Ref(sites_forcing));
+mixedGradientTraining(grads_lib, ml_baseline, in_gargs.train_refs, in_gargs.test_val_refs,
+    in_gargs.total_constraints, in_gargs.loss_fargs, in_gargs.forward_args;
+    n_epochs=500, path_experiment=checkpoint_path)
