2b87c9140b625f070a05010dd77914a8189a8ec9
Lazaro Alonso <lazarus.alon@gmail.com>
2025-03-17 11:40:26 +0100

    examples, docstrings

--- lib/SindbadML/src/neuralNetwork.jl
+++ lib/SindbadML/src/neuralNetwork.jl
@@ -1,5 +1,8 @@
 export denseNN
 export destructureNN
+export getPullback
+export JoinDenseNN
+# export SplitNN
 
 """
     denseNN(in_dim::Int, n_neurons::Int, out_dim::Int; extra_hlayers=0, activation_hidden=Flux.relu, activation_out= Flux.sigmoid, seed=1618)
@@ -48,6 +51,7 @@ function destructureNN(model; nn_opt=Optimisers.Adam())
 end
 
 # Custom Join layers
+# from
 # https://fluxml.ai/Flux.jl/stable/tutorials/custom_layers/
 
 struct Join{T, F}
@@ -71,50 +75,105 @@ Flux.@layer Join
 - all parameters as a vector or matrix (multiple samples)
 
 # EXAMPLE:
-    using Random
-    Random.seed!(123)
+```julia
+using Flux
+using Random
+Random.seed!(123)
 
-    m_big = Chain(Dense(4 => 5, relu), Dense(5 => 3), Flux.sigmoid)
-    m_eta = Dense(1=>1, Flux.sigmoid)
+m_big = Chain(Dense(4 => 5, relu), Dense(5 => 3), Flux.sigmoid)
+m_eta = Dense(1=>1, Flux.sigmoid)
 
-    x_big_a = rand(Float32, 4, 10)
-    x_small_a1 = rand(Float32, 1, 10)
-    x_small_a2 = rand(Float32, 1, 10)
+x_big_a = rand(Float32, 4, 10)
+x_small_a1 = rand(Float32, 1, 10)
+x_small_a2 = rand(Float32, 1, 10)
 
-    model = JoinDenseNN((m_big, m_eta))
-    model((x_big_a, x_small_a2))
-
+model = JoinDenseNN((m_big, m_eta))
+model((x_big_a, x_small_a2))
+```
 """
 function JoinDenseNN(models::Tuple)
     return Chain(Join(vcat, models...))
 end
 
+# ? multiple outputs and one input
+# We would also like to have multiple outputs, so that different predictors can be evaluated independently in the loss function.
+# from
+# https://fluxml.ai/Flux.jl/stable/tutorials/custom_layers/#Multiple-outputs:-a-custom-Split-layer
+
+# custom split layer
+struct SplitNN{T}
+    paths::T
+end
+  
+SplitNN(paths...) = SplitNN(paths)
+  
+Flux.@layer SplitNN
+  
+(m::SplitNN)(x::AbstractArray) = map(f -> f(x), m.paths)
+
+
 # Define pullbacks for single and multi inputs
 
 """
-    getPullback(re, flat, features::AbstractArray)
-    getPullback(re, flat, features::Tuple)
+    getPullback(flat, re, features::AbstractArray)
+    getPullback(flat, re, features::Tuple)
 
 # Arguments:
-- re, flat :: model structure (vanilla Chain Dense Layers) and weight parameters.
+- flat :: weight parameters.
+- re :: model structure (vanilla Chain Dense Layers).
 - features ::  `n` predictors and `s` samples.
     - A vector of predictors
-    - A matrix of predictors: (p_n x s)
-    - A tuple vector of predictors: (p1, p2)
-    - A tuple of matrices of predictors: [(p1_n x s), (p2_n x s)]
+    - A matrix of predictors: `(p_n x s)`
+    - A tuple vector of predictors: `(p1, p2)`
+    - A tuple of matrices of predictors: `[(p1_n x s), (p2_n x s)]`
 
 # Returns:
 - new parameters and pullback function
+
+# EXAMPLE: One input
+```julia
+using Flux
+# model
+m = Chain(Dense(4 => 5, relu), Dense(5 => 3), Flux.sigmoid)
+# features
+_feat = rand(Float32, 4)
+# apply
+flat, re = destructureNN(m)
+# Zygote
+new_params, pullback_func = getPullback(flat, re, _feat)
+# ? or
+_feat_ns = rand(Float32, 4, 3) # `n` predictors and `s` samples.
+new_params, pullback_func = getPullback(flat, re, _feat_ns)
+```
+
+# EXAMPLE: Multiple inputs
+```julia
+using Flux
+# model
+m1 = Chain(Dense(4 => 5, relu), Dense(5 => 3), Flux.sigmoid)
+m2 = Dense(2=>1, Flux.sigmoid)
+combo_ms = JoinDenseNN((m1, m2))
+# features
+_feat1 = rand(Float32, 4)
+_feat2 = rand(Float32, 2)
+# apply
+flat, re = destructureNN(combo_ms)
+# Zygote
+new_params, pullback_func = getPullback(flat, re, (_feat1, _feat2))
+# ? or with multiple samples
+_feat1_ns = rand(Float32, 4, 3) # `n` predictors and `s` samples.
+_feat2_ns = rand(Float32, 2, 3) # `n` predictors and `s` samples.
+new_params, pullback_func = getPullback(flat, re, (_feat1_ns, _feat2_ns))
+```
 """
 function getPullback end
 
-
-function getPullback(re, flat, features::AbstractArray)
+function getPullback(flat, re, features::AbstractArray)
     new_params, pullback_func = Zygote.pullback(p -> re(p)(features), flat)
     return new_params, pullback_func
 end
 
-function getPullback(re, flat, features::Tuple)
+function getPullback(flat, re, features::Tuple)
     new_params, pullback_func = Zygote.pullback(p -> re(p)(features), flat)
     return new_params, pullback_func
 end
