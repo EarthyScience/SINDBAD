8b013e1ade76daf663686049224afda10dc0297a
dr-ko <skoirala@bgc-jena.mpg.de>
2025-03-25 12:11:46 +0100

    external non-standard model order list through replace_info

--- lib/SindbadSetup/src/setupModels.jl
+++ lib/SindbadSetup/src/setupModels.jl
@@ -2,93 +2,6 @@ export setOrderedSelectedModels
 export setSpinupAndForwardModels
 
 """
-    changeModelOrder(info::NamedTuple, selected_models::AbstractArray)
-
-Reorders the list of models based on the order specified in the `model_structure.json` file.
-
-# Arguments:
-- `info`: A NamedTuple containing the experiment configuration.
-- `selected_models`: An array of selected models to reorder.
-
-# Returns:
-- A reordered list of models based on the specified order.
-
-# Notes:
-- USE WITH EXTREME CAUTION AS CHANGING ORDER MAY RESULT IN MODEL INCONSISTENCY
-- The default order is taken from `standard_sindbad_models`.
-- Models cannot be set before `getPools` or after `cCycle`.
-- Changing the order may result in model inconsistency, so use with caution.
-- Issues warnings if the model order is changed or duplicates are found in the order.
-"""
-function changeModelOrder(info::NamedTuple, selected_models::AbstractArray; sindbad_models=standard_sindbad_models::Tuple)
-    sindbad_models = [sindbad_models...]
-    checkSelectedModels(sindbad_models, selected_models)
-    # get orders of fixed models that cannot be changed
-    order_getPools = findfirst(e -> e == :getPools, sindbad_models)
-    order_cCycle = findfirst(e -> e == :cCycle, sindbad_models)
-
-    # get the new orders and models from model_structure.json
-    new_orders = Int64[]
-    new_models = (;)
-    order_changed_warn = true
-    for sm ∈ selected_models
-        model_info = getfield(info.settings.model_structure.models, sm)
-        if :order in propertynames(model_info)
-            push!(new_orders, model_info.order)
-            new_models = setTupleField(new_models, (sm, model_info.order))
-            if model_info.order <= order_getPools
-                error(
-                    "The model order for $(sm) is set at $(model_info.order). Any order earlier than or same as getPools ($order_getPools) is not permitted."
-                )
-            end
-            if model_info.order >= order_cCycle
-                error(
-                    "The model order for $(sm) is set at $(model_info.order). Any order later than or same as cCycle ($order_cCycle) is not permitted."
-                )
-            end
-            if order_changed_warn
-                @warn "changeModelOrder:: Model order has been changed through model_structure.json. Make sure that model structure is consistent by accessing the model list in info.models.selected_models and comparing it with standard_sindbad_models"
-                order_changed_warn = false
-            end
-            @warn "$(sm) [$(Pair(findfirst(e->e==sm, sindbad_models), model_info.order))]"
-        end
-    end
-
-    #check for duplicates in the order
-    if length(new_orders) != length(unique(new_orders))
-        nun = nonUnique(new_orders)
-        error(
-            "There are duplicates in the order [$(nun)] set in model_structure.json. Cannot set the same order for different models."
-        )
-    end
-
-    # sort the orders
-    new_orders = sort(new_orders; rev=true)
-
-    # create re-ordered list of full models
-    full_models_reordered = deepcopy(sindbad_models)
-    for new_order ∈ new_orders
-        sm = nothing
-        for nm ∈ keys(new_models)
-            if getproperty(new_models, nm) == new_order
-                sm = nm
-            end
-        end
-        old_order = findfirst(e -> e == sm, full_models_reordered)
-        # get the models without the model to be re-ordered
-        tmp = filter!(e -> e ≠ sm, full_models_reordered)
-        # insert the re-ordered model to the right place
-        if old_order >= new_order
-            insert!(tmp, new_order, sm)
-        else
-            insert!(tmp, new_order - 1, sm)
-        end
-        full_models_reordered = deepcopy(tmp)
-    end
-    return full_models_reordered
-end
-
-"""
     checkSelectedModels(sindbad_models::AbstractArray, selected_models::AbstractArray)
 
 Validates that the selected models in `model_structure.json` exist in the full list of `standard_sindbad_models`.
@@ -131,14 +44,44 @@ Retrieves the list of all SINDBAD models, either from the provided `info` object
 - If the `info` object has a property `sindbad_models`, it overrides the default list.
 - This function ensures flexibility by allowing custom model lists to be specified in the experiment configuration.
 """
-function getAllSindbadModels(info; all_models_default=standard_sindbad_models)
-    sindbad_models = all_models_default
-    @show propertynames(info.temp)
-    if hasproperty(info.temp, :sindbad_models)
-        sindbad_models = info.temp.sindbad_models
-        @show "I am here...", sindbad_models
+function getAllSindbadModels(info; sindbad_models=standard_sindbad_models)
+    if hasproperty(info.settings.model_structure, :sindbad_models)
+        sindbad_models = info.settings.model_structure.sindbad_models
+        @info "Using non-standard model order and list from model_structure.sindbad_models: "
+    else
+        @info "Using standard model order and list from standard_sindbad_models: "
     end
+        @info "         $(sindbad_models)"
     return sindbad_models
+end
+
+"""
+    getModelImplicitTRepeat(info::NamedTuple, selected_models)
+
+Retrieves the `implicit_t_repeat` values for the specified models from the experiment configuration.
+
+# Arguments:
+- `info::NamedTuple`: A SINDBAD NamedTuple containing the experiment configuration, including model structure details.
+- `selected_models`: A list of model names (symbols) for which the `implicit_t_repeat` values are to be retrieved.
+
+# Returns:
+- A vector of `implicit_t_repeat` values corresponding to the `selected_models`.
+
+# Notes:
+- If a model has an `implicit_t_repeat` property defined in its configuration, that value is used.
+- If the property is not defined for a model, the default value from `info.settings.model_structure.default_model.implicit_t_repeat` is used.
+"""
+function getModelImplicitTRepeat(info::NamedTuple, selected_models)
+    t_repeat = Int64[]
+    for sm ∈ selected_models
+        model_info = getfield(info.settings.model_structure.models, sm)
+        if :implicit_t_repeat in propertynames(model_info)
+            push!(t_repeat, model_info.implicit_t_repeat)
+        else
+            push!(t_repeat, info.settings.model_structure.default_model.implicit_t_repeat)
+        end
+    end
+    return t_repeat
 end
 
 
@@ -160,18 +103,18 @@ Retrieves and orders the list of selected models based on the configuration in `
 function setOrderedSelectedModels(info::NamedTuple)
     selected_models = collect(propertynames(info.settings.model_structure.models))
     sindbad_models = getAllSindbadModels(info)
-    all_sindbad_models_reordered = changeModelOrder(info, selected_models; sindbad_models=sindbad_models)
-    checkSelectedModels(all_sindbad_models_reordered, selected_models)
+    checkSelectedModels(sindbad_models, selected_models)
+    t_repeat_models = getModelImplicitTRepeat(info, selected_models)
+    checkSelectedModels(sindbad_models, selected_models)
     order_selected_models = []
-    for msm ∈ all_sindbad_models_reordered
+    for msm ∈ sindbad_models
         if msm in selected_models
             push!(order_selected_models, msm)
         end
     end
     @debug "     setupInfo: creating initial out/land..."
 
-    info = (; info..., temp=(; info.temp..., models=(; selected_models=Table((; model=[order_selected_models...])))))
-    info = (; info..., temp=(; info.temp..., models=(; selected_models=Table((; model=[order_selected_models...])))))
+    info = (; info..., temp=(; info.temp..., models=(; sindbad_models=sindbad_models, selected_models=Table((; model=[order_selected_models...],t_repeat=t_repeat_models)))))
     return info
 end
 
