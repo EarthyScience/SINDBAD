d816f6845d410a05198d3aef0f6035ae64ec3ebe
dr-ko <skoirala@bgc-jena.mpg.de>
2025-06-04 08:09:04 +0200

    prioritize abspath of depot than env

--- lib/SindbadUtils/src/utils.jl
+++ lib/SindbadUtils/src/utils.jl
@@ -152,7 +152,7 @@ Retrieve the Sindbad data depot path.
 The path to the Sindbad data depot.
 """
 function getSindbadDataDepot(; env_data_depot_var="SINDBAD_DATA_DEPOT", local_data_depot="../data")
-    data_depot = haskey(ENV, env_data_depot_var) ? ENV[env_data_depot_var] : local_data_depot
+    data_depot = isabspath(local_data_depot) ? local_data_depot : haskey(ENV, env_data_depot_var) ? ENV[env_data_depot_var] : local_data_depot
     return data_depot
 end
 
