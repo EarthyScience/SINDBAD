b701b3f9bdcb4feef3cf7259b0d3fb8bbfcedaf3
Lazaro <lazarus.alon@gmail.com>
2025-09-25 12:55:25 +0200

    get chunk size

--- examples/exp_fluxnet_hybrid/exp_fluxnet_hybrid_replace_r.jl
+++ examples/exp_fluxnet_hybrid/exp_fluxnet_hybrid_replace_r.jl
@@ -1,3 +1,4 @@
+using Revise
 using SindbadML
 using SindbadUtils
 using SindbadSetup
@@ -44,41 +45,6 @@ sites_forcing = forcing.data[1].site;
 hybrid_helpers = prepHybrid(forcing, observations, info, info.hybrid.ml_training.method);
 
 trainML(hybrid_helpers, info.hybrid.ml_training.method)
-
-# mixedGradientTraining(grads_lib, ml_model, sites_training, sites_validation, sites_testing, xfeatures, info.optimization.parameter_table, batch_size, chunk_size, num_constraints, metadata_global, loss_functions; n_epochs=2, path_experiment=checkpoint_path)
-
-
-## play around with gradient for sites and batch to understand internal workings
-ml_model = hybrid_helpers.ml_model;
-xfeatures = hybrid_helpers.features.data;
-loss_functions = hybrid_helpers.loss_functions;
-loss_component_functions = hybrid_helpers.loss_component_functions;
-
-params_sites = ml_model(xfeatures)
-@info "params_sites: [$(minimum(params_sites)), $(maximum(params_sites))]"
-
-scaled_params_sites = getParamsAct(params_sites, info.optimization.parameter_table)
-@info "scaled_params_sites: [$(minimum(scaled_params_sites)), $(maximum(scaled_params_sites))]"
-
-
-## try for a site
-site_index = 1
-site_name = sites_forcing[site_index]
-
-loc_params = scaled_params_sites(site=site_name).data.data
-loss_f_site = loss_functions(site=site_name);
-loss_vector_f_site = loss_component_functions(site=site_name);
-@time loss_f_site(loc_params)
-loss_vector_f_site(loc_params)
-
-@time g_site = gradientSite(info.hybrid.ml_gradient.method, loc_params, info.hybrid.ml_gradient.options, loss_functions(site=site_name))
-
-## try for a batch
-sites_batch = hybrid_helpers.sites.training[1:info.hybrid.ml_training.options.batch_size]
-scaled_params_batch = scaled_params_sites(; site=sites_batch)
-grads_batch = zeros(Float32, size(scaled_params_batch, 1), length(sites_batch));
-
-g_batch = gradientBatch!(info.hybrid.ml_gradient.method, grads_batch, info.hybrid.ml_gradient.options, loss_functions, scaled_params_batch, sites_batch; showprog=true)
 
 
 
