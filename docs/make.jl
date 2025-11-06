18a046056b5f15c6c05be4f1c1f2a8d97cf0288f
Lazaro Alonso <lazarus.alon@gmail.com>
2025-11-06 13:57:01 +0100

    docs fixes

--- docs/make.jl
+++ docs/make.jl
@@ -21,25 +21,25 @@ makedocs(; sitename="Sindbad",
     format=DocumenterVitepress.MarkdownVitepress(
         repo = "github.com/EarthyScience/SINDBAD",
     ),
-    remotes=nothing,
-    draft=false,
+    # remotes=nothing,
+    # draft=false,
     warnonly=true,
-    source="src",
-    build="build",
+    # source="src",
+    # build="build",
     )
+# ! ? why this ? 
+# final_site_dir = joinpath(@__DIR__,"build/final_site/")
+# if !isdir(final_site_dir)
+#     final_site_dir = joinpath(@__DIR__,"build/1/")
+# end
 
-final_site_dir = joinpath(@__DIR__,"build/final_site/")
-if !isdir(final_site_dir)
-    final_site_dir = joinpath(@__DIR__,"build/1/")
-end
-
-if !isdir(joinpath(final_site_dir, "/pages/concept/sindbad_info"))
-    cp(joinpath(@__DIR__,"src/pages/concept/sindbad_info"), joinpath(final_site_dir, "pages/concept/sindbad_info"); force=true)
-end
+# if !isdir(joinpath(final_site_dir, "/pages/concept/sindbad_info"))
+#     cp(joinpath(@__DIR__,"src/pages/concept/sindbad_info"), joinpath(final_site_dir, "pages/concept/sindbad_info"); force=true)
+# end
 
 deploydocs(; 
     repo = "github.com/EarthyScience/SINDBAD", # this must be the full URL!
-    target = "build", # this is where Vitepress stores its output
+    target = joinpath(@__DIR__, "build"), # this is where Vitepress stores its output
     branch = "gh-pages",
     devbranch = "main",
     push_preview = true
